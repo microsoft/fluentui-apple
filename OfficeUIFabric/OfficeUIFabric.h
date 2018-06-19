1b67475b5f9edd302efab5ba3b11f5ae21e226c9
Vladislav Filyakov <vladf@microsoft.com>
2018-06-19 11:19:32 -0700

    Initial Commit

---
+++ OfficeUIFabric/OfficeUIFabric.h
@@ -0,0 +1,19 @@
+//
+//  OfficeUIFabric.h
+//  OfficeUIFabric
+//
+//  Created by Vladislav Filyakov on 6/19/18.
+//  Copyright © 2018 Microsoft Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OfficeUIFabric.
+FOUNDATION_EXPORT double OfficeUIFabricVersionNumber;
+
+//! Project version string for OfficeUIFabric.
+FOUNDATION_EXPORT const unsigned char OfficeUIFabricVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OfficeUIFabric/PublicHeader.h>
+
+
