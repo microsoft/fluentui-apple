883e4c988a040958f7aecf8d9d23699b0b5d9a57
Vladislav Filyakov <vladf@microsoft.com>
2018-08-02 14:50:30 -0700

    - Added standard colors and fonts

--- OfficeUIFabric/Core/Colors.swift
+++ OfficeUIFabric/Core/Colors.swift
@@ -2,6 +2,37 @@
 //  Copyright © 2018 Microsoft Corporation. All rights reserved.
 //
 
-public extension UIColor {
+public struct Colors {
+    // MARK: Primary
     
+    /// #0078D4
+    public static var primary: UIColor = #colorLiteral(red: 0, green: 0.4705882353, blue: 0.831372549, alpha: 1)
+    
+    // MARK: Physical
+
+    /// #A8A8AC
+    public static let lightGray: UIColor = #colorLiteral(red: 0.6588235294, green: 0.6588235294, blue: 0.6745098039, alpha: 1)
+    /// #8E8E93
+    public static let gray: UIColor = #colorLiteral(red: 0.5568627451, green: 0.5568627451, blue: 0.5764705882, alpha: 1)
+    /// #777777
+    public static let darkGray: UIColor = #colorLiteral(red: 0.4666666667, green: 0.4666666667, blue: 0.4666666667, alpha: 1)
+    
+    /// #F8F8F8
+    public static let backgroundLightGray: UIColor = #colorLiteral(red: 0.9725490196, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
+    /// #F1F1F1
+    public static let backgroundGray: UIColor = #colorLiteral(red: 0.9450980392, green: 0.9450980392, blue: 0.9450980392, alpha: 1)
+    
+    /// #E1E1E1
+    public static let borderLightGray: UIColor = #colorLiteral(red: 0.8823529412, green: 0.8823529412, blue: 0.8823529412, alpha: 1)
+    /// #C8C8C8
+    public static let borderGray: UIColor = #colorLiteral(red: 0.7843137255, green: 0.7843137255, blue: 0.7843137255, alpha: 1)
+    
+    /// #FFFFFF
+    public static let white: UIColor = .white
+    /// #212121 ???
+    public static let black: UIColor = .black
+    
+    // MARK: Semantic
+    
+    // TODO: Add semantic colors describing colors used for particular control elements (must reference physical colors)
 }
--- OfficeUIFabric/Core/Fonts.swift
+++ OfficeUIFabric/Core/Fonts.swift
@@ -2,6 +2,21 @@
 //  Copyright © 2018 Microsoft Corporation. All rights reserved.
 //
 
-public extension UIFont {
-    
+public struct Fonts {
+    /// Regular 28pt
+    public static let title1: UIFont = .preferredFont(forTextStyle: .title1)    //!!! should be semibold
+    /// Regular 22pt
+    public static let title2: UIFont = .preferredFont(forTextStyle: .title2)    //!!! should be semibold
+    /// Semibold 17pt
+    public static let headline: UIFont = .preferredFont(forTextStyle: .headline)
+    /// Regular 17pt
+    public static let body: UIFont = .preferredFont(forTextStyle: .body)
+    /// Regular 15pt
+    public static let subhead: UIFont = .preferredFont(forTextStyle: .subheadline)
+    /// Regular 13pt
+    public static let footnote: UIFont = .preferredFont(forTextStyle: .footnote)
+    /// Regular 12pt
+    public static let caption1: UIFont = .preferredFont(forTextStyle: .caption1)
+    /// Regular 11pt
+    public static let caption2: UIFont = .preferredFont(forTextStyle: .caption2)
 }
