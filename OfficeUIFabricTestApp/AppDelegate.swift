58f08513b03e0f58ebd69e949695a82a47ed552f
anandrajeswaran <49537028+anandrajeswaran@users.noreply.github.com>
2019-06-13 15:11:53 -0700

    Create a basic master-detail view controller for the test app. (#5)
    * Create a basic master-detail view controller for the test app.
    
    * Update OfficeUIFabricTestApp/TestPlaceholderViewController.swift

--- OfficeUIFabricTestApp/AppDelegate.swift
+++ OfficeUIFabricTestApp/AppDelegate.swift
@@ -3,9 +3,22 @@
 //
 
 import AppKit
+fileprivate struct Constants {
+	private init() {}
+	static let initialWindowWidth: CGFloat = 800
+	static let initialWindowHeight: CGFloat = 600
+}
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+	func applicationWillFinishLaunching(_ notification: Notification) {
+		let window = NSWindow(contentViewController: TestControlsViewController(nibName: nil, bundle: nil))
+		window.title = "Fabric macOS"
+		window.setFrame(.init(origin: .zero, size: .init(width: Constants.initialWindowWidth, height: Constants.initialWindowHeight)), display: true)
+		window.center()
+		window.makeKeyAndOrderFront(nil)
+	}
 }
 
---
+++ OfficeUIFabricTestApp/TestHoverButtonViewController.swift
@@ -0,0 +1,16 @@
+//
+// Copyright Microsoft Corporation
+//
+
+import AppKit
+
+class TestHoverButtonViewController: NSViewController {
+	override func loadView() {		
+		let containerView = NSStackView(frame: .zero)
+		// TODO: Actually add a hover button
+		containerView.addView(NSButton(title: "Button", target: nil, action: nil), in: .center)
+		containerView.orientation = .vertical
+		containerView.distribution = .gravityAreas
+		view = containerView
+	}
+}
