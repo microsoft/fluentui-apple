36ee3046236f5841adabd8e961c373f97f2ecd6f
Mark Vitale <mavitale@microsoft.com>
2020-04-07 19:02:21 -0700

    Separate CI build logic into parallel jobs

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,73 +1,65 @@
 #!/bin/bash
 
-# Invoke xcodebuild
-#
-# \param $1 type of project (Project or Workspace)
-# \param $2 path to  to run against
-# \param $3 scheme to build
-# \param $4 configuration
-# \param $5 sdk
-# \param $6+ build commands
-function invoke_xcodebuild()
+# ci.sh
+# A local version of the ci.yml github action that fires all the jobs sequentially.
+# Useful for local validation, keep in sync with .github/workflows/ci.yml
+# Note: Execute this script from the root of the repository, such as `./scripts/ci.sh`
+
+source "scripts/xcodebuild_wrapper.sh"
+
+# Keep an exit code so that we can return a non-zero exit code if any build fails
+# while still running all builds each time.
+EXIT_CODE=0
+
+function handle_exit_code()
 {
-    /usr/bin/xcodebuild \
-        -"$1" "$2" \
-        -scheme "$3" \
-        -configuration "$4" \
-        -sdk "$5" \
-        "${@:6}" \
-        CODE_SIGNING_ALLOWED=NO
-    
-    return $?
+	if [ $? -ne 0 ]
+	then
+		echo "Previous command exited with non-zero exit code"
+		# Intentionally changing the global EXIT_CODE variable
+		EXIT_CODE=1
+	fi
 }
 
-# Run a macOS build and test with the specified configuration
-# 
-# \param $1 configuration
-function macos_build_test()
-{
-    invoke_xcodebuild project "macos/xcode/OfficeUIFabric.xcodeproj" OfficeUIFabricTestApp "$1" macosx build test
-}
-
-# Run an iOS simulator xcodebuild invocation with the specified scheme, configuration, and build commands
-#
-# \param $1 scheme
-# \param $2 configuration
-# \param $3+ build commands
-function ios_simulator()
-{
-    invoke_xcodebuild workspace "ios/OfficeUIFabric.xcworkspace" "$1" "$2" iphonesimulator "${@:3}" -destination "platform=iOS Simulator,name=iPhone 8"
-}
-
-# Run an iOS device xcodebuild invocation with the specified scheme and configuration
-#
-# \param $1 scheme
-# \param $2 configuration
-function ios_build_device()
-{
-    invoke_xcodebuild workspace "ios/OfficeUIFabric.xcworkspace" "$1" "$2" iphoneos build
-}
 
 echo "Building and Testing macOS Debug"
-macos_build_test Debug
+macos_build OfficeUIFabricTestApp Debug build test
+handle_exit_code
 
 echo "Building and Testing macOS Release"
-macos_build_test Release
+macos_build OfficeUIFabricTestApp Release build test
+handle_exit_code
 
 echo "Building and Testing iOS Debug Simulator"
-ios_simulator OfficeUIFabric Debug build test
+ios_simulator_build OfficeUIFabric Debug build test
+handle_exit_code
 
 echo "Building iOS Release Simulator"
-ios_simulator OfficeUIFabric Release build
+ios_simulator_build OfficeUIFabric Release build
+handle_exit_code
 
 echo "Building iOS Testapp Debug Simulator"
-ios_simulator Demo.Development Debug build
+ios_simulator_build Demo.Development Debug build
+handle_exit_code
 
 echo "Building iOS Testapp Release Simulator"
-ios_simulator Demo.Development Release build
+ios_simulator_build Demo.Development Release build
+handle_exit_code
 
 echo "Building iOS Testapp Debug Device"
-ios_build_device Demo.Development Debug
+ios_device_build Demo.Development Debug build
+handle_exit_code
 
 echo "Building iOS Testapp Release Device"
-ios_build_device Demo.Development Release
+ios_device_build Demo.Development Release build
+handle_exit_code
+
+# Check if any of our individual build steps failed
+if [ $EXIT_CODE -ne 0 ]
+then
+	echo "CI Build Failed, please check logs for failures"
+else
+	echo "CI Build Succeeded"
+fi
+
+exit $EXIT_CODE
---
+++ scripts/xcodebuild_wrapper.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Invoke xcodebuild
+#
+# Match Azure DevOps xcodebuild invocations by not codesigning on CI builds
+#
+# \param $1 type of project (Project or Workspace)
+# \param $2 path to  to run against
+# \param $3 scheme to build
+# \param $4 configuration
+# \param $5 sdk
+# \param $6+ build commands
+function invoke_xcodebuild()
+{
+    /usr/bin/xcodebuild \
+        -"$1" "$2" \
+        -scheme "$3" \
+        -configuration "$4" \
+        -sdk "$5" \
+        "${@:6}" \
+        CODE_SIGNING_ALLOWED=NO
+    
+    return $?
+}
+
+# Run an iOS simulator xcodebuild invocation with the specified scheme, configuration, and build commands
+#
+# \param $1 scheme
+# \param $2 configuration
+# \param $3+ build commands
+function ios_simulator_build()
+{
+    invoke_xcodebuild workspace "ios/OfficeUIFabric.xcworkspace" "$1" "$2" iphonesimulator "${@:3}" -destination "platform=iOS Simulator,name=iPhone 8"
+    return $?
+}
+
+# Run an iOS device xcodebuild invocation with the specified scheme, configuration, and build commands
+#
+# \param $1 scheme
+# \param $2 configuration
+# \param $3+ build commands
+function ios_device_build()
+{
+    invoke_xcodebuild workspace "ios/OfficeUIFabric.xcworkspace" "$1" "$2" iphoneos "${@:3}"
+    return $?
+}
+
+# Run a macOS build and test with the specified scheme, configuration, and build commands
+# 
+# \param $1 scheme
+# \param $2 configuration
+# \param $3+ build commands
+function macos_build()
+{
+    invoke_xcodebuild project "macos/xcode/OfficeUIFabric.xcodeproj" "$1" "$2" macosx "${@:3}"
+    return $?
+}
