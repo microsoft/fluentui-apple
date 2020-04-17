e443c1d844c3af5662e7a4a200030631532c6677
Mark Vitale <mavitale@microsoft.com>
2020-04-17 16:13:28 -0700

    Add nuspec and pack it up

--- scripts/prepare_for_nuget_pack.sh
+++ scripts/prepare_for_nuget_pack.sh
@@ -2,6 +2,8 @@
 
 # NuGet packing doesn't support symlinks, so zip up our frameworks first to preserve the symlinks
 
+# Make a directory if necessary, no-op if it already exists
+#
 # \param $1 the name of the folder to create
 function make_dir_if_necessary()
 {
@@ -13,9 +15,9 @@ function make_dir_if_necessary()
     fi
 }
 
+# Build up our output directory in Products/nuget
 PRODUCTS_DIR="DerivedData/Build/Products"
 NUGET_OUTPUT_DIR="$PRODUCTS_DIR/nuget"
-
 make_dir_if_necessary "$NUGET_OUTPUT_DIR"
 
 NUGET_OUTPUT_INCLUDE_DIR="$NUGET_OUTPUT_DIR/include"
@@ -24,11 +26,13 @@ make_dir_if_necessary "$NUGET_OUTPUT_INCLUDE_DIR"
 NUGET_OUTPUT_INCLUDE_DIR_IOS="$NUGET_OUTPUT_INCLUDE_DIR/ios"
 make_dir_if_necessary "$NUGET_OUTPUT_INCLUDE_DIR_IOS"
 
-# Handle generated headers
+# Copy a single generated header into our output directory in an includes folder. Pick the release device header since that's likely the most important target
 rsync -a "DerivedData/Build/Intermediates.noindex/FluentUI.build/Release-iphoneos/FluentUILib.build/DerivedSources/FluentUILib-Swift.h" "$NUGET_OUTPUT_INCLUDE_DIR_IOS/FluentUILib-Swift.h"
 
+# cd into the products directory to make copying all the output easier
 cd $PRODUCTS_DIR
 
+# Copy each platform. Note that we're omitting binary swiftmodule files in favor of swiftinterface files.
 make_dir_if_necessary "nuget/Debug-macosx"
 rsync -a --exclude '*.swiftsourceinfo' --exclude '*.swiftdoc' --exclude '*.swiftmodule' Debug/FluentUI.framework/ nuget/Debug-macosx/FluentUI.framework/
 
@@ -55,9 +59,10 @@ rsync -a Release-iphonesimulator/libFluentUILib.a nuget/Ship-iphonesimulator/
 rsync -a --exclude 'Project/' --exclude '*.swiftdoc' --exclude '*.swiftmodule' Release-iphonesimulator/FluentUILib.swiftmodule/ nuget/Ship-iphonesimulator/FluentUILib.swiftmodule/
 rsync -a Release-iphonesimulator/FluentUIResources-ios.bundle/ nuget/Ship-iphonesimulator/FluentUIResources-ios.bundle/
 
-
+# cd into our nuget folder to finally zip up our build output
 cd "nuget"
 
+# Zip the build output
 echo "Creating zip archive named BuildOutput.zip containing all the platform folders"
 zip --symlinks -r BuildOutput.zip Debug-macosx/ Ship-macosx/ Debug-iphoneos/ Ship-iphoneos/ Debug-iphonesimulator/ Ship-iphonesimulator/
 
