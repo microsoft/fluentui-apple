microsoft/fluentui-apple

commit c1e481f484f762bdfd56cad4fa34588f6e28cb96

Merged PR 407300: Fix accessibility regressions in MSDrawerPresentationController
Fix accessibility regressions in MSDrawerPresentationController

With recent changes in MSDrawerController animation improvements and rotation support, UIElements in horizontal drawer are no longer accessible by VoiceOver because presentedViewController.view isn't part of MSDrawerPresenationController's accessibilityContainer hierarchy. Even after removing accessibilityContainer, user doesn't seem to be able to access any items behind the drawer. Also, the backgroundView is usually the entire screen and if the accessibilityFrame is that large, it doesn't always outputs the best accessibility elements order. After contentView frame is set, recalculate visible portion dimmingView and set the rect as backgroundView.accessibilityFrame.

Tested:
Landscape/Portrait iPhone X, horizontal/Vertical Drawers with VoiceOver
![dismiss_after.PNG](https://onedrive.visualstudio.com/4dcbf0bc-c3cd-49c8-a7c3-ec1924691d9b/_apis/git/repositories/93ac71ee-b53a-4fc6-a8c4-d46a80d4ca39/pullRequests/407300/attachments/dismiss_after.PNG =200x) ![IMG_0019.PNG](https://onedrive.visualstudio.com/4dcbf0bc-c3cd-49c8-a7c3-ec1924691d9b/_apis/git/repositories/93ac71ee-b53a-4fc6-a8c4-d46a80d4ca39/pullRequests/407300/attachments/IMG_0019.PNG =200x)

OfficeUIFabric/Drawer/MSDrawerPresentationController.swift
@@ -37,16 +37,6 @@ class MSDrawerPresentationController: UIPresentationController {
         }
     }
 
-    // Workaround to get Voiceover to ignore the view behind the drawer.
-    // Setting accessibilityViewIsModal directly on the container does not work.
-    // Presented view requires a mask on its container to prevent sliding over the navigation bar or exposing shadow over it.
-    private lazy var accessibilityContainer: UIView = {
-        let view = UIView()
-        view.accessibilityViewIsModal = true
-        view.mask = UIView()
-        view.mask?.backgroundColor = .white
-        return view
-    }()
     // A transparent view, which if tapped will dismiss the dropdown
     private lazy var backgroundView: UIView = {
         let view = BackgroundView()
@@ -79,15 +69,11 @@ class MSDrawerPresentationController: UIPresentationController {
     // MARK: Presentation
 
     override func presentationTransitionWillBegin() {
-        containerView?.addSubview(accessibilityContainer)
-        accessibilityContainer.fitIntoSuperview()
-
-        accessibilityContainer.addSubview(backgroundView)
+        containerView?.addSubview(backgroundView)
         backgroundView.fitIntoSuperview()
         backgroundView.addSubview(dimmingView)
 
-        accessibilityContainer.addSubview(contentView)
-
+        containerView?.addSubview(contentView)
         if presentationDirection.isVertical && actualPresentationOffset == 0 {
             containerView?.addSubview(separator)
         }
@@ -115,15 +101,18 @@ class MSDrawerPresentationController: UIPresentationController {
 
     override func presentationTransitionDidEnd(_ completed: Bool) {
         if completed {
+            let focusElement: UIView?
             // Horizontally presented drawers must be inside containerView in order for device rotation animation to work correctly
             if presentationDirection.isHorizontal {
                 containerView?.addSubview(presentedViewController.view)
                 presentedViewController.view.frame = frameOfPresentedViewInContainerView
+                focusElement = containerView
+            } else {
+                focusElement = contentView
             }
-            UIAccessibility.post(notification: .screenChanged, argument: contentView)
+            UIAccessibility.post(notification: .screenChanged, argument: focusElement)
             UIAccessibility.post(notification: .announcement, argument: "Accessibility.Alert".localized)
         } else {
-            accessibilityContainer.removeFromSuperview()
             separator.removeFromSuperview()
             removePresentedViewMask()
             shadowView.owner = nil
@@ -144,7 +133,6 @@ class MSDrawerPresentationController: UIPresentationController {
 
     override func dismissalTransitionDidEnd(_ completed: Bool) {
         if completed {
-            accessibilityContainer.removeFromSuperview()
             separator.removeFromSuperview()
             removePresentedViewMask()
             shadowView.owner = nil
@@ -276,13 +264,28 @@ class MSDrawerPresentationController: UIPresentationController {
         if separator.superview != nil {
             separator.frame = frameForSeparator(in: contentView.frame, withThickness: separator.height)
         }
+        updateBackgroundAccessibilityFrame()
     }
 
     private func updateLayout() {
         dimmingView.frame = frameForDimmingView(in: backgroundView.bounds)
-        accessibilityContainer.mask?.frame = dimmingView.frame
-
         setContentViewFrame(frameForContentView())
+    }
+
+    private func updateBackgroundAccessibilityFrame() {
+        let bounds = contentView.frame
+        var margins: UIEdgeInsets = .zero
+        switch presentationDirection {
+        case .down:
+            margins.top = bounds.height
+        case .up:
+            margins.bottom = bounds.height
+        case .fromLeading:
+            margins.left = bounds.width
+        case .fromTrailing:
+            margins.right = bounds.width
+        }
+        backgroundView.accessibilityFrame = dimmingView.frame.inset(by: margins)
     }
 
     private func frameForDimmingView(in bounds: CGRect) -> CGRect {

commit 4cf480089029460bea46ade17d62f92ddbd804a9

Fix regression caused by commit c1e481f
Fix regression caused by commit c1e481f
add a white mask to MSDrawerPresentationController so that during presentation it won't slide over the navigation bar.

Tested:
MSDrawerController Demo > "Show from custom base"
Device testing: VoiceOver for left/right/top/bottom drawer with rotation.

OfficeUIFabric/Drawer/MSDrawerPresentationController.swift
@@ -69,15 +69,18 @@ class MSDrawerPresentationController: UIPresentationController {
     // MARK: Presentation
 
     override func presentationTransitionWillBegin() {
-        containerView?.addSubview(backgroundView)
-        backgroundView.fitIntoSuperview()
-        backgroundView.addSubview(dimmingView)
+        if let containerView = containerView {
+            containerView.addSubview(backgroundView)
+            backgroundView.fitIntoSuperview()
+            backgroundView.addSubview(dimmingView)
 
-        containerView?.addSubview(contentView)
-        if presentationDirection.isVertical && actualPresentationOffset == 0 {
-            containerView?.addSubview(separator)
+            containerView.addSubview(contentView)
+            // Clipping is added to prevent any animation bug sliding over the navigation bar
+            contentView.clipsToBounds = true
+            if presentationDirection.isVertical && actualPresentationOffset == 0 {
+                containerView.addSubview(separator)
+            }
         }
-
         updateLayout()
 
         contentView.addSubview(shadowView)