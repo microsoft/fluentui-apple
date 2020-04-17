07e169cf1664d93dd988998f59421033ca4a35e5
Mark Vitale <mavitale@microsoft.com>
2020-04-17 15:36:59 -0700

    Add all platforms to prepare_for_nuget_pack

--- scripts/prepare_for_nuget_pack.sh
+++ scripts/prepare_for_nuget_pack.sh
@@ -1,25 +1,64 @@
 #!/bin/bash
 
 # NuGet packing doesn't support symlinks, so zip up our frameworks first to preserve the symlinks
-cd "DerivedData/OfficeUIFabric/Build/Products/"
 
-if ! [ -d "Debug-macosx" ]; then
-    echo "Making Debug-macosx folder"
-    mkdir "Debug-macosx"
-else
-    echo "Debug-macosx folder already exists"
-fi
+# \param $1 the name of the folder to create
+function make_dir_if_necessary()
+{
+    if ! [ -d "$1" ]; then
+        echo "Making $1 folder"
+        mkdir "$1"
+    else
+        echo "$1 folder already exists"
+    fi
+}
 
-rsync -a Debug/OfficeUIFabric.framework/ Debug-macosx/OfficeUIFabric.framework/
+PRODUCTS_DIR="DerivedData/Build/Products"
+NUGET_OUTPUT_DIR="$PRODUCTS_DIR/nuget"
 
-if ! [ -d "Ship-macosx" ]; then
-    echo "Making Ship-macosx folder"   
-    mkdir "Ship-macosx"
-else
-    echo "Ship-macosx folder already exists"
-fi
+make_dir_if_necessary "$NUGET_OUTPUT_DIR"
 
-rsync -a Release/OfficeUIFabric.framework/ Ship-macosx/OfficeUIFabric.framework/
+NUGET_OUTPUT_INCLUDE_DIR="$NUGET_OUTPUT_DIR/include"
+make_dir_if_necessary "$NUGET_OUTPUT_INCLUDE_DIR"
 
-echo "Creating zip archive named BuildOutput.zip containing Debug-macosx and Ship-macosx"
-zip --symlinks -r BuildOutput.zip Debug-macosx/ Ship-macosx/
+NUGET_OUTPUT_INCLUDE_DIR_IOS="$NUGET_OUTPUT_INCLUDE_DIR/ios"
+make_dir_if_necessary "$NUGET_OUTPUT_INCLUDE_DIR_IOS"
+
+# Handle generated headers
+rsync -a "DerivedData/Build/Intermediates.noindex/FluentUI.build/Release-iphoneos/FluentUILib.build/DerivedSources/FluentUILib-Swift.h" "$NUGET_OUTPUT_INCLUDE_DIR_IOS/FluentUILib-Swift.h"
+
+cd $PRODUCTS_DIR
+
+make_dir_if_necessary "nuget/Debug-macosx"
+rsync -a --exclude '*.swiftsourceinfo' --exclude '*.swiftdoc' --exclude '*.swiftmodule' Debug/FluentUI.framework/ nuget/Debug-macosx/FluentUI.framework/
+
+make_dir_if_necessary "nuget/Ship-macosx"
+rsync -a --exclude '*.swiftsourceinfo' --exclude '*.swiftdoc' --exclude '*.swiftmodule' Release/FluentUI.framework/ nuget/Ship-macosx/FluentUI.framework/
+
+make_dir_if_necessary "nuget/Debug-iphoneos"
+rsync -a Debug-iphoneos/libFluentUILib.a nuget/Debug-iphoneos/
+rsync -a --exclude 'Project/' --exclude '*.swiftdoc' --exclude '*.swiftmodule' Debug-iphoneos/FluentUILib.swiftmodule/ nuget/Debug-iphoneos/FluentUILib.swiftmodule/
+rsync -a Debug-iphoneos/FluentUIResources-ios.bundle/ nuget/Debug-iphoneos/FluentUIResources-ios.bundle/
+
+make_dir_if_necessary "nuget/Ship-iphoneos"
+rsync -a Release-iphoneos/libFluentUILib.a nuget/Ship-iphoneos/
+rsync -a --exclude 'Project/' --exclude '*.swiftdoc' --exclude '*.swiftmodule' Release-iphoneos/FluentUILib.swiftmodule/ nuget/Ship-iphoneos/FluentUILib.swiftmodule/
+rsync -a Release-iphoneos/FluentUIResources-ios.bundle/ nuget/Ship-iphoneos/FluentUIResources-ios.bundle/
+
+make_dir_if_necessary "nuget/Debug-iphonesimulator"
+rsync -a Debug-iphonesimulator/libFluentUILib.a nuget/Debug-iphonesimulator/
+rsync -a --exclude 'Project/' --exclude '*.swiftdoc' --exclude '*.swiftmodule' Debug-iphonesimulator/FluentUILib.swiftmodule/ nuget/Debug-iphonesimulator/FluentUILib.swiftmodule/
+rsync -a Debug-iphonesimulator/FluentUIResources-ios.bundle/ nuget/Debug-iphonesimulator/FluentUIResources-ios.bundle/
+
+make_dir_if_necessary "nuget/Ship-iphonesimulator"
+rsync -a Release-iphonesimulator/libFluentUILib.a nuget/Ship-iphonesimulator/
+rsync -a --exclude 'Project/' --exclude '*.swiftdoc' --exclude '*.swiftmodule' Release-iphonesimulator/FluentUILib.swiftmodule/ nuget/Ship-iphonesimulator/FluentUILib.swiftmodule/
+rsync -a Release-iphonesimulator/FluentUIResources-ios.bundle/ nuget/Ship-iphonesimulator/FluentUIResources-ios.bundle/
+
+
+cd "nuget"
+
+echo "Creating zip archive named BuildOutput.zip containing all the platform folders"
+zip --symlinks -r BuildOutput.zip Debug-macosx/ Ship-macosx/ Debug-iphoneos/ Ship-iphoneos/ Debug-iphonesimulator/ Ship-iphonesimulator/
+
+exit $?
