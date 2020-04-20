394d837b6b5c249db64a09ea7ffe5823519dcba9
Mark Vitale <mavitale@microsoft.com>
2020-04-20 10:08:42 -0700

    Centralize Xcode version selection logic

---
+++ scripts/xcode_select_current_version.sh
@@ -0,0 +1,2 @@
+# Select the current officially supported version of Xcode with the naming convention used by GitHub Actions/Azure DevOps Build Pipelines
+sudo xcode-select --switch '/Applications/Xcode_11.2.1.app/Contents/Developer'
