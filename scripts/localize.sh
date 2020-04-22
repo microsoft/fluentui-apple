b437be082169392314cdea70a3a6f4f03fa97eca
Mark Vitale <mavitale@microsoft.com>
2020-04-22 14:13:14 -0700

    Add temporary localization script

---
+++ scripts/localize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+printf "Team ID: "
+read teamId
+
+printf "Alias: "
+read alias
+
+stty -echo
+printf "Password: "
+read password
+stty echo
+printf "\n"
+
+scripts/GetLocalizedFiles.sh -t $teamId -u -a $alias -p $password -f ios/FluentUI/Resources/Localization/en.lproj/Localizable.strings -r ios/FluentUI/Resources/Localization/Localizable.strings -o ios/FluentUI/Resources/Localization
