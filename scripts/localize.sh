83a9e6d43f4f65d178ebedbde95012d58d0dd677
Mark Vitale <mavitale@microsoft.com>
2020-04-25 11:28:12 -0700

    Add automatic PR action

--- scripts/localize.sh
+++ scripts/localize.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check if environment variables have been defined for our team ID, AAD App ID, and AAD App Secret for automated workflows before requesting them interactively
 if [ -z $TDBUILD_TEAM_ID ]; then
     printf "Team ID: "
     read TDBUILD_TEAM_ID
@@ -18,5 +19,8 @@ if [ -z $TDBUILD_AAD_APPLICATION_CLIENT_SECRET ]; then
     printf "\n"
 fi
 
+# Localize iOS resources
 scripts/GetLocalizedFiles.sh -t $TDBUILD_TEAM_ID -u -a $TDBUILD_AAD_APPLICATION_CLIENT_ID -p $TDBUILD_AAD_APPLICATION_CLIENT_SECRET -f ios/FluentUI/Resources/Localization/en.lproj -r ios/Localization/en.lproj -o ios/FluentUI/Resources/Localization
+
+# Localize macOS resources
 scripts/GetLocalizedFiles.sh -t $TDBUILD_TEAM_ID -u -a $TDBUILD_AAD_APPLICATION_CLIENT_ID -p $TDBUILD_AAD_APPLICATION_CLIENT_SECRET -f macos/FluentUIResources-macos/Strings/en.lproj -r macos/Localization/en.lproj -o macos/FluentUIResources-macos/Strings
