acd198f03eea1cae32820ff6b6fa15dedd846bd5
Mark Vitale <mavitale@microsoft.com>
2020-04-29 11:56:02 -0700

    Add a workflow to publish to CocoaPods on new tags

---
+++ scripts/publishCocoaPod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# A simple script to publish our pod to cocoapods.org. Run from the root of the repo.
+
+# Note: In CI/CD scenarios, expect the COCOAPODS_TRUNK_TOKEN env variable to be set which allows this to run automatically
+echo "pod trunk push MicrosoftFluentUI.podspec"
