42f74204c7fce56f34c6dfebd217a94e9a3511a5
Mark A Vitale <658243+markavitale@users.noreply.github.com>
2020-05-11 16:17:18 -0700

    [Xcode] Update Actions/Pipelines to Xcode 11.4.1 (#57)
    * Update to Xcode 11.4.1
    
    * Make nuget publish testable
    
    * Pack binary files

--- scripts/prepare_for_nuget_pack.sh
+++ scripts/prepare_for_nuget_pack.sh
@@ -15,32 +15,6 @@ function make_dir_if_necessary()
     fi
 }
 
-# Invoke rsync -am but exclude swiftsourceinfo and swiftdoc file types
-#
-# \param $@ additional arguments to rsync, see man rsync for more details
-function rsync_excluding_swiftsourceinfo_swiftdoc()
-{
-    rsync -am --exclude '*.swiftsourceinfo' --exclude '*.swiftdoc' "$@"
-}
-
-# Invoke rsync_excluding_swiftsourceinfo_swiftdoc but also exclude nested .swiftmodule files.
-# For use when rsync'ing an entire framework directory that contains a .swiftmodule folder
-#
-# \param $@ additional arguments to rsync, see man rsync for more details
-function rsync_excluding_binary_swift_module_files_framework()
-{
-    rsync_excluding_swiftsourceinfo_swiftdoc --exclude '*.swiftmodule/*.swiftmodule' "$@"
-}
-
-# Invoke rsync_excluding_swiftsourceinfo_swiftdoc but also exclude .swiftmodule files.
-# For use when rsync'ing an existing .swiftmodule folder
-#
-# \param $@ additional arguments to rsync, see man rsync for more details
-function rsync_excluding_binary_swift_module_files_swiftmodule_folder()
-{
-    rsync_excluding_swiftsourceinfo_swiftdoc --exclude '*.swiftmodule' "$@"
-}
-
 # Build up our output directory in Products/nuget
 PRODUCTS_DIR="DerivedData/Build/Products"
 NUGET_OUTPUT_DIR="$PRODUCTS_DIR/nuget"
@@ -65,29 +39,29 @@ cd $PRODUCTS_DIR
 
 # Copy each platform
 make_dir_if_necessary "nuget/Debug-macosx"
-rsync_excluding_binary_swift_module_files_framework Debug/FluentUI.framework/ nuget/Debug-macosx/FluentUI.framework/
+rsync -a Debug/FluentUI.framework/ nuget/Debug-macosx/FluentUI.framework/
 
 make_dir_if_necessary "nuget/Ship-macosx"
 rsync_excluding_binary_swift_module_files_framework Release/FluentUI.framework/ nuget/Ship-macosx/FluentUI.framework/
 
 make_dir_if_necessary "nuget/Debug-iphoneos"
 rsync -a Debug-iphoneos/libFluentUILib.a nuget/Debug-iphoneos/
-rsync_excluding_binary_swift_module_files_swiftmodule_folder Debug-iphoneos/FluentUILib.swiftmodule/ nuget/Debug-iphoneos/FluentUILib.swiftmodule/
+rsync -a Debug-iphoneos/FluentUILib.swiftmodule/ nuget/Debug-iphoneos/FluentUILib.swiftmodule/
 rsync -a Debug-iphoneos/FluentUIResources-ios.bundle/ nuget/Debug-iphoneos/FluentUIResources-ios.bundle/
 
 make_dir_if_necessary "nuget/Ship-iphoneos"
 rsync -a Release-iphoneos/libFluentUILib.a nuget/Ship-iphoneos/
-rsync_excluding_binary_swift_module_files_swiftmodule_folder Release-iphoneos/FluentUILib.swiftmodule/ nuget/Ship-iphoneos/FluentUILib.swiftmodule/
+rsync -a Release-iphoneos/FluentUILib.swiftmodule/ nuget/Ship-iphoneos/FluentUILib.swiftmodule/
 rsync -a Release-iphoneos/FluentUIResources-ios.bundle/ nuget/Ship-iphoneos/FluentUIResources-ios.bundle/
 
 make_dir_if_necessary "nuget/Debug-iphonesimulator"
 rsync -a Debug-iphonesimulator/libFluentUILib.a nuget/Debug-iphonesimulator/
-rsync_excluding_binary_swift_module_files_swiftmodule_folder Debug-iphonesimulator/FluentUILib.swiftmodule/ nuget/Debug-iphonesimulator/FluentUILib.swiftmodule/
+rsync -a Debug-iphonesimulator/FluentUILib.swiftmodule/ nuget/Debug-iphonesimulator/FluentUILib.swiftmodule/
 rsync -a Debug-iphonesimulator/FluentUIResources-ios.bundle/ nuget/Debug-iphonesimulator/FluentUIResources-ios.bundle/
 
 make_dir_if_necessary "nuget/Ship-iphonesimulator"
 rsync -a Release-iphonesimulator/libFluentUILib.a nuget/Ship-iphonesimulator/
-rsync_excluding_binary_swift_module_files_swiftmodule_folder Release-iphonesimulator/FluentUILib.swiftmodule/ nuget/Ship-iphonesimulator/FluentUILib.swiftmodule/
+rsync -a Release-iphonesimulator/FluentUILib.swiftmodule/ nuget/Ship-iphonesimulator/FluentUILib.swiftmodule/
 rsync -a Release-iphonesimulator/FluentUIResources-ios.bundle/ nuget/Ship-iphonesimulator/FluentUIResources-ios.bundle/
 
 # cd into our nuget folder to finally zip up our build output
