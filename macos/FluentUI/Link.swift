384ca449323dbaf5204ef934347f375988cde10b
Saad Najmi <saadnajmi2@gmail.com>
2020-05-21 03:58:33 -0700

    Addressed initial PR comments, added optional hover effect

--- macos/FluentUI/Link.swift
+++ macos/FluentUI/Link.swift
@@ -9,22 +9,20 @@ import AppKit
 @objc(MSFLink)
 open class Link : NSButton {
 	
-	/// Initialies a link with a content string that is displayed, and an underlying
+	/// Initializes a hyperlink with a content string that is displayed, and an underlying URL that opens when clicked
 	/// - Parameters:
-	///   - content: The visible text of the link that the user sees.
+	///   - title: The visible text of the link that the user sees.
 	///   - url: The URL that is opened when the link is clicked
 	@objc public init(title: String, url: NSURL) {
 		self.url = url
 		super.init(frame: .zero)
+		self.title = title
 		alignment = .natural
 		isBordered = false
 		target = self
-		action = #selector(linkClicked(_:))
+		action = #selector(linkClicked)
 		
-		let titleAttributes: [NSAttributedString.Key: Any] = [
-			.foregroundColor : NSColor.linkColor
-		]
-		self.attributedTitle = NSAttributedString(string: title, attributes: titleAttributes)
+		updateTitle()
 	}
 	
 	@available(*, unavailable)
@@ -33,9 +31,77 @@ open class Link : NSButton {
 	}
 	
 	/// The URL that is opened when the link is clicked
-	@objc public let url: NSURL
+	@objc public var url: NSURL
 	
-	@objc private func linkClicked(_ sender: NSButton) {
+	@objc public var showsUnderlineWhileMouseInside: Bool = false {
+		didSet {
+			guard oldValue != showsUnderlineWhileMouseInside else {
+				return
+			}
+			updateTitle()
+		}
+	}
+	
+	/// The text displayed on the control, stylized to look like a hyperlink
+	open override var title: String {
+		didSet {
+			guard oldValue != title else {
+				return
+			}
+			updateTitle()
+		}
+	}
+	
+	private var trackingArea: NSTrackingArea?
+	
+	override public func updateTrackingAreas() {
+		super.updateTrackingAreas()
+
+		// Remove existing trackingArea
+		if let trackingArea = trackingArea {
+			removeTrackingArea(trackingArea)
+			self.trackingArea = nil
+		}
+
+		// Create a new trackingArea
+		let opts: NSTrackingArea.Options = [.mouseEnteredAndExited, .activeAlways]
+		let trackingArea = NSTrackingArea(
+			rect: bounds,
+			options: opts,
+			owner: self,
+			userInfo: nil
+		)
+		addTrackingArea(trackingArea)
+		self.trackingArea = trackingArea
+	}
+	
+	open override func mouseEntered(with event: NSEvent) {
+		mouseEntered = true
+		updateTitle()
+	}
+
+	open override func mouseExited(with event: NSEvent) {
+		mouseEntered = false
+		updateTitle()
+	}
+
+	private var mouseEntered = false
+	
+	private func updateTitle() {
+		let titleAttributes = (showsUnderlineWhileMouseInside && mouseEntered) ? underlinedlinkAttributes : linkAttributes
+		self.attributedTitle = NSAttributedString(string: title, attributes: titleAttributes)
+	}
+	
+	@objc private func linkClicked() {
 		NSWorkspace.shared.open(url as URL)
 	}
 }
+
+fileprivate let linkAttributes: [NSAttributedString.Key : Any] = [
+	.foregroundColor : NSColor.linkColor
+]
+
+fileprivate let underlinedlinkAttributes: [NSAttributedString.Key : Any] = [
+	.foregroundColor : NSColor.linkColor,
+	.underlineStyle : NSUnderlineStyle.single.rawValue
+]
--- macos/FluentUITestApp/TestLinkViewController.swift
+++ macos/FluentUITestApp/TestLinkViewController.swift
@@ -7,9 +7,14 @@ import AppKit
 import FluentUI
 
 class TestLinkViewController: NSViewController {
-	override func loadView() {		
-		let containerView = NSStackView(frame: .zero)
-		containerView.addView(Link(title: "Bing", url: NSURL(string: "https://wwww.bing.com")!), in: .center)
+	override func loadView() {
+		let linkWithNoHover = Link(title: "Bing", url: NSURL(string: "https://wwww.bing.com")!)
+		
+		let linkWithHover = Link(title: "Bing with hover", url: NSURL(string: "https://wwww.bing.com")!)
+		linkWithHover.showsUnderlineWhileMouseInside = true
+		
+		let containerView = NSStackView(views: [linkWithNoHover, linkWithHover])
+		containerView.edgeInsets = NSEdgeInsets(top: 100, left: 100, bottom: 100, right: 100)
 		containerView.orientation = .vertical
 		containerView.distribution = .gravityAreas
 		view = containerView
