6e1b0c880df9c47bc5b276f36412d129ab4b0747
Mark Vitale <mavitale@microsoft.com>
2020-07-09 00:04:33 -0700

    Add an xcode-12 beta publish pipeline

--- scripts/generate_build_number.sh
+++ scripts/generate_build_number.sh
@@ -4,7 +4,12 @@
 BUILD_VERSION_SUFFIX=""
 BUILD_VERSION_STRING="0.0.0.1"
 
-if [ -n "$BUILD_SOURCEBRANCHNAME" ] && [ "$BUILD_SOURCEBRANCHNAME" != "master" ]; then
+if [ -n "$BUILD_NUMBER_SUFFIX_OVERRIDE" ]; then 
+    # Allow someone calling us to provide an override to always add a suffix to a given build number by providing an env variable
+    ADJUSTED_BUILD_NUMBER_SUFFIX_OVERRIDE="${BUILD_NUMBER_SUFFIX_OVERRIDE//[^a-z0-9A-Z]/}"
+    BUILD_VERSION_SUFFIX="-$ADJUSTED_BUILD_NUMBER_SUFFIX_OVERRIDE"
+elif [ -n "$BUILD_SOURCEBRANCHNAME" ] && [ "$BUILD_SOURCEBRANCHNAME" != "master" ]; then 
+    # Otherwise append the branch name unless we're the main branch for this repo
     ADJUSTED_SOURCEBRANCHNAME="${BUILD_SOURCEBRANCHNAME//[^a-z0-9A-Z]/}"
     BUILD_VERSION_SUFFIX="-$ADJUSTED_SOURCEBRANCHNAME"
 fi
--- scripts/xcode_select_current_version.sh
+++ scripts/xcode_select_current_version.sh
@@ -1,3 +1,10 @@
 # Select the current officially supported version of Xcode with the naming convention used by GitHub Actions/Azure DevOps Build Pipelines
-echo "Running command: sudo xcode-select --switch '/Applications/Xcode_11.4.1.app/Contents/Developer'"
-sudo xcode-select --switch '/Applications/Xcode_11.4.1.app/Contents/Developer'
+
+if [ -n "$XCODE_PATH_OVERRIDE" ]; then # If someone calls this with the XCODE_PATH_OVERRIDE variable set to a path to a developer dir, use it instead
+    XCODE_PATH="$XCODE_PATH_OVERRIDE"
+else
+    XCODE_PATH='/Applications/Xcode_11.4.1.app/Contents/Developer'
+fi
+
+echo "Running command: sudo xcode-select --switch $XCODE_PATH"
+sudo xcode-select --switch "$XCODE_PATH"
