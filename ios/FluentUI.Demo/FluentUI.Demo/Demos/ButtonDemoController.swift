3f730db73f41011bd0bee5ff10a29cbfce773701
Mathieu Kavalec <mathka@microsoft.com>
2020-07-27 16:10:01 -0700

    icon support for fluent buttons

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/ButtonDemoController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/ButtonDemoController.swift
@@ -22,14 +22,35 @@ class ButtonDemoController: DemoController {
             disabledButton.isEnabled = false
             disabledButton.setTitle("Button", for: .normal)
 
-            addRow(items: [button, disabledButton], itemSpacing: 20)
+            var buttons: [Button] = [button, disabledButton]
+
+            if let image = style.image {
+                let iconButton = Button(style: style)
+                iconButton.setTitle("Button", for: .normal)
+                iconButton.image = image
+
+                let disabledIconButton = Button(style: style)
+                disabledIconButton.isEnabled = false
+                disabledIconButton.setTitle("Button", for: .normal)
+                disabledIconButton.image = image
+
+                buttons.append(contentsOf: [iconButton, disabledIconButton])
+            }
+
+            addRow(items: buttons, itemSpacing: 20)
         }
 
         addTitle(text: "With multi-line title")
         let button = Button(style: .primaryFilled)
         button.setTitle("Longer Text Button", for: .normal)
         button.titleLabel?.numberOfLines = 0
-        addRow(items: [button])
+
+        let iconButton = Button(style: .primaryFilled)
+        iconButton.setTitle("Longer Text Button", for: .normal)
+        iconButton.titleLabel?.numberOfLines = 0
+        iconButton.image = ButtonStyle.primaryFilled.image
+
+        addRow(items: [button, iconButton])
 
         container.addArrangedSubview(UIView())
     }
@@ -48,6 +69,17 @@ extension ButtonStyle {
             return "Tertiary outline"
         case .borderless:
             return "Borderless"
+        }
+    }
+
+    var image: UIImage? {
+        switch self {
+        case .primaryFilled, .primaryOutline:
+            return UIImage(named: "Placeholder_24")!
+        case .secondaryOutline:
+            return UIImage(named: "Placeholder_20")!
+        case .tertiaryOutline, .borderless:
+            return nil
         }
     }
 }
