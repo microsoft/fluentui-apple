ab301c56dda18c4c61da3680a191da786ebf6166
Mathieu Kavalec <mathka@microsoft.com>
2020-08-26 23:46:28 -0700

    Top accessory view is not centered correctly. Change layout logic

--- ios/FluentUI/Navigation/NavigationBar.swift
+++ ios/FluentUI/Navigation/NavigationBar.swift
@@ -235,8 +235,7 @@ open class NavigationBar: UINavigationBar {
     private let contentStackView = ContentStackView() //used to contain the various custom UI Elements
     private let rightBarButtonItemsStackView = UIStackView()
     private let leftBarButtonItemsStackView = UIStackView()
-    private let trailingSpacerView = UIView() //defines the leading space between the left and right barbuttonitems stack
-    private let leadingSpacerView = UIView() //defines the trailing space between the left and right barbuttonitems stack
+    private let spacerView = UIView() //defines the leading space between the left and right barbuttonitems stack
     private var topAccessoryView: UIView?
     private var topAccessoryViewConstraints: [NSLayoutConstraint] = []
 
@@ -295,19 +294,11 @@ open class NavigationBar: UINavigationBar {
         titleView.setContentHuggingPriority(.required, for: .horizontal)
         titleView.setContentCompressionResistancePriority(.required, for: .horizontal)
 
-        //leadingSpacerView
-        contentStackView.addArrangedSubview(leadingSpacerView)
-        leadingSpacerView.backgroundColor = .clear
-        leadingSpacerView.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        leadingSpacerView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-
-        //trailingSpacerView
-        contentStackView.addArrangedSubview(trailingSpacerView)
-        trailingSpacerView.backgroundColor = .clear
-        trailingSpacerView.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        trailingSpacerView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-
-        trailingSpacerView.widthAnchor.constraint(equalTo: leadingSpacerView.widthAnchor).isActive = true
+        //spacerView
+        contentStackView.addArrangedSubview(spacerView)
+        spacerView.backgroundColor = .clear
+        spacerView.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        spacerView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
 
         //rightBarButtonItemsStackView: layout priorities are slightly lower to make sure titleView has the highest priority in horizontal spacing
         contentStackView.addArrangedSubview(rightBarButtonItemsStackView)
@@ -330,8 +321,7 @@ open class NavigationBar: UINavigationBar {
         self.topAccessoryView = navigationItem?.topAccessoryView
 
         if let topAccessoryView = self.topAccessoryView {
-            let insertionIndex = contentStackView.arrangedSubviews.firstIndex(of: leadingSpacerView)! + 1
-            contentStackView.insertArrangedSubview(topAccessoryView, at: insertionIndex)
+            contentStackView.addSubview(topAccessoryView)
 
             NSLayoutConstraint.deactivate(topAccessoryViewConstraints)
             topAccessoryViewConstraints.removeAll()
@@ -345,7 +335,9 @@ open class NavigationBar: UINavigationBar {
                 topAccessoryViewConstraints.append(contentsOf: [
                     widthConstraint,
                     topAccessoryView.widthAnchor.constraint(lessThanOrEqualToConstant: attributes.maxWidth),
-                    topAccessoryView.widthAnchor.constraint(greaterThanOrEqualToConstant: attributes.minWidth)
+                    topAccessoryView.widthAnchor.constraint(greaterThanOrEqualToConstant: attributes.minWidth),
+                    topAccessoryView.centerXAnchor.constraint(equalTo: contentStackView.centerXAnchor),
+                    topAccessoryView.centerYAnchor.constraint(equalTo: contentStackView.centerYAnchor)
                 ])
 
                 NSLayoutConstraint.activate(topAccessoryViewConstraints)
