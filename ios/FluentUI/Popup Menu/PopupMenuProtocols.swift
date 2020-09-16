92a481df785291a78a864aa18b70107c81643342
Anand Rajeswaran <araje@microsoft.com>
2020-09-16 12:44:37 -0700

    Update comment

--- ios/FluentUI/Popup Menu/PopupMenuProtocols.swift
+++ ios/FluentUI/Popup Menu/PopupMenuProtocols.swift
@@ -35,7 +35,7 @@ public protocol PopupMenuTemplateItem: AnyObject {
 /**
 `PopupMenuItemTemplateCell` represents a template cell protocol inside `PopupMenuController`.
  The built-in type is `PopupMenuItemCell`.
- You can use `UITableViewCell` conforms to this protocol for customization.
+ If you are making a custom object conform to this protocol, it must be a `UITableViewCell` for the Pop Up Menu to work properly
 */
 @objc(MSFPopupMenuItemTemplateCell)
 public protocol PopupMenuItemTemplateCell {
