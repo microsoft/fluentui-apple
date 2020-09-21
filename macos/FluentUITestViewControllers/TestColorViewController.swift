640dcd0e0e97c244535ae9d247ad2f8f968d27f9
chiuam <>
2020-09-21 12:38:40 -0400

    Show colors in tinted view

--- macos/FluentUITestViewControllers/TestColorViewController.swift
+++ macos/FluentUITestViewControllers/TestColorViewController.swift
@@ -9,38 +9,76 @@ import FluentUI
 class TestColorViewController: NSViewController {
 	override func loadView() {
 		let containerView = NSView()
-		var textViews = [NSView]()
-		
-		for color in Colors.Palette.allCases {
-			let textField = NSTextField(labelWithString: color.name)
-			textField.textColor = color.color
-			textViews.append(textField)
-		}
-		
-		let stackView = NSStackView(views: textViews)
-		stackView.translatesAutoresizingMaskIntoConstraints = false
-		stackView.orientation = .vertical
-		
 		let scrollView = NSScrollView()
 		scrollView.translatesAutoresizingMaskIntoConstraints = false
 		scrollView.hasVerticalScroller = true
 		containerView.addSubview(scrollView)
+		var colorViews = [NSView]()
+		var textViews = [NSView]()
+		
+		for color in Colors.Palette.allCases {
+			let colorView = ColorRectView(color: color.color)
+			colorView.invalidateIntrinsicContentSize()
+			colorViews.append(colorView)
+			let textView = NSTextField(labelWithString: color.name)
+			textViews.append(textView)
+		}
+		
+		let colorStackView = NSStackView(views: colorViews)
+		let textStackView = NSStackView(views: textViews)
+		colorStackView.translatesAutoresizingMaskIntoConstraints = false
+		colorStackView.orientation = .vertical
+		textStackView.translatesAutoresizingMaskIntoConstraints = false
+		textStackView.orientation = .vertical
+		textStackView.alignment = .leading
 		
 		let documentView = NSView()
 		documentView.translatesAutoresizingMaskIntoConstraints = false
 		scrollView.documentView = documentView
-		documentView.addSubview(stackView)
+		documentView.addSubview(colorStackView)
+		documentView.addSubview(textStackView)
 		
 		NSLayoutConstraint.activate([
 			containerView.heightAnchor.constraint(equalTo:scrollView.heightAnchor),
 			containerView.widthAnchor.constraint(equalTo:scrollView.widthAnchor),
-			stackView.topAnchor.constraint(equalTo: documentView.topAnchor),
-			stackView.leadingAnchor.constraint(equalTo: documentView.leadingAnchor),
-			stackView.trailingAnchor.constraint(equalTo: documentView.trailingAnchor),
-			stackView.bottomAnchor.constraint(equalTo: documentView.bottomAnchor),
-			stackView.widthAnchor.constraint(equalTo: documentView.widthAnchor)
+			colorStackView.topAnchor.constraint(equalTo: documentView.topAnchor),
+			colorStackView.leadingAnchor.constraint(equalTo: documentView.leadingAnchor, constant: 20.0),
+			colorStackView.trailingAnchor.constraint(equalTo: textStackView.leadingAnchor, constant: -20.0),
+			colorStackView.bottomAnchor.constraint(equalTo: documentView.bottomAnchor),
+			
+			textStackView.topAnchor.constraint(equalTo: documentView.topAnchor),
+			textStackView.leadingAnchor.constraint(equalTo: colorStackView.trailingAnchor),
+			textStackView.trailingAnchor.constraint(equalTo: documentView.trailingAnchor),
+			textStackView.bottomAnchor.constraint(equalTo: documentView.bottomAnchor)
 		])
 		
 		view = containerView
+	}
+}
+
+class ColorRectView: NSView {
+	
+	var color: NSColor = {
+		let color = NSColor()
+		return color
+	}()
+	
+	init(color: NSColor) {
+		super.init(frame: .zero)
+		self.color = color
+	}
+	override var intrinsicContentSize: CGSize {
+		return CGSize(width: 17, height: 17)
+	}
+	
+	@available(*, unavailable)
+	required public init?(coder decoder: NSCoder) {
+		preconditionFailure()
+	}
+	
+	override func draw(_ dirtyRect: NSRect) {
+		super.draw(dirtyRect)
+		color.setFill()
+		bounds.fill()
 	}
 }
