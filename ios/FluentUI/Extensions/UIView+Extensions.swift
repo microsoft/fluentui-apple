054863a6141d53641807191f64184f4d0f825e36
Rafael Amaral de Assis <rdeassis@microsoft.com>
2020-10-23 15:08:35 -0700

    no message

--- ios/FluentUI/Extensions/UIView+Extensions.swift
+++ ios/FluentUI/Extensions/UIView+Extensions.swift
@@ -200,16 +200,3 @@ extension UIView {
         NSLayoutConstraint.contain(view: view, in: self, withInsets: insets, respectingSafeAreaInsets: respectsSafeAreaInsets)
     }
 }
-
-// MARK: - Pointer Interaction APIs check
-
-extension UIView {
-    /// Some beta versions of iOS 13.4 and later don't include the pointer interaction APIs.
-    /// Checking the UIButton's isPointerInteractionEnabled property is a workaround that prevents a crash due to an
-    /// exception thrown while trying to add the UIPointerInteraction instance while running on those iOS versions:
-    /// "uncaught ObjC exception, reason: Invalid parameter not satisfying: interaction"
-    /// - Returns: true if Pointer Interaction APIs are available, otherwise false
-    func arePointerInteractionAPIsAvailable() -> Bool {
-        return UIButton.instancesRespond(to: Selector(("isPointerInteractionEnabled")))
-    }
-}
