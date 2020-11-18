aff8bff72ed0dbe39198dac040ee1ecdaca03f8c
Saad Najmi <saadnajmi2@gmail.com>
2020-11-18 13:12:19 -0800

    Rename iOS Static Library from "FluentUILib" to "FluentUI" (#317)

--- scripts/prepare_for_nuget_pack.sh
+++ scripts/prepare_for_nuget_pack.sh
@@ -36,7 +36,7 @@ make_dir_if_necessary "$NUGET_OUTPUT_INCLUDE_DIR_IOS_FLUENTUI"
 # Pick the release simulator header since the device target has some arm64 specific ifdefs while the simulator version works on all platforms.
 # Rename the FluentUILib-Swift.h to FluentUI-Swift.h for consistency with Framework consumption
 echo "Copy iOS generated Swift header to include dir"
-rsync -a "DerivedData/Build/Intermediates.noindex/FluentUI.build/Release-iphonesimulator/FluentUILib.build/DerivedSources/FluentUILib-Swift.h" "$NUGET_OUTPUT_INCLUDE_DIR_IOS_FLUENTUI/FluentUI-Swift.h"
+rsync -a "DerivedData/Build/Intermediates.noindex/FluentUI.build/Release-iphonesimulator/FluentUILib.build/DerivedSources/FluentUI-Swift.h" "$NUGET_OUTPUT_INCLUDE_DIR_IOS_FLUENTUI/FluentUI-Swift.h"
 
 # cd into the products directory to make copying all the output easier
 cd $PRODUCTS_DIR
@@ -52,26 +52,26 @@ rsync -a Release/FluentUI.framework/ nuget/Ship-macosx/FluentUI.framework/
 
 make_dir_if_necessary "nuget/Debug-iphoneos"
 echo "Copy Debug-iphoneos build output into nuget folder"
-rsync -a Debug-iphoneos/libFluentUILib.a nuget/Debug-iphoneos/
-rsync -a Debug-iphoneos/FluentUILib.swiftmodule/ nuget/Debug-iphoneos/FluentUILib.swiftmodule/
+rsync -a Debug-iphoneos/libFluentUI.a nuget/Debug-iphoneos/
+rsync -a Debug-iphoneos/FluentUI.swiftmodule/ nuget/Debug-iphoneos/FluentUI.swiftmodule/
 rsync -a Debug-iphoneos/FluentUIResources-ios.bundle/ nuget/Debug-iphoneos/FluentUIResources-ios.bundle/
 
 make_dir_if_necessary "nuget/Ship-iphoneos"
 echo "Copy Ship-iphoneos build output into nuget folder"
-rsync -a Release-iphoneos/libFluentUILib.a nuget/Ship-iphoneos/
-rsync -a Release-iphoneos/FluentUILib.swiftmodule/ nuget/Ship-iphoneos/FluentUILib.swiftmodule/
+rsync -a Release-iphoneos/libFluentUI.a nuget/Ship-iphoneos/
+rsync -a Release-iphoneos/FluentUI.swiftmodule/ nuget/Ship-iphoneos/FluentUI.swiftmodule/
 rsync -a Release-iphoneos/FluentUIResources-ios.bundle/ nuget/Ship-iphoneos/FluentUIResources-ios.bundle/
 
 make_dir_if_necessary "nuget/Debug-iphonesimulator"
 echo "Copy Debug-iphonesimulator build output into nuget folder"
-rsync -a Debug-iphonesimulator/libFluentUILib.a nuget/Debug-iphonesimulator/
-rsync -a Debug-iphonesimulator/FluentUILib.swiftmodule/ nuget/Debug-iphonesimulator/FluentUILib.swiftmodule/
+rsync -a Debug-iphonesimulator/libFluentUI.a nuget/Debug-iphonesimulator/
+rsync -a Debug-iphonesimulator/FluentUI.swiftmodule/ nuget/Debug-iphonesimulator/FluentUI.swiftmodule/
 rsync -a Debug-iphonesimulator/FluentUIResources-ios.bundle/ nuget/Debug-iphonesimulator/FluentUIResources-ios.bundle/
 
 make_dir_if_necessary "nuget/Ship-iphonesimulator"
 echo "Copy Ship-iphonesimulator build output into nuget folder"
-rsync -a Release-iphonesimulator/libFluentUILib.a nuget/Ship-iphonesimulator/
-rsync -a Release-iphonesimulator/FluentUILib.swiftmodule/ nuget/Ship-iphonesimulator/FluentUILib.swiftmodule/
+rsync -a Release-iphonesimulator/libFluentUI.a nuget/Ship-iphonesimulator/
+rsync -a Release-iphonesimulator/FluentUI.swiftmodule/ nuget/Ship-iphonesimulator/FluentUI.swiftmodule/
 rsync -a Release-iphonesimulator/FluentUIResources-ios.bundle/ nuget/Ship-iphonesimulator/FluentUIResources-ios.bundle/
 
 # cd into our nuget folder to finally zip up our build output
