714bd1afd4c7e58cf03ac3de3ebfcb361ffa94de
Chris Pearson <chripear@microsoft.com>
2020-12-10 14:15:57 -0800

    Updated Fluent Button designs for macOS (#327)
    * initial commit
    
    * Fix a comment, revert unnecessary changes
    
    * Remove redundant @available tags and deployment target
    
    * Make accentColor non-optional
    
    Also remove instances of the word "highlight" in comments, except one location where it is applicable in reference to system buttons.
    
    * Clear backgroundColor and borderColor if nil
    
    If backgroundColor and borderColor are set to nil, clear them from the layer when updating.
    
    * Clarify relationship between computed color property and state-specific colors
    
    Also fix a bracket style issue
    
    * Arrange size constants into separate struct
    
    * Arrange Button.swift with "important" stuff at top
    
    * Set needsDisplay in a few more places
    
    * Make button colors private except accentColor
    
    * Fix borderless contentTintColorPressed
    
    This was supposed to be .pressed, not .disabled.  Changing it to .deepPressed because .pressed is hard to see in the smaller area of the title (compared to the easy-to-see changes in background color)
    
    * Avoid setting contentTintColor in updateLayer
    
    * Switch some 'if let' to 'guard let'
    
    * Ensure Xcode records all color values as hex
    
    * Make ButtonFormat components public
    
    * Invalidate intrinsic content size when 'size' property changes
    
    * Docs

--- macos/FluentUITestViewControllers/TestButtonViewController.swift
+++ macos/FluentUITestViewControllers/TestButtonViewController.swift
@@ -6,134 +6,308 @@
 import AppKit
 import FluentUI
 
+let effectViewMaterials: [String: NSVisualEffectView.Material] = [
+	"titlebar": .titlebar,
+	"selection": .selection,
+	"menu": .menu,
+	"popover": .popover,
+	"sidebar": .sidebar,
+	"headerView": .headerView,
+	"sheet": .sheet,
+	"windowBackground": .windowBackground,
+	"hudWindow": .hudWindow,
+	"fullScreenUI": .fullScreenUI,
+	"toolTip": .toolTip,
+	"contentBackground": .contentBackground,
+	"underWindowBackground": .underWindowBackground,
+	"underPageBackground": .underPageBackground,
+]
 
-class TestButtonViewController: NSViewController {
+let backgroundColors: [String: NSColor] = [
+	"textBackgroundColor": .textBackgroundColor,
+	"selectedTextBackgroundColor": .selectedTextBackgroundColor,
+	"unemphasizedSelectedTextBackgroundColor": .unemphasizedSelectedTextBackgroundColor,
+	"windowBackgroundColor": .windowBackgroundColor,
+	"controlBackgroundColor": .controlBackgroundColor,
+	"underPageBackgroundColor": .underPageBackgroundColor,
+	"selectedContentBackgroundColor": .selectedContentBackgroundColor,
+	"unemphasizedSelectedContentBackgroundColor": .unemphasizedSelectedContentBackgroundColor,
+]
 
-	override func loadView() {
-		let columnLabels: [NSView] = [
-			NSTextField(labelWithString: "Primary Filled"),
-			NSTextField(labelWithString: "Primary Outline"),
-			NSTextField(labelWithString: "Borderless"),
-			NSTextField(labelWithString: "Custom Button"),
-		]
-		
-		let customButton = Button(title: "Custom Button")
-		customButton.restBackgroundColor = Colors.Palette.green20.color
-		customButton.contentTintColor = .white
-		
-		let buttonsWithTitle: () -> [NSButton] = {
-			return [
-				Button(title: "FluentUI Button", style: .primaryFilled),
-				Button(title: "FluentUI Button", style: .primaryOutline),
-				Button(title: "FluentUI Button", style: .borderless),
-				customButton,
-			]
-		}
+let buttonStates: [String] = [
+	"rest",
+	"pressed",
+	"disabled",
+]
 
-		let customButtonWithImage = Button()
-		customButtonWithImage.restBackgroundColor = Colors.Palette.communicationBlue.color
-		customButtonWithImage.image = NSImage(named: NSImage.stopProgressTemplateName)!
-		customButtonWithImage.contentTintColor = .white
-		
-		let buttonsWithImage: () -> [NSButton] = {
-			return [
-				Button(image: NSImage(named: NSImage.stopProgressTemplateName)!, style: .primaryFilled),
-				Button(image: NSImage(named: NSImage.stopProgressTemplateName)!, style: .primaryOutline),
-				Button(image: NSImage(named: NSImage.stopProgressTemplateName)!, style: .borderless),
-				customButtonWithImage,
-			]
-		}
-		
-		let leadingArrowImage = NSImage(named: TestButtonViewController.leadingArrow)!
-		let trailingArrowImage = NSImage(named: TestButtonViewController.trailingArrow)!
-		
-		let customButtonWithTitleAndImage = Button()
-		customButtonWithTitleAndImage.title = "BtnTitle+Img"
-		customButtonWithTitleAndImage.contentTintColor = .white
-		customButtonWithTitleAndImage.image = trailingArrowImage
-		customButtonWithTitleAndImage.imagePosition = .imageTrailing
-		customButtonWithTitleAndImage.restBackgroundColor = Colors.Palette.blueMagenta30.color
-		
-		let buttonsWithTitleAndImage: () -> [NSButton] = {
-			return [
-				Button(title: "Back", image: leadingArrowImage, imagePosition: .imageLeading, style: .primaryFilled),
-				Button(title: "Skip", image: trailingArrowImage, imagePosition: .imageTrailing, style: .primaryOutline),
-				Button(title: "Back", image: leadingArrowImage, imagePosition: .imageLeading, style: .borderless),
-				customButtonWithTitleAndImage,
-			]
-		}
-		
-		let customDisabledButton = Button()
-		customDisabledButton.title = "DisabledWithImg"
-		customDisabledButton.contentTintColor = .white
-		customDisabledButton.restBackgroundColor = Colors.Palette.blueMagenta30.color
-		customDisabledButton.image = leadingArrowImage
-		customDisabledButton.imagePosition = .imageLeading
-		customDisabledButton.isEnabled = false
-		
-		let disabledPrimaryFilled = Button(title: "PrimaryFilled disbaled", style: .primaryFilled)
-		let disabledPrimaryOutline = Button(title: "PrimaryOutline disabled", style: .primaryOutline)
-		let disabledBorderless = Button(title: "Borderless disabled", style: .borderless)
-		disabledPrimaryFilled.isEnabled = false
-		disabledPrimaryOutline.isEnabled = false
-		disabledBorderless.isEnabled = false
-		
-		let disabledButtons: () -> [NSButton] = {
-			return [
-				disabledPrimaryFilled,
-				disabledPrimaryOutline,
-				disabledBorderless,
-				customDisabledButton,
-			]
-		}
-
-		let gridView = NSGridView(frame: .zero)
-		gridView.rowSpacing = TestButtonViewController.gridViewRowSpacing
-		gridView.columnSpacing = TestButtonViewController.gridViewColumnSpacing
-		gridView.setContentHuggingPriority(.defaultHigh, for: .vertical)
-		
-		gridView.addColumn(with: columnLabels)
-		gridView.addColumn(with: buttonsWithTitle())
-		gridView.addColumn(with: buttonsWithImage())
-		gridView.addColumn(with: buttonsWithTitleAndImage())
-		gridView.addColumn(with: disabledButtons())
-
-		let emptyCell = NSGridCell.emptyContentView
-	
-		// Insert the Row Titles as the last step
-		let rowLabels: [NSView] = [
-			emptyCell,
-			NSTextField(labelWithString: "Title"),
-			NSTextField(labelWithString: "Image"),
-			NSTextField(labelWithString: "Title & Image"),
-			NSTextField(labelWithString: "Title & Image (Disabled)")
-		]
-		gridView.insertRow(at: 0, with: rowLabels)
-		gridView.addRow(with: [])	//Padding
-
-		let containerView = NSStackView()
-		containerView.orientation = .vertical
-		containerView.translatesAutoresizingMaskIntoConstraints = false
-		containerView.edgeInsets = NSEdgeInsets(
-			top: TestButtonViewController.containerViewEdgeInsets,
-			left: TestButtonViewController.containerViewEdgeInsets,
-			bottom: TestButtonViewController.containerViewEdgeInsets,
-			right: TestButtonViewController.containerViewEdgeInsets
-		)
-
-		containerView.addView(gridView, in: .top)
-
-		view = containerView
+class VibrantScrollView: NSScrollView {
+	override var allowsVibrancy: Bool {
+		// Allow view to pick up effect of NSVisualEffectView material
+		return true
 	}
-	
-	private static let leadingArrow = "ic_fluent_chevron_left_16_filled"
-	private static let trailingArrow = "ic_fluent_chevron_right_16_filled"
-	
-	private static let gridViewRowSpacing: CGFloat = 20
-	private static let gridViewColumnSpacing: CGFloat = 20
-	private static let containerViewEdgeInsets: CGFloat = 20
-
 }
 
-// MARK: - Constants
+class TopClipView: NSClipView {
+	override func constrainBoundsRect(_ proposedBounds: NSRect) -> NSRect {
+		var bounds = super.constrainBoundsRect(proposedBounds)
 
+		// Place content smaller than the scroll area at top left rather than the default bottom left
+		if let height = documentView?.frame.height {
+			if height < proposedBounds.height {
+				bounds.origin.y = height - proposedBounds.height
+			}
+		}
+
+		return bounds
+	}
+}
+
+class TestButtonViewController: NSViewController, NSMenuDelegate {
+	let materialsPopup = NSPopUpButton(frame: NSZeroRect, pullsDown: false)
+	let backgroundColorsPopup = NSPopUpButton(frame: NSZeroRect, pullsDown: false)
+	let buttonStatesPopup = NSPopUpButton(frame: NSZeroRect, pullsDown: false)
+	let scrollView = VibrantScrollView()
+	let materialPane = NSVisualEffectView()
+	var fluentButtons: [Button] = []
+
+	override func loadView() {
+		materialsPopup.addItems(withTitles: effectViewMaterials.keys.sorted())
+		materialsPopup.selectItem(withTitle: "contentBackground")
+		materialsPopup.menu?.delegate = self
+		materialsPopup.target = self
+		materialsPopup.action = #selector(TestButtonViewController.materialChanged)
+
+		backgroundColorsPopup.addItems(withTitles: backgroundColors.keys.sorted())
+		backgroundColorsPopup.selectItem(withTitle: "controlBackgroundColor")
+		backgroundColorsPopup.menu?.delegate = self
+		backgroundColorsPopup.target = self
+		backgroundColorsPopup.action = #selector(TestButtonViewController.backgroundColorChanged)
+
+		buttonStatesPopup.addItems(withTitles: buttonStates)
+		buttonStatesPopup.selectItem(withTitle: "rest")
+		buttonStatesPopup.menu?.delegate = self
+		buttonStatesPopup.target = self
+		buttonStatesPopup.action = #selector(TestButtonViewController.stateChanged)
+
+		let tools = [
+			[NSTextField(labelWithString: "Pane Material:"), materialsPopup],
+			[NSTextField(labelWithString: "Pane Color:"), backgroundColorsPopup],
+			[NSTextField(labelWithString: "Button State:"), buttonStatesPopup],
+		]
+
+		let toolsGrid = NSGridView(views: tools)
+		toolsGrid.translatesAutoresizingMaskIntoConstraints = false
+		toolsGrid.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+		toolsGrid.setContentHuggingPriority(.defaultHigh, for: .vertical)
+
+		// Pad left and right sides using the row/column spacing
+		toolsGrid.insertColumn(at: 0, with: [])
+		toolsGrid.addColumn(with: [])
+
+		// Load Excel app color as primary to distinguish .primary and .communicationBlue accentColors
+		Colors.primary = (NSColor(named: "Colors/DemoPrimaryColor"))!
+		let communicationBlue = Colors.Palette.communicationBlue.color
+
+		// ButtonFormats: each will apply to a whole row of sample controls
+		let largePrimary = ButtonFormat(size: .large, style: .primary, accentColor: communicationBlue)
+		let largeSecondary = ButtonFormat(size: .large, style: .secondary, accentColor: communicationBlue)
+		let largeAcrylic = ButtonFormat(size: .large, style: .acrylic, accentColor: communicationBlue)
+		let largeBorderless = ButtonFormat(size: .large, style: .borderless, accentColor: communicationBlue)
+		let largeCustom = ButtonFormat(size: .large, style: .primary, accentColor: Colors.Palette.blueMagenta30.color)
+		let smallPrimary = ButtonFormat(size: .small, style: .primary, accentColor: Colors.primary)
+		let smallSecondary = ButtonFormat(size: .small, style: .secondary, accentColor: Colors.primary)
+		let smallAcrylic = ButtonFormat(size: .small, style: .acrylic, accentColor: Colors.primary)
+		let smallBorderless = ButtonFormat(size: .small, style: .borderless, accentColor: Colors.primary)
+		let smallCustom = ButtonFormat(size: .small, style: .primary, accentColor: Colors.Palette.blueMagenta30.color)
+
+		let formats: [ButtonFormat] = [
+			largePrimary,
+			largeSecondary,
+			largeAcrylic,
+			largeBorderless,
+			largeCustom,
+			smallPrimary,
+			smallSecondary,
+			smallAcrylic,
+			smallBorderless,
+			smallCustom,
+		]
+
+		let rowLabels: [NSView] = [
+			NSTextField(labelWithString: "Large Primary"),
+			NSTextField(labelWithString: "Large Secondary"),
+			NSTextField(labelWithString: "Large Acrylic"),
+			NSTextField(labelWithString: "Large Borderless"),
+			NSTextField(labelWithString: "Large Custom"),
+			NSTextField(labelWithString: "Small Primary"),
+			NSTextField(labelWithString: "Small Secondary"),
+			NSTextField(labelWithString: "Small Acrylic"),
+			NSTextField(labelWithString: "Small Borderless"),
+			NSTextField(labelWithString: "Small Custom"),
+		]
+
+		let buttonsWithTitle: () -> [Button] = {
+			return formats.map({ Button(title: "FluentUI Button", format: $0) })
+		}
+
+		let stopImage = NSImage(named: NSImage.stopProgressTemplateName)!
+
+		let buttonsWithImage: () -> [Button] = {
+			return formats.map({ Button(image: stopImage, format: $0) })
+		}
+
+		let leadingArrowImage = NSImage(named: TestButtonViewController.leadingArrow)!
+		let trailingArrowImage = NSImage(named: TestButtonViewController.trailingArrow)!
+
+		let buttonsWithTitleAndImageHorizontal: () -> [Button] = {
+			return [
+				Button(title: "Back", image: leadingArrowImage, imagePosition: .imageLeading, format: largePrimary),
+				Button(title: "Skip", image: trailingArrowImage, imagePosition: .imageTrailing, format: largeSecondary),
+				Button(title: "Back", image: leadingArrowImage, imagePosition: .imageLeft, format: largeAcrylic),
+				Button(title: "Skip", image: trailingArrowImage, imagePosition: .imageRight, format: largeBorderless),
+				Button(title: "Back", image: leadingArrowImage, imagePosition: .imageLeft, format: largeCustom),
+				Button(title: "Skip", image: trailingArrowImage, imagePosition: .imageTrailing, format: smallPrimary),
+				Button(title: "Back", image: leadingArrowImage, imagePosition: .imageLeading, format: smallSecondary),
+				Button(title: "Skip", image: trailingArrowImage, imagePosition: .imageRight, format: smallAcrylic),
+				Button(title: "Back", image: leadingArrowImage, imagePosition: .imageLeft, format: smallBorderless),
+				Button(title: "Skip", image: trailingArrowImage, imagePosition: .imageRight, format: smallCustom),
+			]
+		}
+
+		let buttonsWithTitleAndImageVertical: () -> [Button] = {
+			return [
+				Button(title: "Nope", image: stopImage, imagePosition: .imageAbove, format: largePrimary),
+				Button(title: "Nope", image: stopImage, imagePosition: .imageBelow, format: largeSecondary),
+				Button(title: "Nope", image: stopImage, imagePosition: .imageOverlaps, format: largeAcrylic),
+				Button(title: "Nope", image: stopImage, imagePosition: .imageAbove, format: largeBorderless),
+				Button(title: "Nope", image: stopImage, imagePosition: .imageBelow, format: largeCustom),
+				Button(title: "Nope", image: stopImage, imagePosition: .imageOverlaps, format: smallPrimary),
+				Button(title: "Nope", image: stopImage, imagePosition: .imageAbove, format: smallSecondary),
+				Button(title: "Nope", image: stopImage, imagePosition: .imageBelow, format: smallAcrylic),
+				Button(title: "Nope", image: stopImage, imagePosition: .imageOverlaps, format: smallBorderless),
+				Button(title: "Nope", image: stopImage, imagePosition: .imageAbove, format: smallCustom),
+			]
+		}
+
+		let fluentButtonsGrid = NSGridView(frame: .zero)
+		fluentButtonsGrid.translatesAutoresizingMaskIntoConstraints = false
+		fluentButtonsGrid.rowSpacing = TestButtonViewController.gridViewRowSpacing
+		fluentButtonsGrid.columnSpacing = TestButtonViewController.gridViewColumnSpacing
+
+		fluentButtonsGrid.addColumn(with: rowLabels)
+		let buttonColumns = [
+			buttonsWithTitle(),
+			buttonsWithImage(),
+			buttonsWithTitleAndImageHorizontal(),
+			buttonsWithTitleAndImageVertical(),
+		]
+		for newColumn in buttonColumns {
+			var nearestPrimary: Button? = nil
+			for button in newColumn {
+				switch button.style {
+				case .primary:
+					nearestPrimary = button
+				default:
+					button.linkedPrimary = nearestPrimary
+				}
+			}
+			fluentButtons.append(contentsOf: newColumn)
+			fluentButtonsGrid.addColumn(with: newColumn)
+		}
+
+		// Insert column headers atop the grid of sample controls
+		let columnLabels: [NSView] = [
+			NSGridCell.emptyContentView,
+			NSTextField(labelWithString: "Title"),
+			NSTextField(labelWithString: "Image"),
+			NSTextField(labelWithString: "Title & Image Horizontal"),
+			NSTextField(labelWithString: "Title & Image Vertical")
+		]
+		fluentButtonsGrid.insertRow(at: 0, with: columnLabels)
+
+		// Pad around the edges using the row/column spacing
+		fluentButtonsGrid.insertRow(at: 0, with: [])
+		fluentButtonsGrid.addRow(with: [])
+		fluentButtonsGrid.insertColumn(at: 0, with: [])
+		fluentButtonsGrid.addColumn(with: [])
+
+		scrollView.translatesAutoresizingMaskIntoConstraints = false
+		scrollView.hasVerticalScroller = true
+		scrollView.hasHorizontalScroller = true
+		scrollView.contentView = TopClipView.init(frame: scrollView.contentView.frame)
+		scrollView.documentView = fluentButtonsGrid
+		if let documentView = scrollView.documentView {
+				documentView.scroll(NSPoint(x: 0, y: documentView.bounds.size.height))
+		}
+
+		materialPane.translatesAutoresizingMaskIntoConstraints = false
+		materialPane.material = .contentBackground
+		materialPane.addSubview(scrollView)
+
+		let mainPanel = NSStackView(views: [toolsGrid, materialPane])
+		mainPanel.translatesAutoresizingMaskIntoConstraints = false
+		mainPanel.orientation = .vertical
+		mainPanel.alignment = .left
+		mainPanel.edgeInsets = NSEdgeInsets(top: mainPanel.spacing, left: 0, bottom: 0, right: 0)
+
+		NSLayoutConstraint.activate([
+			materialPane.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+			materialPane.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+			materialPane.topAnchor.constraint(equalTo: scrollView.topAnchor),
+			materialPane.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+			fluentButtonsGrid.bottomAnchor.constraint(equalTo: scrollView.contentView.bottomAnchor),
+			fluentButtonsGrid.leadingAnchor.constraint(equalTo: scrollView.contentView.leadingAnchor),
+		])
+
+		view = mainPanel
+	}
+
+	override func viewWillAppear() {
+		// Scroll to top [left]
+		if let documentView = scrollView.documentView {
+			documentView.scroll(NSPoint(x: 0, y: documentView.bounds.size.height))
+		}
+	}
+
+	@objc func materialChanged() {
+		if let title = materialsPopup.titleOfSelectedItem {
+			if let material = effectViewMaterials[title] {
+				materialPane.material = material
+				materialPane.needsDisplay = true
+			}
+		}
+	}
+
+	@objc func backgroundColorChanged() {
+		let color = backgroundColors[backgroundColorsPopup.titleOfSelectedItem!]!
+		scrollView.backgroundColor = color
+	}
+
+	@objc func stateChanged() {
+		let state = buttonStatesPopup.titleOfSelectedItem
+		let emptyEvent = NSEvent()
+		for button in fluentButtons {
+			let originalLinkedPrimary = button.linkedPrimary
+			button.linkedPrimary = nil
+			switch state {
+			case "rest":
+				button.isEnabled = true
+				button.mouseUp(with: emptyEvent)
+			case "pressed":
+				button.isEnabled = true
+				button.mouseDown(with: emptyEvent)
+			case "disabled":
+				button.isEnabled = false
+			default:
+				break
+			}
+			button.linkedPrimary = originalLinkedPrimary
+		}
+	}
+
+	private static let leadingArrow = "ic_fluent_chevron_left_16_filled"
+	private static let trailingArrow = "ic_fluent_chevron_right_16_filled"
+
+	private static let gridViewRowSpacing: CGFloat = 20
+	private static let gridViewColumnSpacing: CGFloat = 20
+}
