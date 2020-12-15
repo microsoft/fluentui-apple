b6f43e8d12c487859ffb610f4b8ac89249579031
Zhuoran Tan <t@zhuorantan.com>
2020-12-15 17:31:34 +0800

    Refactor demo

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/CommandBarDemoController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/CommandBarDemoController.swift
@@ -7,6 +7,82 @@ import FluentUI
 import UIKit
 
 class CommandBarDemoController: DemoController {
+    enum Command {
+        case add
+        case mention
+        case calendar
+
+        case textBold
+        case textItalic
+        case textUnderline
+        case textStrikethrough
+
+        case arrowUndo
+        case arrowRedo
+
+        case copy
+        case delete
+
+        case checklist
+        case bulletList
+        case numberList
+        case link
+
+        var iconImage: UIImage? {
+            switch self {
+            case .add:
+                return UIImage(named: "Add_24_Regular")
+            case .mention:
+                return UIImage(named: "Mention_24_Regular")
+            case .calendar:
+                return UIImage(named: "Calendar_24_Regular")
+            case .textBold:
+                return UIImage(named: "Text Bold_24_Regular")
+            case .textItalic:
+                return UIImage(named: "Text Italic_24_Regular")
+            case .textUnderline:
+                return UIImage(named: "Text Underline_24_Regular")
+            case .textStrikethrough:
+                return UIImage(named: "Text Strikethrough_24_Regular")
+            case .arrowUndo:
+                return UIImage(named: "Arrow Undo_24_Regular")
+            case .arrowRedo:
+                return UIImage(named: "Arrow Redo_24_Filled")
+            case .copy:
+                return UIImage(named: "Copy_24_Regular")
+            case .delete:
+                return UIImage(named: "Delete_24_Regular")
+            case .checklist:
+                return UIImage(named: "Text Checklist List LTR_24_Regular")
+            case .bulletList:
+                return UIImage(named: "Text Bullet List_24_Regular")
+            case .numberList:
+                return UIImage(named: "Text Number List LTR_24_Regular")
+            case .link:
+                return UIImage(named: "Link_24_Regular")
+            }
+        }
+
+        var isPersistSelection: Bool {
+            switch self {
+            case .arrowUndo, .arrowRedo:
+                return false
+            default:
+                return true
+            }
+        }
+    }
+
+    class Item: CommandBarItem {
+        let command: Command
+
+        init(command: Command) {
+            self.command = command
+
+            super.init(iconImage: command.iconImage, isPersistSelection: command.isPersistSelection)
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         container.layoutMargins.right = 0
@@ -14,29 +90,29 @@ class CommandBarDemoController: DemoController {
 
         let itemGroups: [CommandBarItemGroup] = [
             [
-                CommandBarItem(iconImage: UIImage(named: "Add_24_Regular")),
-                CommandBarItem(iconImage: UIImage(named: "Mention_24_Regular")),
-                CommandBarItem(iconImage: UIImage(named: "Calendar_24_Regular"))
+                Item(command: .add),
+                Item(command: .mention),
+                Item(command: .calendar)
             ],
             [
-                CommandBarItem(iconImage: UIImage(named: "Text Bold_24_Regular")),
-                CommandBarItem(iconImage: UIImage(named: "Text Italic_24_Regular")),
-                CommandBarItem(iconImage: UIImage(named: "Text Underline_24_Regular")),
-                CommandBarItem(iconImage: UIImage(named: "Text Strikethrough_24_Regular"))
+                Item(command: .textBold),
+                Item(command: .textItalic),
+                Item(command: .textUnderline),
+                Item(command: .textStrikethrough)
             ],
             [
-                CommandBarItem(iconImage: UIImage(named: "Arrow Undo_24_Regular"), isPersistSelection: false),
-                CommandBarItem(iconImage: UIImage(named: "Arrow Redo_24_Filled"), isPersistSelection: false)
+                Item(command: .arrowUndo),
+                Item(command: .arrowRedo)
             ],
             [
-                CommandBarItem(iconImage: UIImage(named: "Copy_24_Regular")),
-                CommandBarItem(iconImage: UIImage(named: "Delete_24_Regular"))
+                Item(command: .copy),
+                Item(command: .delete)
             ],
             [
-                CommandBarItem(iconImage: UIImage(named: "Text Checklist List LTR_24_Regular")),
-                CommandBarItem(iconImage: UIImage(named: "Text Bullet List_24_Regular")),
-                CommandBarItem(iconImage: UIImage(named: "Text Number List LTR_24_Regular")),
-                CommandBarItem(iconImage: UIImage(named: "Link_24_Regular"))
+                Item(command: .checklist),
+                Item(command: .bulletList),
+                Item(command: .numberList),
+                Item(command: .link)
             ]
         ]
 
@@ -49,6 +125,6 @@ class CommandBarDemoController: DemoController {
 
 extension CommandBarDemoController: CommandBarDelegate {
     func commandBar(_ commandBar: CommandBar, didSelectItem item: CommandBarItem) {
-        print("Did select command item \(item)")
+        print("Did select command \((item as! Item).command)")
     }
 }
