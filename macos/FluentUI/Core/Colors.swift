9df18148e4811202fcc5694462062a8c21b9102e
Chris Pearson <chripear@microsoft.com>
2021-01-05 10:47:22 -0800

    Add Objective-C class method for palette colors (#369)

--- macos/FluentUI/Core/Colors.swift
+++ macos/FluentUI/Core/Colors.swift
@@ -247,7 +247,16 @@ public final class Colors: NSObject {
 			}
 		}
 	}
-	
+
+	/// Allows Objective-C to get `NSColor` objects from `Palette` values.
+	/// - Parameter palette: The `Palette` enum value.
+	/// - Returns: The `NSColor` for the given `palette` value.
+	/// # Example #
+	/// `NSColor *communicationBlue = [MSFColors colorFrom:MSFColorPaletteCommunicationBlue];`
+	@objc public static func colorFrom(_ palette: Palette) -> NSColor {
+		return palette.color
+	}
+
 	// MARK: Primary
 	
 	@objc public static var primary: NSColor = Colors.Palette.communicationBlue.color
