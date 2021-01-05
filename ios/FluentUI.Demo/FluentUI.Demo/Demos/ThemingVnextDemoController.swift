022a3d2ef81ce6a7093dcd90c7f3cbda4aacc736
Rafael Amaral de Assis <rdeassis@microsoft.com>
2021-01-05 10:31:25 -0800

    Improving the Theming demo controller.

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/ThemingVnextDemoController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/ThemingVnextDemoController.swift
@@ -12,6 +12,7 @@ class ThemingVnextDemoController: DemoController {
         super.viewDidLoad()
 
         addTitle(text: "Default / Current window theme")
+        addDescription(text: "The default theme is defined by the FluentUIStyle class. The client app can override any of its properties and associate it to a specific UIWindow instance.")
 
         let customThemeButton = MSFButtonVnext(style: .primary,
                                                size: .medium,
@@ -34,13 +35,37 @@ class ThemingVnextDemoController: DemoController {
         disabledCstomThemeButton.state.text = "Reset theme for current window"
 
         addRow(items: [customThemeButton.view, disabledCstomThemeButton.view], itemSpacing: 20)
+
+        let avatarAccent = AvatarVnext(style: .accent, size: .xlarge)
+        avatarAccent.state.isRingVisible = true
+        avatarAccent.state.presence = .available
+
+        let avatarDefault = AvatarVnext(style: .default, size: .xlarge)
+        avatarDefault.state.isRingVisible = true
+        avatarDefault.state.presence = .busy
+
+        let avatarOutlinedPrimary = AvatarVnext(style: .outlinedPrimary, size: .xlarge)
+        avatarOutlinedPrimary.state.isRingVisible = true
+        avatarOutlinedPrimary.state.presence = .away
+
+        addRow(items: [avatarAccent.view, avatarDefault.view, avatarOutlinedPrimary.view], itemSpacing: 20)
+
         container.addArrangedSubview(UIView())
 
         addTitle(text: "Theme overriding")
+        addDescription(text: "A theme can also be applied to a specific control or SwiftUI view hierarichy. This has the highest priority for the controls.")
+
         let overridingTheme = FluentUIStyle()
         let colorAP = overridingTheme.Colors
         let brandAP = colorAP.Brand
-        brandAP.primary = .purple
+        brandAP.primary = UIColor(red: 0.384, green: 0.392, blue: 0.655, alpha: 1.0)
+        brandAP.tint10 = UIColor(red: 0.651, green: 0.655, blue: 0.863, alpha: 1.0)
+        brandAP.tint20 = UIColor(red: 0.741, green: 0.741, blue: 0.902, alpha: 1.0)
+        brandAP.tint30 = UIColor(red: 0.226, green: 0.226, blue: 0.246, alpha: 1.0)
+        brandAP.tint40 = UIColor(red: 0.898, green: 0.898, blue: 0.945, alpha: 1.0)
+        brandAP.shade10 = UIColor(red: 0.345, green: 0.353, blue: 0.588, alpha: 1.0)
+        brandAP.shade20 = UIColor(red: 0.275, green: 0.278, blue: 0.459, alpha: 1.0)
+        brandAP.shade30 = UIColor(red: 0.200, green: 0.204, blue: 0.290, alpha: 1.0)
         colorAP.Brand = brandAP
 
         let border = overridingTheme.Border
@@ -54,16 +79,47 @@ class ThemingVnextDemoController: DemoController {
         overridingTheme.Border = border
         overridingTheme.Colors = colorAP
 
-        for style in MSFButtonVnextStyle.allCases {
-            let customThemeButton = MSFButtonVnext(style: style,
-                                                   size: .medium,
-                                                   action: nil,
-                                                   theme: overridingTheme)
-            customThemeButton.state.text = "Button"
-            customThemeButton.state.image = style.image
+        let customThemeButtonPrimary = MSFButtonVnext(style: .primary,
+                                                      size: .medium,
+                                                      action: nil,
+                                                      theme: overridingTheme)
+        customThemeButtonPrimary.state.text = "Button"
+        customThemeButtonPrimary.state.image = UIImage(named: "Placeholder_24")!
 
-            addRow(items: [customThemeButton.view], itemSpacing: 20)
-        }
+        let customThemeButtonSecondary = MSFButtonVnext(style: .secondary,
+                                                        size: .medium,
+                                                        action: nil,
+                                                        theme: overridingTheme)
+        customThemeButtonSecondary.state.text = "Button"
+        customThemeButtonSecondary.state.image = UIImage(named: "Placeholder_24")!
+
+        let customThemeButtonGhost = MSFButtonVnext(style: .ghost,
+                                                    size: .medium,
+                                                    action: nil,
+                                                    theme: overridingTheme)
+        customThemeButtonGhost.state.text = "Button"
+
+        addRow(items: [customThemeButtonPrimary.view, customThemeButtonSecondary.view, customThemeButtonGhost.view], itemSpacing: 20)
+
+        let customThemeAvatarAccent = AvatarVnext(style: .accent,
+                                                  size: .xlarge,
+                                                  theme: overridingTheme)
+        customThemeAvatarAccent.state.isRingVisible = true
+        customThemeAvatarAccent.state.presence = .available
+
+        let customThemeAvatarDefault = AvatarVnext(style: .default,
+                                                   size: .xlarge,
+                                                   theme: overridingTheme)
+        customThemeAvatarDefault.state.isRingVisible = true
+        customThemeAvatarDefault.state.presence = .busy
+
+        let customThemeAvatarOutlinedPrimary = AvatarVnext(style: .outlinedPrimary,
+                                                           size: .xlarge,
+                                                           theme: overridingTheme)
+        customThemeAvatarOutlinedPrimary.state.isRingVisible = true
+        customThemeAvatarOutlinedPrimary.state.presence = .away
+
+        addRow(items: [customThemeAvatarAccent.view, customThemeAvatarDefault.view, customThemeAvatarOutlinedPrimary.view], itemSpacing: 20)
 
         container.addArrangedSubview(UIView())
     }
