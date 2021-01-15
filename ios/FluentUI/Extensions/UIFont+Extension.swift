89b25b8f176e4870b850fa828738da6ef5ccafed
Zhu Shengqi <zetasq@gmail.com>
2021-01-16 00:15:58 +0800

    Make several internal extensions public and bring back UIFontDescriptor's extensions

--- ios/FluentUI/Extensions/UIFont+Extension.swift
+++ ios/FluentUI/Extensions/UIFont+Extension.swift
@@ -5,7 +5,12 @@
 
 import UIKit
 
-extension UIFont {
+public extension UIFont {
     var deviceLineHeight: CGFloat { return UIScreen.main.roundToDevicePixels(lineHeight) }
+
     var deviceLineHeightWithLeading: CGFloat { return UIScreen.main.roundToDevicePixels(lineHeight + max(0, leading)) }
+
+    func withWeight(_ weight: UIFont.Weight) -> UIFont {
+        return UIFont(descriptor: fontDescriptor.withWeight(weight), size: pointSize)
+  }
 }
--- ios/FluentUI/Extensions/UITableViewCell+Extension.swift
+++ ios/FluentUI/Extensions/UITableViewCell+Extension.swift
@@ -5,7 +5,7 @@
 
 import UIKit
 
-extension UITableViewCell {
+public extension UITableViewCell {
     func hideSystemSeparator() {
         separatorInset = UIEdgeInsets(top: 0, left: max(UIScreen.main.bounds.width, UIScreen.main.bounds.height), bottom: 0, right: 0)
     }
