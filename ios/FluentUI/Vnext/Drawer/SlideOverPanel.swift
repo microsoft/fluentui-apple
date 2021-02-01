30a9bcead80e71cbe5881dac7c3666748a23f150
Kunal Balani <kubalani@microsoft.com>
2021-02-01 14:45:49 -0800

    review feedback

--- ios/FluentUI/Vnext/Drawer/SlideOverPanel.swift
+++ ios/FluentUI/Vnext/Drawer/SlideOverPanel.swift
@@ -60,11 +60,11 @@ struct MSFSlideOverPanel<Content: View>: View {
             content
                 .frame(width: contentWidth)
                 .offset(x: resolvedContentOffset)
-                .shadow(color: shadowColor(true),
+                .shadow(color: tokens.shadow1Color,
                         radius: tokens.shadow1Blur,
                         x: tokens.shadow1DepthX,
                         y: tokens.shadow1DepthY)
-                .shadow(color: shadowColor(false),
+                .shadow(color: tokens.shadow2Color,
                         radius: tokens.shadow2Blur,
                         x: tokens.shadow2DepthX,
                         y: tokens.shadow2DepthY)
@@ -153,10 +153,6 @@ struct MSFSlideOverPanel<Content: View>: View {
             }
         }
         return minValue
-    }
-
-    private func shadowColor(_ isPrimary: Bool = true) -> Color {
-        return isPrimary ? tokens.shadow1Color : tokens.shadow2Color
     }
 }
 
