fb4770544be11281df90f323c4b895fc4f165853
Todd Harris <toddha@microsoft.com>
2021-02-02 14:13:14 -0800

    Adds simple SearchBarDemoController to test SearchBar
    This adds a search bar demo controller to explicitly test the search bar autocorrect scenarios. More tests can be added.
    
    Most tests for the search bar are in the NavigationControllerDemoController, but there's value in just testing the individual pieces.

---
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/SearchBarDemoController.swift
@@ -0,0 +1,55 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import FluentUI
+import UIKit
+
+class SearchBarDemoController: DemoController, SearchBarDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let searchBarNoAutocorrect = buildSearchBar(autocorrectionType: .no, placeholderText: "no autocorrect")
+        let searchBarAutocorrect = buildSearchBar(autocorrectionType: .yes, placeholderText: "autocorrect")
+
+        container.addArrangedSubview(UIView())  // spacer
+        container.addArrangedSubview(UIView())  // spacer
+        container.addArrangedSubview(searchBarNoAutocorrect)
+        container.addArrangedSubview(UIView())  // spacer
+        container.addArrangedSubview(UIView())  // spacer
+        container.addArrangedSubview(searchBarAutocorrect)
+        container.addArrangedSubview(UIView())  // spacer
+        container.addArrangedSubview(UIView())  // spacer
+    }
+
+    func buildSearchBar(autocorrectionType: UITextAutocorrectionType, placeholderText: String) -> SearchBar {
+        let searchBar = SearchBar(frame: .zero)
+        searchBar.delegate = self
+        searchBar.style = .darkContent // we want the opposite as we're not embedded in the header
+        searchBar.placeholderText = placeholderText
+        searchBar.hidesNavigationBarDuringSearch = false
+        searchBar.autocorrectionType = autocorrectionType
+        return searchBar
+    }
+    
+    // MARK: SearchBarDelegate
+    
+    func searchBarDidBeginEditing(_ searchBar: SearchBar) {
+    }
+    
+    func searchBar(_ searchBar: SearchBar, didUpdateSearchText newSearchText: String?) {
+        searchBar.placeholderText = newSearchText ?? "search"
+    }
+    
+    func searchBarDidFinishEditing(_ searchBar: SearchBar) {
+    }
+    
+    func searchBarDidCancel(_ searchBar: SearchBar) {
+        searchBar.progressSpinner.stopAnimating()
+    }
+    
+    func searchBarDidRequestSearch(_ searchBar: SearchBar) {
+        searchBar.progressSpinner.startAnimating()
+    }
+}
