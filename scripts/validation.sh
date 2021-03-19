c5e8edd37f7160fb8aebbf2c4238febe55d026b1
Kunal Balani <kubalani@microsoft.com>
2021-03-19 14:16:20 -0700

    WIP

--- scripts/validation.sh
+++ scripts/validation.sh
@@ -4,12 +4,7 @@ if which swiftlint >/dev/null; then
     export LINTPATH="../."
     export EXCLUDELINTPATH="../tools"
     echo "listing file under directory" $PWD
-    filenames=`ls -a`
-    for eachfile in $filenames
-    do
-      echo $eachfile
-    done
-    swiftlint --config ../.swiftlint.yml
+    swiftlint --config .swiftlint.yml
 else
     echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
