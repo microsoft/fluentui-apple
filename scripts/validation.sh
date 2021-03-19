582d6fd1bbace0d88e8121e796ebaed2c8814067
Kunal Balani <kubalani@microsoft.com>
2021-03-19 14:19:50 -0700

    WIP

--- scripts/validation.sh
+++ scripts/validation.sh
@@ -1,9 +1,6 @@
 # A simple script to validate project settings.
 
 if which swiftlint >/dev/null; then
-    export LINTPATH="../."
-    export EXCLUDELINTPATH="../tools"
-    echo "listing file under directory" $PWD
     swiftlint --config .swiftlint.yml
 else
     echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
