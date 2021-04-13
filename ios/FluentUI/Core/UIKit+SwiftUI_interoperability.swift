51ba63e9b860d0dad0d5146431a19e998dfddb73
Kunal Balani <kubalani@microsoft.com>
2021-04-13 15:42:13 -0700

    Add UIKit file back after being misplaced during rebase

---
+++ ios/FluentUI/Core/UIKit+SwiftUI_interoperability.swift
@@ -0,0 +1,36 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import UIKit
+import SwiftUI
+
+/// This is a generic UIView wrapper to allow SwiftUI to use views from non-SwiftUI environments.
+struct UIViewAdapter: UIViewRepresentable {
+
+    var makeView: () -> UIView
+
+    init(_ makeView: @escaping @autoclosure () -> UIView) {
+        self.makeView = makeView
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        // Wrapping the view passed on a StackView is a workaround for a hang that occurs
+        // on iOS 13 when the view passed directly comes from a UIHostingController.view
+        // property. (e.g. using the MSFAvatar.view property).
+        return UIStackView(arrangedSubviews: [makeView()])
+    }
+
+    func updateUIView(_ view: UIView, context: Context) {
+        // This logic should be removed wnce the "wrapping in a UIStackView" workaround is removed.
+        guard let stackView = view as? UIStackView else {
+            return
+        }
+
+        for view in stackView.arrangedSubviews {
+            view.removeFromSuperview()
+        }
+        stackView.addArrangedSubview(makeView())
+    }
+}
