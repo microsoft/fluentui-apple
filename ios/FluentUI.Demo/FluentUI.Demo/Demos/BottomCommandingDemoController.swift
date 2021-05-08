14b4f03e63d2c58c1b5522aad250411aca60e229
Lukas Capkovic <lucapkov@microsoft.com>
2021-05-07 18:03:19 -0700

    Port the demo controller to a table view

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/BottomCommandingDemoController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/BottomCommandingDemoController.swift
@@ -11,6 +11,16 @@ class BottomCommandingDemoController: DemoController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let optionTableView = UITableView(frame: .zero, style: .plain)
+        optionTableView.translatesAutoresizingMaskIntoConstraints = false
+        optionTableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.identifier)
+        optionTableView.register(BooleanCell.self, forCellReuseIdentifier: BooleanCell.identifier)
+        optionTableView.register(ActionsCell.self, forCellReuseIdentifier: ActionsCell.identifier)
+        optionTableView.dataSource = self
+        optionTableView.delegate = self
+        optionTableView.separatorStyle = .none
+        view.addSubview(optionTableView)
+
         let bottomCommandingVC = BottomCommandingController()
         bottomCommandingVC.heroItems = heroItems
         bottomCommandingVC.expandedListSections = expandedListSections
@@ -21,23 +31,45 @@ class BottomCommandingDemoController: DemoController {
 
         bottomCommandingController = bottomCommandingVC
 
-        container.addArrangedSubview(createLabelAndSwitchRow(labelText: "Expanded list items", switchAction: #selector(toggleExpandedItems), isOn: true))
-        container.addArrangedSubview(createLabelAndSwitchRow(labelText: "Hero command isOn", switchAction: #selector(toggleHeroCommandOnOff)))
-        container.addArrangedSubview(createLabelAndSwitchRow(labelText: "Hero command isEnabled", switchAction: #selector(toggleHeroCommandEnabled), isOn: true))
-        container.addArrangedSubview(createLabelAndSwitchRow(labelText: "List command isEnabled", switchAction: #selector(toggleListCommandEnabled), isOn: true))
-        container.addArrangedSubview(createButton(title: "Change hero command titles", action: #selector(changeHeroCommandTitle)))
-        container.addArrangedSubview(createButton(title: "Change hero command images", action: #selector(changeHeroCommandIcon)))
-        container.addArrangedSubview(createButton(title: "Change list command titles", action: #selector(changeListCommandTitle)))
-        container.addArrangedSubview(createButton(title: "Change list command images", action: #selector(changeListCommandIcon)))
-        addRow(text: "Hero command count", items: [decrementHeroCommandCountButton, incrementHeroCommandCountButton], textWidth: 150)
-
         NSLayoutConstraint.activate([
+            optionTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            optionTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            optionTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            optionTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             bottomCommandingVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             bottomCommandingVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            bottomCommandingVC.view.topAnchor.constraint(equalTo: view.topAnchor),
+            bottomCommandingVC.view.topAnchor.constraint(lessThanOrEqualTo: view.topAnchor),
             bottomCommandingVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
+
+    private lazy var heroItems: [CommandingItem] = {
+        return Array(1...5).map {
+            CommandingItem(title: "Item " + String($0), image: homeImage, action: commandAction, selectedImage: homeSelectedImage)
+        }
+    }()
+
+    private lazy var expandedListSections: [CommandingSection] = [
+        CommandingSection(title: "Section 1", items: Array(1...7).map {
+                CommandingItem(title: "Item " + String($0), image: homeImage, action: commandAction)
+        }),
+        CommandingSection(title: "Section 2", items: Array(1...7).map {
+            CommandingItem(title: "Item " + String($0), image: homeImage, action: commandAction)
+        })
+    ]
+
+    private lazy var demoOptionItems: [DemoItem] = {
+        return [DemoItem(title: "Expanded list items", type: .boolean, action: #selector(toggleExpandedItems), isOn: true),
+                DemoItem(title: "Hero command isOn", type: .boolean, action: #selector(toggleHeroCommandOnOff)),
+                DemoItem(title: "Hero command isEnabled", type: .boolean, action: #selector(toggleHeroCommandEnabled), isOn: true),
+                DemoItem(title: "List command isEnabled", type: .boolean, action: #selector(toggleListCommandEnabled), isOn: true),
+                DemoItem(title: "Change hero command titles", type: .action, action: #selector(changeHeroCommandTitle)),
+                DemoItem(title: "Change hero command images", type: .action, action: #selector(changeHeroCommandIcon)),
+                DemoItem(title: "Change list command titles", type: .action, action: #selector(changeListCommandTitle)),
+                DemoItem(title: "Change list command images", type: .action, action: #selector(changeListCommandIcon)),
+                DemoItem(title: "Hero command count", type: .stepper, action: nil)
+        ]
+    }()
 
     @objc private func toggleExpandedItems() {
         if bottomCommandingController?.expandedListSections.count == 0 {
@@ -47,50 +79,52 @@ class BottomCommandingDemoController: DemoController {
         }
     }
 
+    private let modifiedCommandIndices: [Int] = [0, 2, 4]
+
     @objc private func toggleHeroCommandOnOff() {
-        [0, 2, 4].forEach {
+        modifiedCommandIndices.forEach {
             heroItems[$0].isOn.toggle()
         }
     }
 
     @objc private func toggleHeroCommandEnabled() {
-        [0, 2, 4].forEach {
+        modifiedCommandIndices.forEach {
             heroItems[$0].isEnabled.toggle()
         }
     }
 
     @objc private func toggleListCommandEnabled() {
-        [0, 2, 4].forEach {
+        modifiedCommandIndices.forEach {
             expandedListSections[0].items[$0].isEnabled.toggle()
         }
     }
 
     @objc private func changeHeroCommandTitle() {
-        [0, 2, 4].forEach {
+        modifiedCommandIndices.forEach {
             heroItems[$0].title = "Item " + String(Int.random(in: 6..<100))
         }
     }
 
     @objc private func changeListCommandTitle() {
-        [0, 2, 4].forEach {
+        modifiedCommandIndices.forEach {
             expandedListSections[0].items[$0].title = "Item " + String(Int.random(in: 6..<100))
         }
     }
 
     @objc private func changeHeroCommandIcon() {
-        [0, 2, 4].forEach {
-            heroItems[$0].image = UIImage(named: iconChanged ? "Home_24" : "textBold24Regular")!
-            heroItems[$0].selectedImage = UIImage(named: iconChanged ? "Home_Selected_24" : "textBold24Regular")!
+        modifiedCommandIndices.forEach {
+            heroItems[$0].image = heroIconChanged ? homeImage : boldImage
+            heroItems[$0].selectedImage = heroIconChanged ? homeSelectedImage : boldImage
         }
-        iconChanged.toggle()
+        heroIconChanged.toggle()
     }
 
     @objc private func changeListCommandIcon() {
-        [0, 2, 4].forEach {
-            expandedListSections[0].items[$0].image = UIImage(named: iconChanged ? "Home_24" : "textBold24Regular")!
-            expandedListSections[0].items[$0].selectedImage = UIImage(named: iconChanged ? "Home_Selected_24" : "textBold24Regular")!
+        modifiedCommandIndices.forEach {
+            expandedListSections[0].items[$0].image = listIconChanged ? homeImage : boldImage
+            expandedListSections[0].items[$0].selectedImage = listIconChanged ? homeSelectedImage : boldImage
         }
-        iconChanged.toggle()
+        listIconChanged.toggle()
     }
 
     @objc private func incrementHeroCommands() {
@@ -117,44 +151,100 @@ class BottomCommandingDemoController: DemoController {
         }
     }
 
-    private lazy var heroItems: [CommandingItem] = [
-        CommandingItem(title: "Item 1", image: UIImage(named: "Home_24")!, action: commandAction, selectedImage: UIImage(named: "Home_Selected_24")!),
-        CommandingItem(title: "Item 2", image: UIImage(named: "Home_24")!, action: commandAction, selectedImage: UIImage(named: "Home_Selected_24")!),
-        CommandingItem(title: "Item 3", image: UIImage(named: "Home_24")!, action: commandAction, selectedImage: UIImage(named: "Home_Selected_24")!),
-        CommandingItem(title: "Item 4", image: UIImage(named: "Home_24")!, action: commandAction, selectedImage: UIImage(named: "Home_Selected_24")!),
-        CommandingItem(title: "Item 5", image: UIImage(named: "Home_24")!, action: commandAction, selectedImage: UIImage(named: "Home_Selected_24")!)
-    ]
-
-    private lazy var expandedListSections: [CommandingSection] = [
-        CommandingSection(title: "Section 1", items: [
-            CommandingItem(title: "Item 1", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 2", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 3", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 4", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 5", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 6", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 7", image: UIImage(named: "Home_24")!, action: commandAction)
-        ]),
-        CommandingSection(title: "Section 2", items: [
-            CommandingItem(title: "Item 1", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 2", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 3", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 4", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 5", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 6", image: UIImage(named: "Home_24")!, action: commandAction),
-            CommandingItem(title: "Item 7", image: UIImage(named: "Home_24")!, action: commandAction)
-        ])
-    ]
-
     private lazy var incrementHeroCommandCountButton: Button = {
-        return createButton(title: "+", action: #selector(incrementHeroCommands))
+        let button = Button()
+        button.image = UIImage(named: "ic_fluent_add_20_regular")
+        button.accessibilityLabel = "Increment hero command count"
+        button.addTarget(self, action: #selector(incrementHeroCommands), for: .touchUpInside)
+        return button
     }()
 
     private lazy var decrementHeroCommandCountButton: Button = {
-        return createButton(title: "-", action: #selector(decrementHeroCommands))
+        let button = Button()
+        button.image = UIImage(named: "ic_fluent_subtract_20_regular")
+        button.accessibilityLabel = "Decrement hero command count"
+        button.addTarget(self, action: #selector(decrementHeroCommands), for: .touchUpInside)
+        return button
     }()
 
-    private var iconChanged: Bool = false
+    private let homeImage = UIImage(named: "Home_24")!
+    private let homeSelectedImage = UIImage(named: "Home_Selected_24")!
+    private let boldImage = UIImage(named: "textBold24Regular")!
+
+    private var heroIconChanged: Bool = false
+    private var listIconChanged: Bool = false
 
     private var bottomCommandingController: BottomCommandingController?
+
+    private enum DemoItemType {
+        case action
+        case boolean
+        case stepper
+    }
+
+    private struct DemoItem {
+        let title: String
+        let type: DemoItemType
+        let action: Selector?
+        var isOn: Bool = false
+    }
+}
+
+extension BottomCommandingDemoController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+}
+
+extension BottomCommandingDemoController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return demoOptionItems.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = demoOptionItems[indexPath.row]
+
+        if item.type == .boolean {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: BooleanCell.identifier) as? BooleanCell else {
+                return UITableViewCell()
+            }
+            cell.setup(title: item.title, isOn: item.isOn)
+            cell.titleNumberOfLines = 0
+            cell.onValueChanged = { [weak self, weak cell] in
+                self?.perform(item.action, with: cell)
+            }
+            return cell
+        } else if item.type == .action {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ActionsCell.identifier) as? ActionsCell else {
+                return UITableViewCell()
+            }
+            cell.setup(action1Title: item.title)
+            if let action = item.action {
+                cell.action1Button.addTarget(self, action: action, for: .touchUpInside)
+            }
+            cell.bottomSeparatorType = .full
+            return cell
+        } else if item.type == .stepper {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier) as? TableViewCell else {
+                return UITableViewCell()
+            }
+
+            let stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+            stackView.addArrangedSubview(decrementHeroCommandCountButton)
+            stackView.addArrangedSubview(incrementHeroCommandCountButton)
+            stackView.distribution = .fillEqually
+            stackView.alignment = .center
+            stackView.spacing = 4
+
+            cell.setup(title: item.title, customAccessoryView: stackView)
+            cell.titleNumberOfLines = 0
+            return cell
+        }
+
+        return UITableViewCell()
+    }
 }
