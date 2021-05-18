c360c0faebe0108de0d85af7e96ababcf76f51ed
Lukas Capkovic <3610850+lcapkovic@users.noreply.github.com>
2021-05-18 16:50:07 -0700

    Use constraints for TabBarItemView image resizing (#575)
    - use constraints instead of frame modification to force image size
    - fixes issues where images weren't reliably resized, and their position changed on screen rotation

--- ios/FluentUI/Tab Bar/TabBarItemView.swift
+++ ios/FluentUI/Tab Bar/TabBarItemView.swift
@@ -116,9 +116,6 @@ class TabBarItemView: UIView {
     }
 
     open override func sizeThatFits(_ size: CGSize) -> CGSize {
-        if canResizeImage {
-            imageView.frame = CGRect(x: 0, y: 0, width: suggestImageSize, height: suggestImageSize)
-        }
         let size = container.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
         return size
     }
@@ -172,13 +169,24 @@ class TabBarItemView: UIView {
         return container
     }()
 
-    private let imageView: UIImageView = {
+    private lazy var imageView: UIImageView = {
         let imageView = UIImageView(frame: .zero)
         imageView.contentMode = .scaleAspectFit
         imageView.tintColor = Constants.unselectedColor
 
+        if canResizeImage {
+            let sizeConstraints = (
+                width: imageView.widthAnchor.constraint(equalToConstant: suggestImageSize),
+                height: imageView.heightAnchor.constraint(equalToConstant: suggestImageSize)
+            )
+            sizeConstraints.width.isActive = true
+            sizeConstraints.height.isActive = true
+            imageViewSizeConstraints = sizeConstraints
+        }
         return imageView
     }()
+
+    private var imageViewSizeConstraints: (width: NSLayoutConstraint, height: NSLayoutConstraint)?
 
     private let titleLabel: Label = {
         let titleLabel = Label()
@@ -201,7 +209,15 @@ class TabBarItemView: UIView {
         return badgeView
     }()
 
-    private var suggestImageSize: CGFloat
+    private var suggestImageSize: CGFloat {
+        didSet {
+            if canResizeImage,
+               let sizeConstraints = imageViewSizeConstraints {
+                sizeConstraints.width.constant = suggestImageSize
+                sizeConstraints.height.constant = suggestImageSize
+            }
+        }
+    }
     private let canResizeImage: Bool
 
     private var imageViewFrame: CGRect = .zero {
