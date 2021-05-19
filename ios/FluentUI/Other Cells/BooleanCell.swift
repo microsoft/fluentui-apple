b235cced326b292e667d3dcd63bb6614d40240b0
Harry Qian <heqia@microsoft.com>
2021-05-19 21:37:35 +0800

    Update

--- ios/FluentUI/Other Cells/BooleanCell.swift
+++ ios/FluentUI/Other Cells/BooleanCell.swift
@@ -15,6 +15,8 @@ public typealias MSBooleanCell = BooleanCell
 
  Use the `isOn` property to update the cell accessory view's `isOn` property.
 
+ Use the `isSwitchEnabled` property to update the cell accessory view's `isEnabled` property.
+
  Use `onValueChanged` as a callback for an event that changes the value of the accessory view.
  */
 @objc(MSFBooleanCell)
@@ -23,6 +25,12 @@ open class BooleanCell: TableViewCell {
     @objc public var isOn: Bool {
         get { return `switch`.isOn }
         set { `switch`.isOn = newValue }
+    }
+
+    /// Updates the cell accessory view's `isEnabled` property
+    @objc public var isSwitchEnabled: Bool {
+      get { return `switch`.isEnabled }
+      set { `switch`.isEnabled = newValue }
     }
 
     /// `onValueChanged` is called when the cell accessory view's value has changed
@@ -40,9 +48,11 @@ open class BooleanCell: TableViewCell {
     ///   - title: Text that appears as a single line
     ///   - customView: An optional custom view that appears on the leading edge, adjacent to the start of the `title` text
     ///   - isOn: A boolean value describing the `isOn` state of the accessory view
-    @objc open func setup(title: String, customView: UIView? = nil, isOn: Bool = false) {
+    ///   - isSwitchEnabled: A boolean value describing the `isEnabled` state of the accessory view
+    @objc open func setup(title: String, customView: UIView? = nil, isOn: Bool = false, isSwitchEnabled: Bool = true) {
         setup(title: title, customView: customView, customAccessoryView: `switch`)
         self.isOn = isOn
+        self.isSwitchEnabled = isSwitchEnabled
     }
 
     @objc private func handleOnSwitchValueChanged() {
