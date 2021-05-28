aa70abf9cc6961b265dcdcf43a911cc2768e9bde
Harry Qian <heqia@microsoft.com>
2021-05-28 13:48:18 +0800

    Fix accessibility

--- ios/FluentUI/Other Cells/BooleanCell.swift
+++ ios/FluentUI/Other Cells/BooleanCell.swift
@@ -29,8 +29,19 @@ open class BooleanCell: TableViewCell {
 
     /// Updates the cell accessory view's `isEnabled` property
     @objc public var isSwitchEnabled: Bool {
-      get { return `switch`.isEnabled }
-      set { `switch`.isEnabled = newValue }
+      get {
+        return `switch`.isEnabled && isEnabled
+      }
+      set {
+        `switch`.isEnabled = newValue
+        updateAccessibility()
+      }
+    }
+
+    @objc open override var isEnabled: Bool {
+        didSet {
+            updateAccessibility()
+        }
     }
 
     /// `onValueChanged` is called when the cell accessory view's value has changed
@@ -41,6 +52,26 @@ open class BooleanCell: TableViewCell {
         `switch`.addTarget(self, action: #selector(handleOnSwitchValueChanged), for: .valueChanged)
         return `switch`
     }()
+
+    @objc public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        updateAccessibility()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        updateAccessibility()
+    }
+
+    /// Sets up the content of the cell
+    ///
+    /// - Parameters:
+    ///   - title: Text that appears as a single line
+    ///   - customView: An optional custom view that appears on the leading edge, adjacent to the start of the `title` text
+    ///   - isOn: A boolean value describing the `isOn` state of the accessory view
+    @objc open func setup(title: String, customView: UIView? = nil, isOn: Bool = false) {
+        setup(title: title, customView: customView, isOn: isOn, isSwitchEnabled: true)
+    }
 
     /// Sets up the content of the cell
     ///
@@ -63,6 +94,14 @@ open class BooleanCell: TableViewCell {
         super.didMoveToWindow()
         if let window = window {
             `switch`.onTintColor = Colors.primary(for: window)
+        }
+    }
+
+    private func updateAccessibility() {
+        if isEnabled && isSwitchEnabled {
+            accessibilityTraits.remove(.notEnabled)
+        } else {
+            accessibilityTraits.insert(.notEnabled)
         }
     }
 }
