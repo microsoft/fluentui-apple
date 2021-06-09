95458b3d7ec682b786fd3d80d3bac3ddce0269fd
Curia Damiano <damiano.curia@gmail.com>
2021-06-09 10:01:12 +0200

    New changes after PR comments

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/DateTimePickerDemoController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/DateTimePickerDemoController.swift
@@ -42,8 +42,8 @@ class DateTimePickerDemoController: DemoController {
         container.addArrangedSubview(createButton(title: "Show date range picker (tabbed)", action: #selector(presentTabbedDateRangePicker)))
         container.addArrangedSubview(createButton(title: "Show date time range picker", action: #selector(presentDateTimeRangePicker)))
         container.addArrangedSubview(createButton(title: "Show picker with custom subtitles or tabs", action: #selector(presentCustomSubtitlePicker)))
-        container.addArrangedSubview(createButton(title: "Show picker with left glyph", action: #selector(presentLeftGlyphPicker)))
-        container.addArrangedSubview(createButton(title: "Show picker with left and right glyphs", action: #selector(presentRightGlyphPicker)))
+        container.addArrangedSubview(createButton(title: "Show picker with left bar-button", action: #selector(presentLeftBarButtonPicker)))
+        container.addArrangedSubview(createButton(title: "Show picker with left and right bar-buttons", action: #selector(presentRightBarButtonPicker)))
         container.addArrangedSubview(UIView())
         container.addArrangedSubview(createDatePickerTypeUI())
         container.addArrangedSubview(createValidationUI())
@@ -101,6 +101,24 @@ class DateTimePickerDemoController: DemoController {
         return (startDate, endDate, titles)
     }
 
+    func confirmButton(target: Any?, action: Selector?) -> UIBarButtonItem {
+        let image = UIImage(named: "checkmark-24x24", in: FluentUIFramework.resourceBundle, compatibleWith: nil)
+        let landscapeImage = UIImage(named: "checkmark-thin-20x20", in: FluentUIFramework.resourceBundle, compatibleWith: nil)
+
+        let button = UIBarButtonItem(image: image, landscapeImagePhone: landscapeImage, style: .plain, target: target, action: action)
+        button.accessibilityLabel = NSLocalizedString("Accessibility.Done.Label", bundle: FluentUIFramework.resourceBundle, comment: "")
+        return button
+    }
+
+    func cancelButton(target: Any?, action: Selector?) -> UIBarButtonItem {
+        let image = UIImage(named: "dismiss-20x20", in: FluentUIFramework.resourceBundle, compatibleWith: nil)
+        let landscapeImage = UIImage(named: "dismiss-36x36", in: FluentUIFramework.resourceBundle, compatibleWith: nil)
+
+        let button = UIBarButtonItem(image: image, landscapeImagePhone: landscapeImage, style: .plain, target: target, action: action)
+        button.accessibilityLabel = NSLocalizedString("Accessibility.Dismiss.Label", bundle: FluentUIFramework.resourceBundle, comment: "")
+        return button
+    }
+
     @objc func presentDateRangePicker() {
         let (startDate, endDate, _) = calcDatePickerParams()
         dateTimePicker.present(from: self, with: .dateRange, startDate: startDate, endDate: endDate, datePickerType: datePickerType)
@@ -121,29 +139,29 @@ class DateTimePickerDemoController: DemoController {
         dateTimePicker.present(from: self, with: .dateRange, startDate: startDate, endDate: endDate, datePickerType: datePickerType, titles: titles)
     }
 
-    @objc func presentLeftGlyphPicker() {
+    @objc func presentLeftBarButtonPicker() {
         let (startDate, endDate, titles) = calcDatePickerParams()
-        let leftBarButtonItem = DemoBarButtonItems.cancel(target: self, action: #selector(handleDidTapCancel))
+        let leftBarButtonItem = cancelButton(target: self, action: #selector(handleDidTapCancel))
         dateTimePicker.present(from: self, with: .dateRange, startDate: startDate, endDate: endDate, datePickerType: datePickerType, titles: titles, leftBarButtonItem: leftBarButtonItem)
     }
 
-    @objc func presentRightGlyphPicker() {
+    @objc func presentRightBarButtonPicker() {
         let (startDate, endDate, titles) = calcDatePickerParams()
-        let leftBarButtonItem = DemoBarButtonItems.confirm(target: self, action: #selector(handleDidTapDone))
-        let rightBarButtonItem = DemoBarButtonItems.cancel(target: self, action: #selector(handleDidTapCancel)) // or simply assign UIBarButtonItem() to hide the default confirm button
+        let leftBarButtonItem = confirmButton(target: self, action: #selector(handleDidTapDone))
+        let rightBarButtonItem = cancelButton(target: self, action: #selector(handleDidTapCancel)) // or simply assign UIBarButtonItem() to hide the default confirm button
         dateTimePicker.present(from: self, with: .dateRange, startDate: startDate, endDate: endDate, datePickerType: datePickerType, titles: titles, leftBarButtonItem: leftBarButtonItem, rightBarButtonItem: rightBarButtonItem)
     }
 
     @objc private func handleDidTapCancel() {
         dateTimePicker.dismiss()
-        let alert = UIAlertController(title: "Callback from picker", message: "User has pressed Cancel glyph", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Callback from picker", message: "User has pressed Cancel bar-button", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         present(alert, animated: true)
     }
 
     @objc private func handleDidTapDone() {
         dateTimePicker.dismiss()
-        let alert = UIAlertController(title: "Callback from picker", message: "User has pressed Confirm glyph", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Callback from picker", message: "User has pressed Confirm bar-button", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         present(alert, animated: true)
     }
@@ -193,35 +211,5 @@ extension DateTimePickerDemoController: DateTimePickerDelegate {
             return false
         }
         return true
-    }
-}
-
-class DemoBarButtonItems: NSObject {
-    static func localized(_ string: String) -> String {
-        NSLocalizedString(string, bundle: FluentUIFramework.resourceBundle, comment: "")
-    }
-
-    /// When adding this barButtonItem to the view, tint it with appropriate app color UIColor(light: Colors.primary(for: window), dark: Colors.textDominant)
-    @objc static func confirm(target: Any?, action: Selector?) -> UIBarButtonItem {
-        let image = UIImage(named: "checkmark-24x24", in: FluentUIFramework.resourceBundle, compatibleWith: nil)
-        let landscapeImage = UIImage(named: "checkmark-thin-20x20", in: FluentUIFramework.resourceBundle, compatibleWith: nil)
-
-        let button = UIBarButtonItem(image: image, landscapeImagePhone: landscapeImage, style: .plain, target: target, action: action)
-        button.accessibilityLabel = localized("Accessibility.Done.Label")
-        return button
-    }
-
-    /// When adding this barButtonItem to the view, tint it with appropriate app color UIColor(light: Colors.primary(for: window), dark: Colors.textDominant)
-    @objc static func cancel(target: Any?, action: Selector?) -> UIBarButtonItem {
-        let image = UIImage(named: "dismiss-20x20", in: FluentUIFramework.resourceBundle, compatibleWith: nil)
-        let landscapeImage = UIImage(named: "dismiss-36x36", in: FluentUIFramework.resourceBundle, compatibleWith: nil)
-
-        let button = UIBarButtonItem(image: image, landscapeImagePhone: landscapeImage, style: .plain, target: target, action: action)
-        button.accessibilityLabel = localized("Accessibility.Dismiss.Label")
-        return button
-    }
-
-    private override init() {
-        super.init()
     }
 }
--- ios/FluentUI/Date Time Pickers/DateTimePicker.swift
+++ ios/FluentUI/Date Time Pickers/DateTimePicker.swift
@@ -127,8 +127,8 @@ public class DateTimePicker: NSObject {
     ///   - datePickerType: The type of UI to use for picking dates: calendar or spinners representing date components (day, month, and year) or date (when time picking is also enabled). `.components` will be forced when VoiceOver is running. `.paged` date range presentation is not supported for `.components` date picker type (tabbed presentation will be used instead).
     ///   - dateRangePresentation: The `DateRangePresentation` in which to show any date pickers when `mode` is `.dateRange` or `.dateTimeRange`. Does not affect the time picker, which is always tabbed in range mode, but may change whether the date picker is presented in certain modes.
     ///   - titles: A `Titles` object that holds strings for use in overriding the default picker titles, subtitles, and tab titles. If a string is provided for a property that does not apply to the current mode, it will be ignored.
-    ///   - leftBarButtonItem: optional UIBarButtonItem to be presented as left glyph.
-    ///   - rightBarButtonItem: optional UIBarButtonItem to be presented oas right glyph. Note that if this view is presented, the Confirm button is not generated automatically.
+    ///   - leftBarButtonItem: optional UIBarButtonItem to be presented as left bar-button.
+    ///   - rightBarButtonItem: optional UIBarButtonItem to be presented as right bar-button. Note that if this view is presented, the Confirm button is not generated automatically.
     /// - Tag: MSDateTimePicker.present
     @objc public func present(from presentingController: UIViewController, with mode: DateTimePickerMode, startDate: Date = Date(), endDate: Date? = nil, datePickerType: DatePickerType = .calendar, dateRangePresentation: DateRangePresentation = .paged, titles: Titles? = nil, leftBarButtonItem: UIBarButtonItem? = nil, rightBarButtonItem: UIBarButtonItem? = nil) {
         self.presentingController = presentingController
