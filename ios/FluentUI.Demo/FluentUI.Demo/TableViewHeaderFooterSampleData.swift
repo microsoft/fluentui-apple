4da5011d775be711a2df757d40757ef95b2bb958
Harrie Shin <hyshin@microsoft.com>
2021-06-15 12:34:30 -0700

    TableViewHeaderFooterView larger text support when there is accessoryView (#605)
    when calculating TableViewheaderfooterview height, take in accessoryView into consideration if available.
    update the tableviewheaderfootersampledata to show case multiline scenarios.

--- ios/FluentUI.Demo/FluentUI.Demo/TableViewHeaderFooterSampleData.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/TableViewHeaderFooterSampleData.swift
@@ -12,13 +12,13 @@ class TableViewHeaderFooterSampleData: TableViewSampleData {
 
     static let groupedSections: [Section] = [
         Section(title: "Primary Title", headerStyle: .headerPrimary),
-        Section(title: "Primary Expandable", headerStyle: .headerPrimary, hasCustomLeadingView: true, hasHandler: true),
+        Section(title: "Primary Expandable", numberOfLines: 0, headerStyle: .headerPrimary, hasCustomLeadingView: true, hasHandler: true),
         Section(title: "Default"),
-        Section(title: "Default with accessory button", hasAccessory: true),
-        Section(title: "Default with primary accessory button", hasAccessory: true, accessoryButtonStyle: .primary),
+        Section(title: "Default with accessory button", numberOfLines: 0, hasAccessory: true),
+		Section(title: "Default with primary accessory button", numberOfLines: 0, hasAccessory: true, accessoryButtonStyle: .primary),
         Section(title: "Default with multi-line text - A description that starts at the bottom and provides three to two lines of info.", numberOfLines: 0, hasFooter: true, footerText: "Footer - A description that starts at the top and provides three to two lines of info. Learn More", footerLinkText: "Learn More"),
         Section(title: "Default with multi-line truncated text - A description that starts at the bottom and provides three to two lines of info. Also maybe used for providing detailed documentation for a specific feature.", numberOfLines: 3, hasFooter: true, footerText: "Footer - A description that starts at the top and provides three to two lines of info. Custom Learn More", footerLinkText: "Custom Learn More", hasCustomLinkHandler: true),
-        Section(title: "Default with custom accessory view", hasCustomAccessoryView: true)
+        Section(title: "Default with custom accessory view", numberOfLines: 0, hasCustomAccessoryView: true)
     ]
 
     static let plainSections: [Section] = [
