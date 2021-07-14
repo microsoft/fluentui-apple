6d4f21a7ebf1f8ddf6eb74b855248e63ca9b1cb9
Sophia <sophia.lee0416@gmail.com>
2021-07-13 17:44:57 -0700

    Fixing tokens

--- ios/FluentUI/Vnext/AvatarGroup/AvatarGroupTokens.swift
+++ ios/FluentUI/Vnext/AvatarGroup/AvatarGroupTokens.swift
@@ -47,7 +47,7 @@ class MSFAvatarGroupTokens: MSFTokensBase, ObservableObject {
     override func updateForCurrentTheme() {
         let currentTheme = theme
         var appearanceProxy: AppearanceProxyType
-        let avatarTokens = MSFAvatarTokens(style: .default, size: size)
+        let avatarTokens = currentTheme.MSFAvatarTokens
 
         switch style {
         case .stack:
@@ -59,20 +59,34 @@ class MSFAvatarGroupTokens: MSFTokensBase, ObservableObject {
         switch size {
         case .xsmall:
             interspace = appearanceProxy.interspace.xSmall
+            ringInnerGap = avatarTokens.ringInnerGap.xSmall
+            ringThickness = avatarTokens.ringThickness.xSmall
+            ringOuterGap = avatarTokens.ringOuterGap.xSmall
         case .small:
             interspace = appearanceProxy.interspace.small
+            ringInnerGap = avatarTokens.ringInnerGap.small
+            ringThickness = avatarTokens.ringThickness.small
+            ringOuterGap = avatarTokens.ringOuterGap.small
         case .medium:
             interspace = appearanceProxy.interspace.medium
+            ringInnerGap = avatarTokens.ringInnerGap.medium
+            ringThickness = avatarTokens.ringThickness.medium
+            ringOuterGap = avatarTokens.ringOuterGap.medium
         case .large:
             interspace = appearanceProxy.interspace.large
+            ringInnerGap = avatarTokens.ringInnerGap.large
+            ringThickness = avatarTokens.ringThickness.large
+            ringOuterGap = avatarTokens.ringOuterGap.large
         case .xlarge:
             interspace = appearanceProxy.interspace.xlarge
+            ringInnerGap = avatarTokens.ringInnerGap.xlarge
+            ringThickness = avatarTokens.ringThickness.xlarge
+            ringOuterGap = avatarTokens.ringOuterGap.xlarge
         case .xxlarge:
             interspace = appearanceProxy.interspace.xxlarge
+            ringInnerGap = avatarTokens.ringInnerGap.xxlarge
+            ringThickness = avatarTokens.ringThickness.xxlarge
+            ringOuterGap = avatarTokens.ringOuterGap.xxlarge
         }
-
-        ringInnerGap = avatarTokens.ringInnerGap
-        ringThickness = avatarTokens.ringThickness
-        ringOuterGap = avatarTokens.ringOuterGap
     }
 }
