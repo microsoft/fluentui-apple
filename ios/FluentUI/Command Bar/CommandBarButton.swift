7704f03a5e1b2d8faaa9ddee95f56e2c1c6f11ae
Michael Schreiber <mike.schreiber@microsoft.com>
2021-07-22 17:57:40 -0700

    Replace fatalError with preconditionFailure

--- ios/FluentUI/Command Bar/CommandBarButton.swift
+++ ios/FluentUI/Command Bar/CommandBarButton.swift
@@ -49,7 +49,7 @@ class CommandBarButton: UIButton {
 
     @available(*, unavailable)
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        preconditionFailure("init(coder:) has not been implemented")
     }
 
     func updateState() {
