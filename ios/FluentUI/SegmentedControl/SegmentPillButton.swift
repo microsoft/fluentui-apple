edc6c70da9f91b81cd46e252c83ddd4598649751
Mike Schreiber <mike.schreiber@microsoft.com>
2021-07-23 14:48:57 -0700

    Replace fatalError with preconditionFailure (#647)

--- ios/FluentUI/SegmentedControl/SegmentPillButton.swift
+++ ios/FluentUI/SegmentedControl/SegmentPillButton.swift
@@ -48,7 +48,7 @@ class SegmentPillButton: UIButton {
     }
 
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        preconditionFailure("init(coder:) has not been implemented")
     }
 
     override func layoutSubviews() {
