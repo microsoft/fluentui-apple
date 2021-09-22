88efd7deee661dfae668de7da4b9607be5937f2e
Lukas Capkovic <lucapkov@microsoft.com>
2021-09-22 15:02:45 -0700

    Return UIViewPropertyAnimator in prepareInteractiveIsHiddenChange

--- ios/FluentUI/Bottom Commanding/BottomCommandingController.swift
+++ ios/FluentUI/Bottom Commanding/BottomCommandingController.swift
@@ -203,19 +203,19 @@ open class BottomCommandingController: UIViewController {
         }
     }
 
-    /// Initiates an interactive `isHidden` state change driven by the returned `UIViewAnimating` object.
+    /// Initiates an interactive `isHidden` state change driven by the returned `UIViewPropertyAnimator`.
     ///
     /// For usage details, see `BottomSheetController.prepareInteractiveIsHiddenChange`.
     /// - Parameters:
     ///   - isHidden: The target state.
     ///   - completion: Closure to be called when the state change completes.
-    /// - Returns: An animator that conforms to `UIViewAnimating`. The associated animations start in a paused state.
-    @objc public func prepareInteractiveIsHiddenChange(_ isHidden: Bool, completion: ((_ finalPosition: UIViewAnimatingPosition) -> Void)? = nil) -> UIViewAnimating? {
+    /// - Returns: A `UIViewPropertyAnimator`. The associated animations start in a paused state.
+    @objc public func prepareInteractiveIsHiddenChange(_ isHidden: Bool, completion: ((_ finalPosition: UIViewAnimatingPosition) -> Void)? = nil) -> UIViewPropertyAnimator? {
         guard isViewLoaded else {
             return nil
         }
 
-        var animator: UIViewAnimating?
+        var animator: UIViewPropertyAnimator?
 
         if isInSheetMode {
             animator = bottomSheetController?.prepareInteractiveIsHiddenChange(isHidden, completion: completion)
