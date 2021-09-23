6df32a82ca9fe4c7f635a0900a96a408a0035a2f
Mike Schreiber <mischreiber@microsoft.com>
2021-09-23 13:23:23 -0700

    First checkin of global and alias token structs

---
+++ ios/FluentUI/Core/Theme/Tokens/AliasTokens.swift
@@ -0,0 +1,97 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+struct AliasTokens {
+    enum Background: TokenSet {
+        case neutral1
+        case neutral2
+        case neutral3
+
+        var value: UIColor {
+            return UIColor(light: .white,
+                           dark: .black)
+        }
+    }
+
+    enum Color: TokenSet {
+        case neutralForeground1
+        case neutralForeground2
+
+        var value: UIColor {
+            switch self {
+            case .neutralForeground1:
+                return UIColor(light: GlobalTokens.Color.Neutral.grey14.value,
+                               lightHighContrast: GlobalTokens.Color.Neutral.black.value,
+                               dark: GlobalTokens.Color.Neutral.white.value,
+                               darkHighContrast: GlobalTokens.Color.Neutral.white.value,
+                               darkElevated: GlobalTokens.Color.Neutral.white.value)
+            case .neutralForeground2:
+                return GlobalTokens.Color.Neutral.grey26.value
+            }
+        }
+    }
+    enum Font: TokenSet {
+        case body
+        case caption1
+        case footnote
+        case headline
+        case subheadline
+
+        var value: Font {
+            switch self {
+            case .body:
+                return Font.body
+            case .caption1:
+                return Font.caption1
+            case .footnote:
+                return Font.footnote
+            case .headline:
+                return Font.headline
+            case .subheadline:
+                return Font.subheadline
+            }
+        }
+    }
+
+    enum Shadow: TokenSet {
+        case clear
+        case opacity12
+        case opacity14
+        case opacity20
+        case opacity24
+        case opacity28
+        case opacity40
+        case opacity48
+        case opacity60
+        case opaque
+
+        var value: Int {
+            switch self {
+            case .clear:
+                return 0x00
+            case .opacity12:
+                return 0x1F
+            case .opacity14:
+                return 0x24
+            case .opacity20:
+                return 0x33
+            case .opacity24:
+                return 0x3D
+            case .opacity28:
+                return 0x47
+            case .opacity40:
+                return 0x66
+            case .opacity48:
+                return 0x7A
+            case .opacity60:
+                return 0x99
+            case .opaque:
+                return 0xFF
+            }
+        }
+    }
+}
---
+++ ios/FluentUI/Core/Theme/Tokens/GlobalTokens.swift
@@ -0,0 +1,247 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+/// Template for all token sets, both global and alias. This ensures a unified return type for any given token set.
+protocol TokenSet {
+    associatedtype TokenType
+    var value: TokenType { get }
+}
+
+/// Global Tokens represent a unified set of constants to be used by Fluent UI.
+struct GlobalTokens {
+    struct Color {
+        enum Brand: TokenSet {
+            case primary
+            case shade10
+            case shade20
+            case shade30
+            case tint10
+            case tint20
+            case tint30
+            case tint40
+
+            var value: UIColor {
+                switch self {
+                case .primary:
+                    return UIColor(light: UIColor(hexValue: 0x0078D4),
+                                   dark: UIColor(hexValue: 0x0086F0))
+                case .shade10:
+                    return UIColor(light: UIColor(hexValue: 0x106EBE),
+                                   dark: UIColor(hexValue: 0x1890F1))
+                case .shade20:
+                    return UIColor(light: UIColor(hexValue: 0x005A9E),
+                                   dark: UIColor(hexValue: 0x3AA0F3))
+                case .shade30:
+                    return UIColor(light: UIColor(hexValue: 0x004578),
+                                   dark: UIColor(hexValue: 0x6CB8F6))
+                case .tint10:
+                    return UIColor(light: UIColor(hexValue: 0x2B88D8),
+                                   dark: UIColor(hexValue: 0x0074D3))
+                case .tint20:
+                    return UIColor(light: UIColor(hexValue: 0xC7E0F4),
+                                   dark: UIColor(hexValue: 0x004F90))
+                case .tint30:
+                    return UIColor(light: UIColor(hexValue: 0xDEECF9),
+                                   dark: UIColor(hexValue: 0x002848))
+                case .tint40:
+                    return UIColor(light: UIColor(hexValue: 0xEFF6FC),
+                                   dark: UIColor(hexValue: 0x001526))
+                }
+            }
+        }
+
+        enum Neutral: TokenSet {
+            case black
+            case grey2
+            case grey4
+            case grey6
+            case grey8
+            case grey10
+            case grey12
+            case grey14
+            case grey16
+            case grey18
+            case grey20
+            case grey22
+            case grey24
+            case grey26
+            case grey28
+            case grey30
+            case grey32
+            case grey34
+            case grey36
+            case grey38
+            case grey40
+            case grey42
+            case grey44
+            case grey46
+            case grey48
+            case grey50
+            case grey52
+            case grey54
+            case grey56
+            case grey58
+            case grey60
+            case grey62
+            case grey64
+            case grey66
+            case grey68
+            case grey70
+            case grey72
+            case grey74
+            case grey76
+            case grey78
+            case grey80
+            case grey82
+            case grey84
+            case grey86
+            case grey88
+            case grey90
+            case grey92
+            case grey94
+            case grey96
+            case grey98
+            case white
+
+            var value: UIColor {
+                switch self {
+                case .black:
+                    return UIColor(hexValue: 0x000000)
+                case .grey2:
+                    return UIColor(hexValue: 0x050505)
+                case .grey4:
+                    return UIColor(hexValue: 0x0A0A0A)
+                case .grey6:
+                    return UIColor(hexValue: 0x0F0F0F)
+                case .grey8:
+                    return UIColor(hexValue: 0x141414)
+                case .grey10:
+                    return UIColor(hexValue: 0x1A1A1A)
+                case .grey12:
+                    return UIColor(hexValue: 0x1F1F1F)
+                case .grey14:
+                    return UIColor(hexValue: 0x242424)
+                case .grey16:
+                    return UIColor(hexValue: 0x292929)
+                case .grey18:
+                    return UIColor(hexValue: 0x2E2E2E)
+                case .grey20:
+                    return UIColor(hexValue: 0x333333)
+                case .grey22:
+                    return UIColor(hexValue: 0x383838)
+                case .grey24:
+                    return UIColor(hexValue: 0x3D3D3D)
+                case .grey26:
+                    return UIColor(hexValue: 0x424242)
+                case .grey28:
+                    return UIColor(hexValue: 0x474747)
+                case .grey30:
+                    return UIColor(hexValue: 0x4D4D4D)
+                case .grey32:
+                    return UIColor(hexValue: 0x525252)
+                case .grey34:
+                    return UIColor(hexValue: 0x575757)
+                case .grey36:
+                    return UIColor(hexValue: 0x5C5C5C)
+                case .grey38:
+                    return UIColor(hexValue: 0x616161)
+                case .grey40:
+                    return UIColor(hexValue: 0x666666)
+                case .grey42:
+                    return UIColor(hexValue: 0x6B6B6B)
+                case .grey44:
+                    return UIColor(hexValue: 0x707070)
+                case .grey46:
+                    return UIColor(hexValue: 0x757575)
+                case .grey48:
+                    return UIColor(hexValue: 0x7A7A7A)
+                case .grey50:
+                    return UIColor(hexValue: 0x808080)
+                case .grey52:
+                    return UIColor(hexValue: 0x858585)
+                case .grey54:
+                    return UIColor(hexValue: 0x8A8A8A)
+                case .grey56:
+                    return UIColor(hexValue: 0x8F8F8F)
+                case .grey58:
+                    return UIColor(hexValue: 0x949494)
+                case .grey60:
+                    return UIColor(hexValue: 0x999999)
+                case .grey62:
+                    return UIColor(hexValue: 0x9E9E9E)
+                case .grey64:
+                    return UIColor(hexValue: 0xA3A3A3)
+                case .grey66:
+                    return UIColor(hexValue: 0xA8A8A8)
+                case .grey68:
+                    return UIColor(hexValue: 0xADADAD)
+                case .grey70:
+                    return UIColor(hexValue: 0xB3B3B3)
+                case .grey72:
+                    return UIColor(hexValue: 0xB8B8B8)
+                case .grey74:
+                    return UIColor(hexValue: 0xBDBDBD)
+                case .grey76:
+                    return UIColor(hexValue: 0xC2C2C2)
+                case .grey78:
+                    return UIColor(hexValue: 0xC7C7C7)
+                case .grey80:
+                    return UIColor(hexValue: 0xCCCCCC)
+                case .grey82:
+                    return UIColor(hexValue: 0xD1D1D1)
+                case .grey84:
+                    return UIColor(hexValue: 0xD6D6D6)
+                case .grey86:
+                    return UIColor(hexValue: 0xDBDBDB)
+                case .grey88:
+                    return UIColor(hexValue: 0xE0E0E0)
+                case .grey90:
+                    return UIColor(hexValue: 0xE6E6E6)
+                case .grey92:
+                    return UIColor(hexValue: 0xEBEBEB)
+                case .grey94:
+                    return UIColor(hexValue: 0xF0F0F0)
+                case .grey96:
+                    return UIColor(hexValue: 0xF5F5F5)
+                case .grey98:
+                    return UIColor(hexValue: 0xFAFAFA)
+                case .white:
+                    return UIColor(hexValue: 0xFFFFFF)
+                }
+            }
+        }
+
+    }
+
+    struct Border {
+        enum Radius: TokenSet {
+            case circle
+            case xlarge
+            case large
+            case medium
+            case small
+            case none
+
+            var value: Int {
+                switch self {
+                case .circle:
+                    return 9999
+                case .xlarge:
+                    return 12
+                case .large:
+                    return 8
+                case .medium:
+                    return 4
+                case .small:
+                    return 2
+                case .none:
+                    return 0
+                }
+            }
+        }
+    }
+}
