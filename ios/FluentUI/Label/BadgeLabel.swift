6cbcd1a77fa87507c8d06f99b4e36ee07de81fa6
Prateek Khandelwal <pkhandelwal@microsoft.com>
2021-11-02 12:57:52 +0530

    Fix background color and text color for a badge label

--- ios/FluentUI/Label/BadgeLabel.swift
+++ ios/FluentUI/Label/BadgeLabel.swift
@@ -22,11 +22,21 @@ class BadgeLabel: UILabel {
     /// Base function for initialization
     private func initBase() {
         layer.masksToBounds = true
-        backgroundColor = Colors.Palette.dangerPrimary.color
-        textColor = .white
         textAlignment = .center
         font = UIFont.systemFont(ofSize: Constants.badgeFontSize, weight: .regular)
         isHidden = true
+    }
+
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        updateWindowSpecificColors()
+    }
+
+    private func updateWindowSpecificColors() {
+        if let window = window {
+            textColor = UIColor(light: Colors.primary(for: window), dark: .white)
+            backgroundColor = UIColor(light: .white, dark: Colors.primary(for: window))
+        }
     }
 
     private struct Constants {
