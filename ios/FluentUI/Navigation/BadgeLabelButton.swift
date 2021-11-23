7c501da18084ce4d24e1e2a756aa6f0a50a45ca3
Prateek Khandelwal <pkhandelwal@microsoft.com>
2021-11-23 08:19:30 +0530

    Fix accessibility logic

--- ios/FluentUI/Navigation/BadgeLabelButton.swift
+++ ios/FluentUI/Navigation/BadgeLabelButton.swift
@@ -215,10 +215,12 @@ class BadgeLabelButton: UIButton {
         guard let item = item else {
             return
         }
-        if let itemAccessibilityLabel = item.accessibilityLabel, let badgeAccessibilityLabel = item.badgeAccessibilityLabel {
-            accessibilityLabel = String(format: "Accessibility.BadgeLabelButton.LabelFormat".localized,
-                                        itemAccessibilityLabel,
-                                        badgeAccessibilityLabel)
+        if let badgeAccessibilityLabel = item.badgeAccessibilityLabel {
+            if let itemAccessibilityLabel = item.accessibilityLabel {
+                accessibilityLabel = String.localizedStringWithFormat("Accessibility.BadgeLabelButton.LabelFormat".localized, itemAccessibilityLabel, badgeAccessibilityLabel)
+            } else {
+                accessibilityLabel = badgeAccessibilityLabel
+            }
         } else {
             accessibilityLabel = item.accessibilityLabel
         }
