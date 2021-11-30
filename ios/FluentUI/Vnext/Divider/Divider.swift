91eae8145f1ff365a9bf8de5c0d38413c23abfeb
huwilkes <67026548+huwilkes@users.noreply.github.com>
2021-11-30 12:22:50 -0800

    Add Fluent Divider (#803)
    * Add tokenized Divider
    
    Add a new SwiftUI Separator that uses the token system. Also, change the name from separator to divider.
    
    * Address various comments from the first iteration
    
    Moved the new Divider under the vNext folder
    Separated out MSFDivider
    Removed switch from body function in favor of ternary operators
    Added custom color (black) demo
    Switched from width and height of frame to maxWidth and maxHeight
    Removed shadow style (which then means removing MSFDividerStyle in general since now there's only one)
    Added missing spaces around {} and added new lines to longer parameter lists
    Added initializer to MSFDivider with theme
    Added .designTokens modifier to FluentDivider
    
    * Add Swift UI demo
    
    Added a Swift UI demo, along with changing the existing demo controller to use UITableViewController rather than DemoController
    
    Changed the custom color demo to communication blue so it would have dark mode support
    
    Fixed the tokens looking for the .rest instead of just the color
    
    Fixed MSFDivider trying to recursively return its own window rather than its view's window
    
    * Add subspec for Divider
    
    Add a subspec for the divider to MicrosoftFluentUIVnext.podspec
    
    Remove background color MSFDivider's view and add a constraint to set the height or width to one device pixel if the divider is horizontal or vertical respectively.
    
    Added margins to the vertical stack in the demo to make it easier to actually see the dividers while not adding spacing to the stack that would be confused for the spacing of the dividers.
    Added text between the vertical dividers in the demo
    Fixed some ambiguity in the horizontal stack by setting the distribution to equal centering
    
    * Align SwiftUI Demo with other demos
    
    Changed the SwiftUI demo to have 5 dividers and two strings to match the UIKit demo and better show the divider in action, rather than just a line
    
    Add padding to height constraint of the hosting UIView to bring back the missing medium dividers
    
    * Add a call in MSFDivider to disable Safe Area insets on the hosting controller
    
    Add a call in MSFDivider to disable Safe Area insets on the hosting controller

---
+++ ios/FluentUI/Vnext/Divider/Divider.swift
@@ -0,0 +1,91 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import UIKit
+import SwiftUI
+
+@objc public enum MSFDividerOrientation: Int {
+    case horizontal
+    case vertical
+}
+
+/// Properties that can be used to customize the appearance of the Divider.
+@objc public protocol MSFDividerState {
+    /// Sets a custom color for the Divider.
+    var color: UIColor? { get set }
+
+    /// Defines the orientation of the Divider.
+    var orientation: MSFDividerOrientation { get set }
+
+    /// Defines the spacing of the Divider.
+    var spacing: MSFDividerSpacing { get set }
+}
+
+/// View that represents the Divider.
+public struct FluentDivider: View {
+    @Environment(\.theme) var theme: FluentUIStyle
+    @Environment(\.windowProvider) var windowProvider: FluentUIWindowProvider?
+    @ObservedObject var state: MSFDividerStateImpl
+    @ObservedObject var tokens: MSFDividerTokens
+
+    /// Creates and initializes a SwiftUI Divider
+    /// - Parameters:
+    ///   - orientation: The DividerOrientation used by the Divider.
+    ///   - spacing: The DividerSpacing used by the Divider.
+    public init (orientation: MSFDividerOrientation = .horizontal,
+                 spacing: MSFDividerSpacing = .none) {
+        let state = MSFDividerStateImpl(orientation: orientation, spacing: spacing)
+
+        self.state = state
+        self.tokens = state.tokens
+    }
+
+    public var body: some View {
+        let isHorizontal = state.orientation == .horizontal
+
+        return Rectangle()
+            .fill(Color(state.color ?? tokens.color))
+            .frame(maxWidth: isHorizontal ? .infinity : thickness,
+                   maxHeight: isHorizontal ? thickness : .infinity)
+            .padding(isHorizontal ?
+                     EdgeInsets(top: tokens.padding,
+                                leading: 0,
+                                bottom: tokens.padding,
+                                trailing: 0)
+                     : EdgeInsets(top: 0,
+                                  leading: tokens.padding,
+                                  bottom: 0,
+                                  trailing: tokens.padding))
+            .designTokens(tokens,
+                          from: theme,
+                          with: windowProvider)
+    }
+
+    private let thickness: CGFloat = UIScreen.main.devicePixel
+}
+
+/// Properties available to customize the Divider.
+class MSFDividerStateImpl: NSObject, ObservableObject, MSFDividerState {
+    @Published var color: UIColor?
+    @Published var orientation: MSFDividerOrientation
+
+    var spacing: MSFDividerSpacing {
+        get {
+            return tokens.spacing
+        }
+        set {
+            tokens.spacing = newValue
+        }
+    }
+
+    var tokens: MSFDividerTokens
+
+    init(orientation: MSFDividerOrientation,
+         spacing: MSFDividerSpacing) {
+        self.orientation = orientation
+        tokens = MSFDividerTokens(spacing: spacing)
+        super.init()
+    }
+}
---
+++ ios/FluentUI/Vnext/Divider/DividerTokens.swift
@@ -0,0 +1,46 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+import UIKit
+
+/// Pre-defined spacings of the divider
+@objc public enum MSFDividerSpacing: Int {
+    case none
+    case medium
+}
+
+/// Representation of design tokens to dividers at runtime which interfaces with the Design Token System auto-generated code
+/// Updating these properties causes the SwiftUI divider to updates its view automatically
+class MSFDividerTokens: MSFTokensBase, ObservableObject {
+    @Published public var color: UIColor!
+    @Published public var padding: CGFloat!
+
+    var spacing: MSFDividerSpacing
+
+    init(spacing: MSFDividerSpacing) {
+        self.spacing = spacing
+
+        super.init()
+        updateForCurrentTheme()
+    }
+
+    @objc open func didChangeAppearanceProxy() {
+        updateForCurrentTheme()
+    }
+
+    override func updateForCurrentTheme() {
+        let appearanceProxy = theme.MSFDividerTokens
+
+        color = appearanceProxy.color
+
+        switch spacing {
+        case .none:
+            padding = appearanceProxy.spacing.none
+        case .medium:
+            padding = appearanceProxy.spacing.medium
+        }
+    }
+}
