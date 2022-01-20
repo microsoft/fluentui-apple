6aea600b3a250ac77571ae78c2e94086b997c806
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-01-19 16:31:16 -0800

    Add new DividerTokens
    Add new Divider Tokens based on the new token system

--- ios/FluentUI/Vnext/Divider/DividerTokens.swift
+++ ios/FluentUI/Vnext/Divider/DividerTokens.swift
@@ -44,3 +44,42 @@ class MSFDividerTokens: MSFTokensBase, ObservableObject {
         }
     }
 }
+
+/// Design token set for the `FluentDivider` control.
+public class DividerTokens: ControlTokens {
+    /// Creates an instance of `MSFDividerTokens`with optional token value overrides.
+    /// - Parameters:
+    ///   - spacing: MSFDividerSpacing enumeration value that will define pre-defined value for the padding
+    ///   - padding: CGFloat that defines the padding around the Fluent Divider
+    ///   - color: The color of the Fluent Divider
+    public init(spacing: MSFDividerSpacing = .none,
+                padding: CGFloat? = nil,
+                color: DynamicColor? = nil) {
+        self.spacing = spacing
+        super.init()
+
+        // Optional overrides
+        if let padding = padding {
+            self.padding = padding
+        }
+
+        if let color = color {
+            self.color = color
+        }
+    }
+
+    // MARK: - Design Tokens
+
+    let spacing: MSFDividerSpacing
+
+    lazy var padding: CGFloat = {
+        switch spacing {
+        case .none:
+            return globalTokens.spacing[.none]
+        case .medium:
+            return globalTokens.spacing[.medium]
+        }
+    }()
+
+    lazy var color: DynamicColor = aliasTokens.strokeColors[.neutral2]
+}
