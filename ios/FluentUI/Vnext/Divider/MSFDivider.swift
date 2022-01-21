89e08cb041a8b023f065e53896f837f1e65d6120
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-01-21 12:47:40 -0800

    Remove MSFDivider init with FluentUIStyle param
    We no longer need the FluentUIStyle param with the new token system, so simplifying MSFDivider to have just the one init.

--- ios/FluentUI/Vnext/Divider/MSFDivider.swift
+++ ios/FluentUI/Vnext/Divider/MSFDivider.swift
@@ -15,21 +15,8 @@ import SwiftUI
     ///  - Parameters:
     ///   - orientation: The DividerOrientation used by the Divider.
     ///   - spacing: The DividerSpacing used by the Divider.
-    @objc public convenience init(orientation: MSFDividerOrientation = .horizontal,
-                                  spacing: MSFDividerSpacing = .none) {
-        self.init(orientation: orientation,
-                  spacing: spacing,
-                  theme: nil)
-    }
-
-    /// Creates a new MSFDivider instance.
-    ///  - Parameters:
-    ///   - orientation: The DividerOrientation used by the Divider.
-    ///   - spacing: The DividerSpacing used by the Divider.
-    ///   - theme: The FluentUIStyle instance representing the theme to be overriden for this Divider.
-    @objc public init(orientation: MSFDividerOrientation,
-                      spacing: MSFDividerSpacing,
-                      theme: FluentUIStyle?) {
+    @objc public init(orientation: MSFDividerOrientation = .horizontal,
+                      spacing: MSFDividerSpacing = .none) {
         let divider = FluentDivider(orientation: orientation, spacing: spacing)
         state = divider.state
         super.init(AnyView(divider))
