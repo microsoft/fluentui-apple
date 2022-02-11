c2e5d7c714ffc0d7761178b4195a052bfdf44144
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-02-10 16:37:40 -0800

    Move ButtonDynamicColors to its own file

---
+++ ios/FluentUI/Core/Theme/Tokens/ButtonDynamicColors.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+/// Represents the set of `DynamicColor` values for the various states of a button.
+public struct ButtonDynamicColors {
+    let rest: DynamicColor
+    let hover: DynamicColor
+    let pressed: DynamicColor
+    let selected: DynamicColor
+    let disabled: DynamicColor
+}
