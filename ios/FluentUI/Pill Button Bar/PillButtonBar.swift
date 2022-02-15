558db826d86e7f30f85ae844f13b041f3114fc84
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-02-14 18:10:32 -0800

    Remove custom colors from Pill Button Bar (and extra inits)

--- ios/FluentUI/Pill Button Bar/PillButtonBar.swift
+++ ios/FluentUI/Pill Button Bar/PillButtonBar.swift
@@ -82,7 +82,8 @@ open class PillButtonBar: UIScrollView, FluentUIWindowProvider {
     }
 
     @objc public let pillButtonStyle: PillButtonStyle
-    @objc public var pillButtonOverrideTokens: PillButtonTokens? = nil
+
+    @objc public var pillButtonOverrideTokens: PillButtonTokens?
 
     /// If set to nil, the previously selected item will be deselected and there won't be any items selected
     @objc public var selectedItem: PillButtonBarItem? {
@@ -125,12 +126,6 @@ open class PillButtonBar: UIScrollView, FluentUIWindowProvider {
         return view
     }
 
-    private var customPillButtonBackgroundColor: UIColor?
-    private var customSelectedPillButtonBackgroundColor: UIColor?
-    private var customPillButtonTextColor: UIColor?
-    private var customSelectedPillButtonTextColor: UIColor?
-    private var customPillButtonUnreadDotColor: UIColor?
-
     private var leadingConstraint: NSLayoutConstraint?
 
     private var centerConstraint: NSLayoutConstraint?
@@ -153,75 +148,12 @@ open class PillButtonBar: UIScrollView, FluentUIWindowProvider {
         }
     }
 
-    /// Initializes the PillButtonBar using the provided style and color overrides.
+    /// Initializes the PillButtonBar using the provided style.
     ///
     /// - Parameters:
     ///   - pillButtonStyle: The style override for the pill buttons in this pill button bar
-    @objc public convenience init(pillButtonStyle: PillButtonStyle = .primary) {
-        self.init(pillButtonStyle: pillButtonStyle,
-                  pillButtonBackgroundColor: nil,
-                  selectedPillButtonBackgroundColor: nil,
-                  pillButtonTextColor: nil,
-                  selectedPillButtonTextColor: nil,
-                  pillButtonUnreadDotColor: nil)
-    }
-
-    /// Initializes the PillButtonBar using the provided style and color overrides.
-    ///
-    /// - Parameters:
-    ///   - pillButtonStyle: The style override for the pill buttons in this pill button bar
-    ///   - pillButtonBackgroundColor: The color override for the background color of the pill buttons
-    @objc public convenience init(pillButtonStyle: PillButtonStyle = .primary,
-                                  pillButtonBackgroundColor: UIColor? = nil) {
-        self.init(pillButtonStyle: pillButtonStyle,
-                  pillButtonBackgroundColor: pillButtonBackgroundColor,
-                  selectedPillButtonBackgroundColor: nil,
-                  pillButtonTextColor: nil,
-                  selectedPillButtonTextColor: nil,
-                  pillButtonUnreadDotColor: nil)
-    }
-
-    /// Initializes the PillButtonBar using the provided style and color overrides.
-    ///
-    /// - Parameters:
-    ///   - pillButtonStyle: The style override for the pill buttons in this pill button bar
-    ///   - pillButtonBackgroundColor: The color override for the background color of the pill buttons
-    ///   - selectedPillButtonBackgroundColor: The color override for the background color of the selected pill button
-    ///   - pillButtonTextColor: The color override for the text of the pill buttons
-    ///   - selectedPillButtonTextColor: The color override for the text of the selected pill button
-    ///   - pillButtonUnreadDotColor: The color override for the unread dot for the pill buttons
-    @objc public convenience init(pillButtonStyle: PillButtonStyle = .primary,
-                                  pillButtonBackgroundColor: UIColor? = nil,
-                                  selectedPillButtonBackgroundColor: UIColor? = nil,
-                                  pillButtonTextColor: UIColor? = nil,
-                                  selectedPillButtonTextColor: UIColor? = nil,
-                                  pillButtonUnreadDotColor: UIColor? = nil) {
-        self.init(pillButtonStyle: pillButtonStyle,
-                  pillButtonBackgroundColor: pillButtonBackgroundColor,
-                  selectedPillButtonBackgroundColor: selectedPillButtonBackgroundColor,
-                  pillButtonTextColor: pillButtonTextColor,
-                  selectedPillButtonTextColor: selectedPillButtonTextColor)
-        self.customPillButtonUnreadDotColor = pillButtonUnreadDotColor
-    }
-
-    /// Initializes the PillButtonBar using the provided style and color overrides.
-    ///
-    /// - Parameters:
-    ///   - pillButtonStyle: The style override for the pill buttons in this pill button bar
-    ///   - pillButtonBackgroundColor: The color override for the background color of the pill buttons
-    ///   - selectedPillButtonBackgroundColor: The color override for the background color of the selected pill button
-    ///   - pillButtonTextColor: The color override for the text of the pill buttons
-    ///   - selectedPillButtonTextColor: The color override for the text of the selected pill button
-    @objc public init(pillButtonStyle: PillButtonStyle = .primary,
-                      pillButtonBackgroundColor: UIColor? = nil,
-                      selectedPillButtonBackgroundColor: UIColor? = nil,
-                      pillButtonTextColor: UIColor? = nil,
-                      selectedPillButtonTextColor: UIColor? = nil) {
+    @objc public init(pillButtonStyle: PillButtonStyle = .primary) {
         self.pillButtonStyle = pillButtonStyle
-        self.customPillButtonBackgroundColor = pillButtonBackgroundColor
-        self.customSelectedPillButtonBackgroundColor = selectedPillButtonBackgroundColor
-        self.customPillButtonTextColor = pillButtonTextColor
-        self.customSelectedPillButtonTextColor = selectedPillButtonTextColor
         super.init(frame: .zero)
         setupScrollView()
         setupStackView()
@@ -586,7 +518,7 @@ extension PillButtonBar: UIPointerInteractionDelegate {
         guard let index = region.identifier as? Int else {
             return
         }
-        if customPillButtonBackgroundColor == nil, index < buttons.count {
+        if pillButtonOverrideTokens == nil, index < buttons.count {
             let pillButton = buttons[index]
             if !pillButton.isSelected {
                 pillButton.isHighlighted = true
@@ -598,7 +530,7 @@ extension PillButtonBar: UIPointerInteractionDelegate {
         guard let index = region.identifier as? Int else {
             return
         }
-        if customPillButtonBackgroundColor == nil && index < buttons.count {
+        if pillButtonOverrideTokens == nil && index < buttons.count {
             let pillButton = buttons[index]
             pillButton.isHighlighted = false
         }
