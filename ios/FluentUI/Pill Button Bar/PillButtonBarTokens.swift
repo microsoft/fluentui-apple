ae39369ac2cb3debb3c8c107d592753e1eb2a37a
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-02-16 18:23:39 -0800

    Remove old PillButtonBar tokens from Fluent project

--- ios/FluentUI/Pill Button Bar/PillButtonBarTokens.swift
+++ ios/FluentUI/Pill Button Bar/PillButtonBarTokens.swift
@@ -5,44 +5,6 @@
 
 import UIKit
 
-class MSFPillButtonBarTokens: MSFTokensBase {
-    public var cornerRadius: CGFloat!
-    public var maxButtonsSpacing: CGFloat!
-    public var minButtonsSpacing: CGFloat!
-    public var minButtonVisibleWidth: CGFloat!
-    public var minButtonWidth: CGFloat!
-    public var minHeight: CGFloat!
-    public var sideInset: CGFloat!
-    public var hoverBackgroundColor: UIColor!
-    public var onBrandHoverBackgroundColor: UIColor!
-
-    override init() {
-        super.init()
-
-        self.themeAware = true
-
-        updateForCurrentTheme()
-    }
-
-    @objc open func didChangeAppearanceProxy() {
-        updateForCurrentTheme()
-    }
-
-    override func updateForCurrentTheme() {
-        let appearanceProxy = theme.MSFPillButtonBarTokens
-
-        cornerRadius = appearanceProxy.cornerRadius
-        maxButtonsSpacing = appearanceProxy.maxButtonsSpacing
-        minButtonsSpacing = appearanceProxy.minButtonsSpacing
-        minButtonVisibleWidth = appearanceProxy.minButtonVisibleWidth
-        minButtonWidth = appearanceProxy.minButtonWidth
-        minHeight = appearanceProxy.minHeight
-        sideInset = appearanceProxy.sideInset
-        hoverBackgroundColor = appearanceProxy.hoverBackgroundColor.primary
-        onBrandHoverBackgroundColor = appearanceProxy.hoverBackgroundColor.onBrand
-    }
-}
-
 /// Design token set for the `PillButtonBar` control
 open class PillButtonBarTokens: ControlTokens {
     /// Maximum spacing between `PillButton` controls
