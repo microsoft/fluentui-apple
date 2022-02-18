f2f3e8d890b41f3b00c4de3c0159dea12e1cc3d6
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-02-17 18:04:37 -0800

    Update MSFAvatar to Control Hosting Container
    Also had to make some changes in the theming demo.

--- ios/FluentUI/Avatar/MSFAvatar.swift
+++ ios/FluentUI/Avatar/MSFAvatar.swift
@@ -7,56 +7,21 @@ import UIKit
 import SwiftUI
 
 /// UIKit wrapper that exposes the SwiftUI Avatar implementation.
-@objc open class MSFAvatar: NSObject, FluentUIWindowProvider {
-
-    /// The UIView representing the Avatar.
-    @objc open var view: UIView {
-        return hostingController.view
-    }
-
-    /// The object that groups properties that allow control over the Avatar appearance.
-    @objc open var state: MSFAvatarState {
-        return avatar.state
-    }
+@objc public class MSFAvatar: ControlHostingContainer {
 
     /// Creates a new MSFAvatar instance.
     /// - Parameters:
     ///   - style: The MSFAvatarStyle value used by the Avatar.
     ///   - size: The MSFAvatarSize value used by the Avatar.
-    @objc public convenience init(style: MSFAvatarStyle = .default,
-                                  size: MSFAvatarSize = .large) {
-        self.init(style: style,
-                  size: size,
-                  theme: nil)
-    }
-
-    /// Creates a new MSFAvatar instance.
-    /// - Parameters:
-    ///   - style: The MSFAvatarStyle value used by the Avatar.
-    ///   - size: The MSFAvatarSize value used by the Avatar.
-    ///   - theme: The FluentUIStyle instance representing the theme to be overriden for this Avatar.
-    @objc public init(style: MSFAvatarStyle,
-                      size: MSFAvatarSize,
-                      theme: FluentUIStyle?) {
-        super.init()
-
-        avatar = Avatar(style: style,
+    @objc public init(style: MSFAvatarStyle = .default,
+                      size: MSFAvatarSize = .large) {
+        let avatar = Avatar(style: style,
                         size: size)
-        hostingController = FluentUIHostingController(rootView: AnyView(avatar
-                                                                            .windowProvider(self)
-                                                                            .modifyIf(theme != nil, { avatar in
-                                                                                avatar.customTheme(theme!)
-                                                                            })))
-        hostingController.disableSafeAreaInsets()
+        state = avatar.state
+        super.init(AnyView(avatar))
         view.backgroundColor = UIColor.clear
     }
 
-    var window: UIWindow? {
-        return self.view.window
-    }
-
-    private var hostingController: FluentUIHostingController!
-
-    private var avatar: Avatar!
+    /// The object that groups properties that allow control over the Avatar appearance.
+    @objc public let state: MSFAvatarState
 }
-
