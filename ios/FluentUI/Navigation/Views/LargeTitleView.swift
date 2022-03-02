22f45009ef0ba47a1bb39237425c5f488436a1d4
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-03-01 16:28:12 -0800

    Remove avatar height / width constraints from LargeTitleView

--- ios/FluentUI/Navigation/Views/LargeTitleView.swift
+++ ios/FluentUI/Navigation/Views/LargeTitleView.swift
@@ -199,28 +199,6 @@ class LargeTitleView: UIView {
 
         avatarView.centerYAnchor.constraint(equalTo: contentStackView.centerYAnchor).isActive = true
 
-        let avatarHeightConstraint = NSLayoutConstraint(item: avatarView,
-                                                        attribute: .height,
-                                                        relatedBy: .equal,
-                                                        toItem: nil,
-                                                        attribute: .notAnAttribute,
-                                                        multiplier: 1,
-                                                        constant: Constants.avatarSize.size)
-        avatarView.addConstraint(avatarHeightConstraint)
-        avatarHeightConstraint.isActive = true
-        self.avatarHeightConstraint = avatarHeightConstraint
-
-        let avatarWidthConstraint = NSLayoutConstraint(item: avatarView,
-                                                       attribute: .width,
-                                                       relatedBy: .equal,
-                                                       toItem: nil,
-                                                       attribute: .notAnAttribute,
-                                                       multiplier: 1,
-                                                       constant: Constants.avatarSize.size)
-        avatarView.addConstraint(avatarWidthConstraint)
-        avatarWidthConstraint.isActive = true
-        self.avatarWidthConstraint = avatarWidthConstraint
-
         // title button setup
         contentStackView.addArrangedSubview(titleButton)
         titleButton.setTitle(nil, for: .normal)
@@ -249,8 +227,6 @@ class LargeTitleView: UIView {
 
         if avatarSize == .automatic {
             avatar?.state.size = Constants.avatarSize
-            avatarWidthConstraint?.constant = Constants.avatarSize.size
-            avatarHeightConstraint?.constant = Constants.avatarSize.size
         }
 
         layoutIfNeeded()
@@ -263,8 +239,6 @@ class LargeTitleView: UIView {
 
         if avatarSize == .automatic {
             avatar?.state.size = Constants.compactAvatarSize
-            avatarWidthConstraint?.constant = Constants.compactAvatarSize.size
-            avatarHeightConstraint?.constant = Constants.compactAvatarSize.size
         }
 
         layoutIfNeeded()
