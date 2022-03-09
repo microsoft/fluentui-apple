a7795f1ad6c85f4e4e1933b3c22a6c01d802d844
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-03-08 16:20:27 -0800

    Move Segmented Control demo window/theme background color update to a new class
    The previous implementation would load blue when first launching the Segmented Control demo even if the theme was set to green because the window wasn't ready in viewDidLoad.

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/SegmentedControlDemoController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/SegmentedControlDemoController.swift
@@ -55,11 +55,6 @@ class SegmentedControlDemoController: DemoController {
         addTitle(text: "Disabled On Brand Pill")
 
         addPillControl(items: Array(segmentItems.prefix(2)), style: .onBrandPill, enabled: false)
-
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(themeDidChange),
-                                               name: .didChangeTheme,
-                                               object: nil)
     }
 
     @objc func updateLabel(forControl control: SegmentedControl) {
@@ -72,13 +67,7 @@ class SegmentedControlDemoController: DemoController {
         pillControl.isEnabled = enabled
         pillControl.addTarget(self, action: #selector(updateLabel(forControl:)), for: .valueChanged)
 
-        let backgroundView = UIView()
-        if style == .primaryPill {
-            backgroundView.backgroundColor = Colors.navigationBarBackground
-        } else {
-            setOnBrandBackgroundColorFor(backgroundView)
-            onBrandSegmentedControlViews.append(backgroundView)
-        }
+        let backgroundView = BackgroundView(style: style)
         segmentedControls.append(pillControl)
 
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
@@ -99,28 +88,7 @@ class SegmentedControlDemoController: DemoController {
         }
     }
 
-    @objc func themeDidChange(_ notification: Notification) {
-        for background in onBrandSegmentedControlViews {
-            setOnBrandBackgroundColorFor(background)
-        }
-    }
-
-    func setOnBrandBackgroundColorFor(_ backgroundView: UIView) {
-        var lightColor: UIColor
-        var darkColor: UIColor
-        if let window = self.view.window {
-            lightColor = Colors.primary(for: window)
-            darkColor = UIColor(colorValue: window.fluentTheme.globalTokens.neutralColors[.grey12])
-        } else {
-            lightColor = Colors.communicationBlue
-            darkColor = Colors.navigationBarBackground
-        }
-
-        backgroundView.backgroundColor = UIColor(light: lightColor, dark: darkColor)
-    }
-
     private var segmentedControls: [SegmentedControl] = []
-    private var onBrandSegmentedControlViews: [UIView] = []
 }
 
 extension SegmentedControlDemoController: DemoAppearanceDelegate {
@@ -163,4 +131,57 @@ extension SegmentedControlDemoController: DemoAppearanceDelegate {
             return FontInfo(name: "Papyrus", size: 10.0, weight: .regular)
         }
     }
+}
+
+class BackgroundView: UIView {
+    init(style: SegmentedControlStyle) {
+        self.style = style
+        super.init(frame: .zero)
+        updateBackgroundColor()
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(themeDidChange),
+                                               name: .didChangeTheme,
+                                               object: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        preconditionFailure("init(coder:) has not been implemented")
+    }
+
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        updateBackgroundColor()
+    }
+
+    @objc func themeDidChange() {
+        updateBackgroundColor()
+    }
+
+    func updateBackgroundColor() {
+        var lightColor: UIColor
+        var darkColor: UIColor
+        switch style {
+        case .primaryPill:
+            if let fluentTheme = window?.fluentTheme {
+                lightColor = UIColor(dynamicColor: fluentTheme.aliasTokens.backgroundColors[.neutral1])
+                darkColor = UIColor(dynamicColor: fluentTheme.aliasTokens.backgroundColors[.neutral4])
+            } else {
+                lightColor = Colors.navigationBarBackground
+                darkColor = Colors.navigationBarBackground
+            }
+        case .onBrandPill:
+            if let fluentTheme = window?.fluentTheme {
+                lightColor = UIColor(dynamicColor: fluentTheme.aliasTokens.backgroundColors[.brandRest])
+                darkColor = UIColor(dynamicColor: fluentTheme.aliasTokens.backgroundColors[.neutral4])
+            } else {
+                lightColor = Colors.communicationBlue
+                darkColor = Colors.navigationBarBackground
+            }
+        }
+
+        backgroundColor = UIColor(light: lightColor, dark: darkColor)
+    }
+
+    let style: SegmentedControlStyle
 }
