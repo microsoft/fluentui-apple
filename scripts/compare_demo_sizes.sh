bf90dc99cd9e3483ed4c8256e7b67e2b8fda02c8
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-03-25 14:47:37 -0700

    Remove some $GITHUB_WORKSPACEs

--- scripts/compare_demo_sizes.sh
+++ scripts/compare_demo_sizes.sh
@@ -2,7 +2,7 @@
 
 #simple scripts used to compare sizes of the iOS device release demo app
 
-headSize=$(<$GITHUB_WORKSPACE/headSize)
-baseSize=$(<$GITHUB_WORKSPACE/baseSize)
+headSize=$(<headSize)
+baseSize=$(<baseSize)
 difference=$(($baseSize-$headSize))
 echo "The FluentUI Demo app went from $headSize kB to $baseSize kB, resulting in a change of $difference kB"
