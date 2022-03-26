2f24e69cdfb2950e2abbfdc37d13dbcacd62493a
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-03-25 17:03:20 -0700

    Fix paths of artifact files

--- scripts/compare_demo_sizes.sh
+++ scripts/compare_demo_sizes.sh
@@ -2,7 +2,7 @@
 
 #simple scripts used to compare sizes of the iOS device release demo app
 
-headSize=$(<headSize)
-baseSize=$(<baseSize)
+headSize=$(<headSize/headSize)
+baseSize=$(<baseSize/baseSize)
 difference=$(($baseSize-$headSize))
 echo "The FluentUI Demo app went from $headSize kB to $baseSize kB, resulting in a change of $difference kB"
