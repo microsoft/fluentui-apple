95bf08cfc5bcc5f030aac6f44dde77900692f717
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-03-28 16:11:25 -0700

    Update compare script to not think artifacts are directories
    Seems like between v1 and v3, they changed the way artifacts are stored/downloaded, so now it's not nested in a directory.

--- scripts/compare_demo_sizes.sh
+++ scripts/compare_demo_sizes.sh
@@ -2,7 +2,7 @@
 
 #simple scripts used to compare sizes of the iOS device release demo app
 
-headSize=$(<headSize/headSize)
-baseSize=$(<baseSize/baseSize)
+headSize=$(<headSize)
+baseSize=$(<baseSize)
 difference=$(($baseSize-$headSize))
 echo "The FluentUI Demo app went from $headSize kB to $baseSize kB, resulting in a change of $difference kB"
