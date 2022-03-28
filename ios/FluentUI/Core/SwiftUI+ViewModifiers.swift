093cf9762c8f8886434e900be2d173d9b2e27d6d
Rafael Amaral de Assis <rdeassis@microsoft.com>
2022-03-28 14:40:10 -0700

    [FHL] Introducing the SquareShapedViewModifier.

--- ios/FluentUI/Core/SwiftUI+ViewModifiers.swift
+++ ios/FluentUI/Core/SwiftUI+ViewModifiers.swift
@@ -5,6 +5,38 @@
 
 import SwiftUI
 
+struct SquareShapedViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        let modifiedContent = HStack {
+            content
+                .alignmentGuide(HorizontalAlignment.center) { (viewDimensions) -> CGFloat in
+                    DispatchQueue.main.async {
+                        self.size = max(size, // ensures the View does not shrink
+                                        min(maxSize,
+                                            max(minSize,
+                                                max(viewDimensions.height,
+                                                    viewDimensions.width))))
+                    }
+
+                    return viewDimensions[HorizontalAlignment.center]
+                }
+        }
+
+        modifiedContent
+            .frame(width: size, height: size)
+    }
+
+    init(minSize: CGFloat, maxSize: CGFloat) {
+        self.size = minSize
+        self.minSize = minSize
+        self.maxSize = maxSize
+    }
+
+    @State var size: CGFloat
+    var minSize: CGFloat
+    var maxSize: CGFloat
+}
+
 extension View {
     /// Applies modifiers defined in a closure if a condition is met.
     /// - Parameters:
@@ -30,5 +62,17 @@ extension View {
         }
 
         return AnyView(self)
+    }
+
+    /// Ensures that the modified View has equal height and width values based on the
+    /// maximum of either dimension considering its dynamic content size.
+    /// Minimum and maximum cap values need to be specified.
+    /// - Parameters:
+    ///   - minSize: Minimum heigh/width value of the view.
+    ///   - maxSize: Maximum heigh/width value of the view.
+    /// - Returns: Modified view in a square shaped format.
+    func squareShaped(minSize: CGFloat, maxSize: CGFloat) -> some View {
+        modifier(SquareShapedViewModifier(minSize: minSize,
+                                          maxSize: maxSize))
     }
 }
