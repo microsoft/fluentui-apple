e1f13c2f0da86822c70f2acd366c816b6a4b06f5
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-04-28 16:17:44 -0400

    [PreviewCard] Updated PreviewCard to display flat and elevated cards (#976)
    * Add and display PreviewCard view with rounded box
    
    * Updated PreviewCard & PreviewCardDemoController w/documentation and fixed values
    
    * Updated variable titles, properly ordered constant structs
    
    * [PreviewCard] Display PreviewCard view, add rounded box inside view (#963)
    
    * Add and display PreviewCard view with rounded box
    
    * Updated PreviewCard & PreviewCardDemoController w/documentation and fixed values
    
    * Updated variable titles, properly ordered constant structs
    
    * Updated PreviewCard to display flat & elevated card
    
    * Removed UIColor usage
    
    * Removed TODO comment

--- ios/FluentUI/PreviewCard/PreviewCard.swift
+++ ios/FluentUI/PreviewCard/PreviewCard.swift
@@ -15,9 +15,9 @@ import SwiftUI
     /// Creates a new MSFPreviewCard instance.
     /// - Parameters:
     ///   - theme: The FluentUIStyle instance representing the theme to be overriden for this PreviewCard.
-    @objc public init(theme: FluentUIStyle?) {
+    @objc public init(theme: FluentUIStyle?, isElevated: Bool = false) {
         super.init()
-        previewCardView = PreviewCard()
+        previewCardView = PreviewCard(isElevated: isElevated)
         hostingController = FluentUIHostingController(rootView: AnyView(previewCardView.windowProvider(self)))
         hostingController.disableSafeAreaInsets()
     }
@@ -30,7 +30,7 @@ import SwiftUI
 
 /// View that represents the PreviewCard.
 public struct PreviewCard: View {
-
+    var isElevated: Bool
     @Environment(\.theme) var theme: FluentUIStyle
     @Environment(\.windowProvider) var windowProvider: FluentUIWindowProvider?
     /// Creates inner PreviewCard view.
@@ -38,28 +38,48 @@ public struct PreviewCard: View {
     var innerContents: some View {
         HStack {
             Spacer()
+                /// Sets the height and width of the Preview Card.
                 .frame(minWidth: Constants.cardMinWidth, minHeight: Constants.cardMinHeight)
-        }
+                /// Sets the background border style, radius, color, and linewidth of the Preview Card.
+                .background(
+                    RoundedRectangle(cornerRadius: Constants.cardCornerRadius)
+                        .strokeBorder(Color(red: Constants.cardStrokeBorderColor,
+                                            green: Constants.cardStrokeBorderColor,
+                                            blue: Constants.cardStrokeBorderColor),
+                                      lineWidth: Constants.cardLineWidth)
+                /// Modifies the background shadow of PreviewCard if the card is elevated.
+                .modifyIf(isElevated, { view in view
+                        .shadow(color: .black.opacity(Constants.cardAmbientAlphaColor),
+                                radius: Constants.cardShadowRadius,
+                                x: Constants.ambientShadowOffsetX,
+                                y: Constants.ambientShadowOffsetY)
+                        .shadow(color: .black.opacity(Constants.cardPerimeterAlphaColor),
+                                radius: Constants.cardShadowRadius,
+                                x: Constants.perimeterShadowOffsetX,
+                                y: Constants.perimeterShadowOffsetY)
+                })
+       )}
     }
     /// Creates the PreviewCard.
     public var body: some View {
-// TODO: - Implement elevated card
 // TODO: - Update to implement light and dark mode
         innerContents
-            .background(
-                RoundedRectangle(cornerRadius: Constants.cardCornerRadius)
-                    .strokeBorder(Color(.black), lineWidth: Constants.cardLineWidth)
-                    .background(
-                        RoundedRectangle(cornerRadius: Constants.cardCornerRadius)
-                            .fill(.white)
-                    )
-    )}
+    }
 // MARK: - PreviewCard Box Properties
     // TODO: - Update the hard values with tokens
-    private struct Constants {
+    struct Constants {
         static let cardMinWidth: CGFloat = 343
         static let cardMinHeight: CGFloat = 147
         static let cardCornerRadius: CGFloat = 8
         static let cardLineWidth: CGFloat = 0.5
+        static let cardBottomPadding: CGFloat = 20
+        static let cardShadowRadius: CGFloat = 2
+        static let ambientShadowOffsetX: CGFloat = 0
+        static let ambientShadowOffsetY: CGFloat = 1
+        static let perimeterShadowOffsetX: CGFloat = 0
+        static let perimeterShadowOffsetY: CGFloat = 0
+        static let cardStrokeBorderColor: Double = 0x61 / 255.0
+        static let cardAmbientAlphaColor: CGFloat = 0.14
+        static let cardPerimeterAlphaColor: CGFloat = 0.12
     }
 }
