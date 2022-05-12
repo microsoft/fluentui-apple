96f0e0ba42503df678753d38ba7062b5ba2bfa1e
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-05-11 17:44:30 -0700

    Remove TODO comment

--- ios/FluentUI/ResizingHandleView/ResizingHandleTokens.swift
+++ ios/FluentUI/ResizingHandleView/ResizingHandleTokens.swift
@@ -10,7 +10,6 @@ import SwiftUI
 open class ResizingHandleTokens: ControlTokens {
     /// Defines the color of the mark of the `ResizingHandle`.
     open var markColor: DynamicColor {
-        // TODO: Update to aliasTokens.strokeColors[.neutral1] with Fluent 2 colors
         return DynamicColor(light: ColorValue(0x919191) /* gray400 */,
                             lightHighContrast: ColorValue(0x404040) /* gray600 */,
                             dark: ColorValue(0x6E6E6E) /* gray500 */,
