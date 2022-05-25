a08fae2d4beaaba0d702630f47a41375dff4df83
TaeVon Lewis <65370736+lewist13@users.noreply.github.com>
2022-05-24 20:43:35 -0400

    [PreviewCard] Created multiple PreviewCards with variations in height (#991)
    * Add and display PreviewCard view with rounded box
    
    * Updated PreviewCard & PreviewCardDemoController w/documentation and fixed values
    
    * Updated variable titles, properly ordered constant structs
    
    * [PreviewCard] Display PreviewCard view, add rounded box inside view (#963)
    
    * Add and display PreviewCard view with rounded box
    
    * Updated PreviewCard & PreviewCardDemoController w/documentation and fixed values
    
    * Updated variable titles, properly ordered constant structs
    
    * Updated PreviewCard to display flat & elevated card
    
    * Removed UIColor usage
    
    * Removed TODO comment
    
    * Changed PreviewCard's dark mode background color
    
    * Inserted placeholder image over PreviewCard & changed height to match 16:9 aspect ratio
    
    * Modified PreviewCard border & renamed placeholder image variable
    
    * Replaced .pdf placeholder with .png file format
    
    * Updated placeholder naming to image
    
    * Created multiple PreviewCards with variations in height
    
    * Added clarifying comment for cardHeight values
    
    * Removed unnecessary UIKit import
    
    * Added whitespace for better readability
    
    * Renamed 4:3 aspect ratio from old landscape to fullscreen
    
    * Removed comment indents

---
+++ ios/FluentUI/PreviewCard/PreviewCardTokens.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+/// Pre-defined sizes of the PreviewCard.
+@objc public enum MSFPreviewCardSize: Int, CaseIterable {
+    /// 21:9 aspect ratio
+    case ultrawideHeight
+    /// 16:9 aspect ratio
+    case landscapeHeight
+    /// 4:3 aspect ratio
+    case fullscreenHeight
+    /// 1:1 aspect ratio
+    case squareHeight
+    /// CGFloat values for the height of individual PreviewCards.
+    var cardHeight: CGFloat {
+        switch self {
+        case .ultrawideHeight:
+        return 147
+        case .landscapeHeight:
+        return 192
+        case .fullscreenHeight:
+        return 257
+        case .squareHeight:
+        return 343
+        }
+    }
+}
