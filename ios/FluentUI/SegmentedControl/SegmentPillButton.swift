cf39449caa7bdf6755906161775d8934dffcc7bf
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-05-26 14:05:50 -0700

    Move accessibility strings
    Turns out we don't need to set all these strings explicitly if we have a title.

--- ios/FluentUI/SegmentedControl/SegmentPillButton.swift
+++ ios/FluentUI/SegmentedControl/SegmentPillButton.swift
@@ -35,10 +35,10 @@ class SegmentPillButton: UIButton {
         self.contentEdgeInsets = Constants.insets
 
         let title = item.title
-        self.accessibilityLabel = title
-        self.largeContentTitle = title
         if let image = item.image {
             self.setImage(image, for: .normal)
+            self.accessibilityLabel = title
+            self.largeContentTitle = title
         } else {
             self.setTitle(title, for: .normal)
         }
