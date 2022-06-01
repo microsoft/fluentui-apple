645a98870b68e34ea072aaecb95b112efbde4ed2
James Edwards <mosuve@microsoft.com>
2022-05-31 18:57:33 -0700

    Documentation for SideTabBarTokens

--- ios/FluentUI/Tab Bar/SideTabBarTokens.swift
+++ ios/FluentUI/Tab Bar/SideTabBarTokens.swift
@@ -10,37 +10,37 @@ open class SideTabBarTokens: ControlTokens {
     /// The width of the SideTabBar
     open var sideTabBarWidth: CGFloat { 62.0 }
 
-    ///
+    /// The safe spacing for the avatar
     open var avatarViewSafeTopSpacing: CGFloat { 18.0 }
 
-    ///
+    /// The minimum spacing for the avatar
     open var avatarViewMinTopSpacing: CGFloat { 36.0 }
 
-    ///
+    /// The
     open var avatarViewTopStackViewSpacing: CGFloat { 34.0 }
 
-    ///
+    /// The safe spacing for the bottom StackView
     open var bottomStackViewSafeSpacing: CGFloat { 14.0 }
 
-    ///
+    /// The min spacing for the bottom StackView
     open var bottomStackViewMinSpacing: CGFloat { 24.0 }
 
-    ///
+    /// The spacing between the top set of TabBarItems
     open var topTabBarItemSpacing: CGFloat { 32.0 }
 
-    ///
+    /// The spacing between the bottom set of TabBarItems
     open var bottomTabBarItemSpacing: CGFloat { 24.0 }
 
-    ///
+    /// The size of the top set of TabBarItems
     open var topTabBarItemSize: CGFloat { 28.0 }
 
-    ///
+    /// The size of the bottom set of TabBarItems
     open var bottomTabBarItemSize: CGFloat { 24.0 }
 
-    ///
+    /// The padding for the badges on the top set of TabBarItems
     open var badgeTopSectionPadding: CGFloat { 2.0 }
 
-    ///
+    /// The padding for the badges on the bottom set of TabBarItems
     open var badgeBottomSectionPadding: CGFloat { 4.0 }
 
     /// Defines the background color of the  of the `TabBarItem` when selected.
