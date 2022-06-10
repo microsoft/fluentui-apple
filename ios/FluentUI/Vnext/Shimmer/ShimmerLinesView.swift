6d6352447723c9c61a953ddcb938190203e939f6
jeaniehuynh <31874971+jeaniehuynh@users.noreply.github.com>
2022-06-10 16:14:53 -0700

    tabs to spaces

--- ios/FluentUI/Vnext/Shimmer/ShimmerLinesView.swift
+++ ios/FluentUI/Vnext/Shimmer/ShimmerLinesView.swift
@@ -11,94 +11,94 @@ import UIKit
 @objc(MSFShimmerLinesView)
 open class ShimmerLinesView: ShimmerView {
 
-	/// Creates the ShimmerLinesView
-	/// - Parameters:
-	///   - lineCount: Number of lines that will shimmer in this view. Use 0 if the number of lines should fill the available space.
-	///   - firstLineFillPercent: The percent the first line (if 2+ lines) should fill the available horizontal space
-	///   - lastLineFillPercent: The percent the last line should fill the available horizontal space.
-	@objc public init(lineCount: Int,
-					  firstLineFillPercent: CGFloat,
-					  lastLineFillPercent: CGFloat) {
-		self.lineCount = lineCount
-		self.firstLineFillPercent = firstLineFillPercent
-		self.lastLineFillPercent = lastLineFillPercent
+    /// Creates the ShimmerLinesView
+    /// - Parameters:
+    ///   - lineCount: Number of lines that will shimmer in this view. Use 0 if the number of lines should fill the available space.
+    ///   - firstLineFillPercent: The percent the first line (if 2+ lines) should fill the available horizontal space
+    ///   - lastLineFillPercent: The percent the last line should fill the available horizontal space.
+    @objc public init(lineCount: Int,
+                      firstLineFillPercent: CGFloat,
+                      lastLineFillPercent: CGFloat) {
+        self.lineCount = lineCount
+        self.firstLineFillPercent = firstLineFillPercent
+        self.lastLineFillPercent = lastLineFillPercent
 
-		super.init()
-	}
+        super.init()
+    }
 
-	required public init?(coder: NSCoder) {
-		preconditionFailure("init(coder:) has not been implemented")
-	}
+    required public init?(coder: NSCoder) {
+        preconditionFailure("init(coder:) has not been implemented")
+    }
 
-	open override func layoutSubviews() {
-		super.layoutSubviews()
+    open override func layoutSubviews() {
+        super.layoutSubviews()
 
-		var currentTop: CGFloat = 0
-		for (index, linelayer) in viewCoverLayers.enumerated() {
-			let fillPercent: CGFloat = {
-				if index == 0 && viewCoverLayers.count > 2 {
-					return firstLineFillPercent
-				} else if index == viewCoverLayers.count - 1 {
-					return lastLineFillPercent
-				} else {
-					return 1
-				}
-			}()
+        var currentTop: CGFloat = 0
+        for (index, linelayer) in viewCoverLayers.enumerated() {
+            let fillPercent: CGFloat = {
+                if index == 0 && viewCoverLayers.count > 2 {
+                    return firstLineFillPercent
+                } else if index == viewCoverLayers.count - 1 {
+                    return lastLineFillPercent
+                } else {
+                    return 1
+                }
+            }()
 
-			let labelHeight = tokens.labelHeight
-			linelayer.frame = CGRect(x: 0, y: currentTop, width: fillPercent * frame.width, height: labelHeight)
+            let labelHeight = tokens.labelHeight
+            linelayer.frame = CGRect(x: 0, y: currentTop, width: fillPercent * frame.width, height: labelHeight)
 
-			currentTop += labelHeight + tokens.labelSpacing
-		}
+            currentTop += labelHeight + tokens.labelSpacing
+        }
 
-		shimmeringLayer.frame = CGRect(x: -tokens.shimmerWidth, y: 0.0, width: frame.width + 2 * tokens.shimmerWidth, height: frame.height)
-		viewCoverLayers.forEach { $0.frame = flipRectForRTL($0.frame) }
+        shimmeringLayer.frame = CGRect(x: -tokens.shimmerWidth, y: 0.0, width: frame.width + 2 * tokens.shimmerWidth, height: frame.height)
+        viewCoverLayers.forEach { $0.frame = flipRectForRTL($0.frame) }
 
-		updateShimmeringLayer()
-		updateShimmeringAnimation()
-	}
+        updateShimmeringLayer()
+        updateShimmeringAnimation()
+    }
 
-	open override func sizeThatFits(_ size: CGSize) -> CGSize {
-		let desiredLineCount = CGFloat(lineCount(for: size.height))
-		let height = desiredLineCount * tokens.labelHeight + (desiredLineCount - 1) * tokens.labelSpacing
-		return CGSize(width: size.width, height: height)
-	}
+    open override func sizeThatFits(_ size: CGSize) -> CGSize {
+        let desiredLineCount = CGFloat(lineCount(for: size.height))
+        let height = desiredLineCount * tokens.labelHeight + (desiredLineCount - 1) * tokens.labelSpacing
+        return CGSize(width: size.width, height: height)
+    }
 
-	open override var intrinsicContentSize: CGSize {
-		return CGSize(width: UIView.noIntrinsicMetric, height: sizeThatFits(CGSize(width: frame.width, height: .infinity)).height)
-	}
+    open override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: sizeThatFits(CGSize(width: frame.width, height: .infinity)).height)
+    }
 
-	override func updateViewCoverLayers() {
-		var newLineLayers = [CALayer]()
-		let desiredLineCount = lineCount(for: frame.height)
+    override func updateViewCoverLayers() {
+        var newLineLayers = [CALayer]()
+        let desiredLineCount = lineCount(for: frame.height)
 
-		for i in 0..<desiredLineCount {
-			let lineLayer = i < viewCoverLayers.count ? viewCoverLayers[i] : CALayer()
-			lineLayer.cornerRadius = tokens.labelCornerRadius >= 0 ? tokens.labelCornerRadius : tokens.cornerRadius
-			lineLayer.backgroundColor = UIColor(dynamicColor: tokens.tintColor).cgColor
+        for i in 0..<desiredLineCount {
+            let lineLayer = i < viewCoverLayers.count ? viewCoverLayers[i] : CALayer()
+            lineLayer.cornerRadius = tokens.labelCornerRadius >= 0 ? tokens.labelCornerRadius : tokens.cornerRadius
+            lineLayer.backgroundColor = UIColor(dynamicColor: tokens.tintColor).cgColor
 
-			// Add layer
-			newLineLayers.append(lineLayer)
-			layer.addSublayer(lineLayer)
-		}
+            // Add layer
+            newLineLayers.append(lineLayer)
+            layer.addSublayer(lineLayer)
+        }
 
-		Set(viewCoverLayers).subtracting(Set(newLineLayers)).forEach { $0.removeFromSuperlayer() }
+        Set(viewCoverLayers).subtracting(Set(newLineLayers)).forEach { $0.removeFromSuperlayer() }
 
-		viewCoverLayers = newLineLayers
-	}
+        viewCoverLayers = newLineLayers
+    }
 
-	@objc private func lineCount(for availableHeight: CGFloat) -> Int {
-		if lineCount == 0 {
-			let lineSpacing = tokens.labelSpacing
-			// Deduce lines count based on available height
-			return Int(floor((availableHeight + lineSpacing) / (tokens.labelHeight + lineSpacing)))
-		} else {
-			// Hardcoded lines count
-			return lineCount
-		}
-	}
+    @objc private func lineCount(for availableHeight: CGFloat) -> Int {
+        if lineCount == 0 {
+            let lineSpacing = tokens.labelSpacing
+            // Deduce lines count based on available height
+            return Int(floor((availableHeight + lineSpacing) / (tokens.labelHeight + lineSpacing)))
+        } else {
+            // Hardcoded lines count
+            return lineCount
+        }
+    }
 
-	private var lineCount: Int
-	private var firstLineFillPercent: CGFloat
-	private var lastLineFillPercent: CGFloat
+    private var lineCount: Int
+    private var firstLineFillPercent: CGFloat
+    private var lastLineFillPercent: CGFloat
 }
