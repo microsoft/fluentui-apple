70dc352badfa41387aff4fa88cc30eee02a6d0ca
James Edwards <mosuve@microsoft.com>
2022-07-27 17:06:55 -0700

    Added more robust documentation to ShadowInfo struct

--- ios/FluentUI/Core/Theme/Tokens/ShadowInfo.swift
+++ ios/FluentUI/Core/Theme/Tokens/ShadowInfo.swift
@@ -6,14 +6,28 @@
 import CoreGraphics
 
 /// Represents a two-part shadow as used by FluentUI.
-/// Blur refers to the radius of the shadow's blur.
 public struct ShadowInfo {
+    /// The color of the shador for shadow 1
     let colorOne: DynamicColor
+
+    /// The radius of the shadow for shadow 1
     let blurOne: CGFloat
+
+    /// The size of the shadow on the x-axis (also, width) for shadow 1
     let xOne: CGFloat
+
+    /// The size of the shadow on the y-axis (also, height) for shadow 1
     let yOne: CGFloat
+
+    /// The color of the shador for shadow 2
     let colorTwo: DynamicColor
+
+    /// The radius of the shadow for shadow 2
     let blurTwo: CGFloat
+
+    /// The size of the shadow on the x-axis (also, width) for shadow 2
     let xTwo: CGFloat
+
+    /// The size of the shadow on the y-axis (also, height) for shadow 2
     let yTwo: CGFloat
 }
