148cbff0643e3de082c271e40270240df336c7c7
Mike Schreiber <mischreiber@microsoft.com>
2022-07-31 19:02:40 -0700

    List: ListCell, ListHeader, PersonaView

--- ios/FluentUI/Vnext/List/ListTokens.swift
+++ ios/FluentUI/Vnext/List/ListTokens.swift
@@ -44,97 +44,195 @@ import SwiftUI
 
 // MARK: ListCell Tokens
 
-open class CellBaseTokens: ControlTokens {
-    /// Defines the size of the leading view of the Cell.
-    public internal(set) var cellLeadingViewSize: MSFListCellLeadingViewSize = .medium
+public class CellBaseTokenSet: ControlTokenSet<CellBaseTokenSet.Tokens> {
+    public enum Tokens: TokenSetKey {
+        /// Defines the foreground color of the disclosure icon of the Cell.
+        case disclosureIconForegroundColor
 
-    /// Defines the foreground color of the disclosure icon of the Cell.
-    open var disclosureIconForegroundColor: DynamicColor { aliasTokens.foregroundColors[.neutral4] }
+        /// Defines the color of the label of the Cell.
+        case labelColor
 
-    /// Defines the color of the label of the Cell.
-    open var labelColor: DynamicColor { aliasTokens.foregroundColors[.neutral1] }
+        /// Defines the color of the label when the Cell is selected.
+        case labelSelectedColor
 
-    /// Defines the color of the label when the Cell is selected.
-    open var labelSelectedColor: DynamicColor { aliasTokens.foregroundColors[.brandRest] }
+        /// Defines the color of the leading view of the Cell.
+        case leadingViewColor
 
-    /// Defines the color of the leading view of the Cell.
-    open var leadingViewColor: DynamicColor { aliasTokens.foregroundColors[.neutral1] }
+        /// Defines the color of the sublabel of the Cell.
+        case sublabelColor
 
-    /// Defines the color of the sublabel of the Cell.
-    open var sublabelColor: DynamicColor { aliasTokens.foregroundColors[.neutral3] }
+        /// Defines the color of the sublabel when the Cell is selected.
+        case sublabelSelectedColor
 
-    /// Defines the color of the sublabel when the Cell is selected.
-    open var sublabelSelectedColor: DynamicColor { aliasTokens.foregroundColors[.brandRest] }
+        /// Defines the foreground color of the trailing item of the Cell.
+        case trailingItemForegroundColor
 
-    /// Defines the foreground color of the trailing item of the Cell.
-    open var trailingItemForegroundColor: DynamicColor { aliasTokens.foregroundColors[.neutral3] }
+        /// Defines the foreground color of the trailing item when the Cell is selected.
+        case trailingItemSelectedForegroundColor
 
-    /// Defines the foreground color of the trailing item when the Cell is selected.
-    open var trailingItemSelectedForegroundColor: DynamicColor { aliasTokens.foregroundColors[.brandRest] }
+        /// Defines the color of the background when the Cell is at rest.
+        case backgroundColor
 
-    /// Defines the color of the background when the Cell is at rest.
-    open var backgroundColor: DynamicColor { aliasTokens.backgroundColors[.neutral1] }
+        /// Defines the color of the background when the Cell is highlighted.
+        case highlightedBackgroundColor
 
-    /// Defines the color of the background when the Cell is highlighted.
-    open var highlightedBackgroundColor: DynamicColor { aliasTokens.backgroundColors[.neutral5] }
+        /// Defines the height of the Cell with one line.
+        case cellHeightOneLine
 
-    /// Defines the height of the Cell with one line.
-    open var cellHeightOneLine: CGFloat { globalTokens.spacing[.xxxLarge] }
+        /// Defines the height of the Cell with two lines.
+        case cellHeightTwoLines
 
-    /// Defines the height of the Cell with two lines.
-    open var cellHeightTwoLines: CGFloat { 64 }
+        /// Defines the height of the Cell with three lines.
+        case cellHeightThreeLines
 
-    /// Defines the height of the Cell with three lines.
-    open var cellHeightThreeLines: CGFloat { globalTokens.spacing[.xxxxLarge] }
+        /// Defines the padding on the leading side of the disclosure of the Cell.
+        case disclosureInterspace
 
-    /// Defines the padding on the leading side of the disclosure of the Cell.
-    open var disclosureInterspace: CGFloat { globalTokens.spacing[.xxSmall] }
+        /// Defines the size of the disclosure of the Cell.
+        case disclosureSize
 
-    /// Defines the size of the disclosure of the Cell.
-    open var disclosureSize: CGFloat { globalTokens.iconSize[.small] }
+        /// Defines the horizontal padding around the content of the Cell.
+        case horizontalCellPadding
 
-    /// Defines the horizontal padding around the content of the Cell.
-    open var horizontalCellPadding: CGFloat { globalTokens.spacing[.small] }
+        /// Defines the padding on the leading side of the non-disclosure accessory icons
+        case iconInterspace
 
-    /// Defines the padding on the leading side of the non-disclosure accessory icons
-    open var iconInterspace: CGFloat { globalTokens.spacing[.medium] }
+        /// Defines the space between a label and its accessory view
+        case labelAccessoryInterspace
 
-    /// Defines the space between a label and its accessory view
-    open var labelAccessoryInterspace: CGFloat { globalTokens.spacing[.xSmall] }
+        /// Defines the size of the accessory view of a label
+        case labelAccessorySize
 
-    /// Defines the size of the accessory view of a label
-    open var labelAccessorySize: CGFloat { globalTokens.iconSize[.xxSmall] }
+        /// Defines the size of the leading view of the Cell.
+        case leadingViewSize
 
-    /// Defines the size of the leading view of the Cell.
-    open var leadingViewSize: CGFloat {
-        switch cellLeadingViewSize {
-        case .small:
-            return globalTokens.iconSize[.xSmall]
-        case .medium:
-            return globalTokens.iconSize[.medium]
-        case .large:
-            return globalTokens.iconSize[.xxLarge]
+        /// Defines the size of the area aroudn the leading view of the Cell.
+        case leadingViewAreaSize
+
+        /// Defines the size of the accessories for the sublabel
+        case sublabelAccessorySize
+
+        /// Defines the size of the trailing item of the Cell.
+        case trailingItemSize
+
+        /// Defines the vertical padding of the Cell.
+        case verticalCellPadding
+
+        /// Defines the font of the footnote of the Cell.
+        case footnoteFont
+
+        /// Defiens the font of the sublabel of the Cell.
+        case sublabelFont
+
+        /// Defines the font of the label of the Cell.
+        case labelFont
+    }
+
+    init(cellLeadingViewSize: @escaping () -> MSFListCellLeadingViewSize) {
+        self.cellLeadingViewSize = cellLeadingViewSize
+        super.init()
+    }
+
+    @available(*, unavailable)
+    required init() {
+        preconditionFailure("init() has not been implemented")
+    }
+
+    override func defaultValue(_ token: Tokens) -> ControlTokenValue {
+        switch token {
+
+        case .disclosureIconForegroundColor:
+            return .dynamicColor { self.aliasTokens.foregroundColors[.neutral4] }
+
+        case .labelColor:
+            return .dynamicColor { self.aliasTokens.foregroundColors[.neutral1] }
+
+        case .labelSelectedColor:
+            return .dynamicColor { self.aliasTokens.foregroundColors[.brandRest] }
+
+        case .leadingViewColor:
+            return .dynamicColor { self.aliasTokens.foregroundColors[.neutral1] }
+
+        case .sublabelColor:
+            return .dynamicColor { self.aliasTokens.foregroundColors[.neutral3] }
+
+        case .sublabelSelectedColor:
+            return .dynamicColor { self.aliasTokens.foregroundColors[.brandRest] }
+
+        case .trailingItemForegroundColor:
+            return .dynamicColor { self.aliasTokens.foregroundColors[.neutral3] }
+
+        case .trailingItemSelectedForegroundColor:
+            return .dynamicColor { self.aliasTokens.foregroundColors[.brandRest] }
+
+        case .backgroundColor:
+            return .dynamicColor { self.aliasTokens.backgroundColors[.neutral1] }
+
+        case .highlightedBackgroundColor:
+            return .dynamicColor { self.aliasTokens.backgroundColors[.neutral5] }
+
+        case .cellHeightOneLine:
+            return .float { self.globalTokens.spacing[.xxxLarge] }
+
+        case .cellHeightTwoLines:
+            return .float { 64 }
+
+        case .cellHeightThreeLines:
+            return .float { self.globalTokens.spacing[.xxxxLarge] }
+
+        case .disclosureInterspace:
+            return .float { self.globalTokens.spacing[.xxSmall] }
+
+        case .disclosureSize:
+            return .float { self.globalTokens.iconSize[.small] }
+
+        case .horizontalCellPadding:
+            return .float { self.globalTokens.spacing[.small] }
+
+        case .iconInterspace:
+            return .float { self.globalTokens.spacing[.medium] }
+
+        case .labelAccessoryInterspace:
+            return .float { self.globalTokens.spacing[.xSmall] }
+
+        case .labelAccessorySize:
+            return .float { self.globalTokens.iconSize[.xxSmall] }
+
+        case .leadingViewSize:
+            return .float {
+                switch self.cellLeadingViewSize() {
+                case .small:
+                    return self.globalTokens.iconSize[.xSmall]
+                case .medium:
+                    return self.globalTokens.iconSize[.medium]
+                case .large:
+                    return self.globalTokens.iconSize[.xxLarge]
+                }
+            }
+
+        case .leadingViewAreaSize:
+            return .float { self.globalTokens.spacing[.xxxLarge] }
+
+        case .sublabelAccessorySize:
+            return .float { self.globalTokens.iconSize[.xxSmall] }
+
+        case .trailingItemSize:
+            return .float { self.globalTokens.iconSize[.medium] }
+
+        case .verticalCellPadding:
+            return .float { self.globalTokens.spacing[.xSmall] }
+
+        case .footnoteFont:
+            return .fontInfo { self.aliasTokens.typography[.caption1] }
+
+        case .sublabelFont:
+            return .fontInfo { self.aliasTokens.typography[.body2] }
+
+        case .labelFont:
+            return .fontInfo { self.aliasTokens.typography[.body1] }
         }
     }
 
-    /// Defines the size of the area aroudn the leading view of the Cell.
-    open var leadingViewAreaSize: CGFloat { globalTokens.spacing[.xxxLarge] }
-
-    /// Defines the size of the accessories for the sublabel
-    open var sublabelAccessorySize: CGFloat { globalTokens.iconSize[.xxSmall] }
-
-    /// Defines the size of the trailing item of the Cell.
-    open var trailingItemSize: CGFloat { globalTokens.iconSize[.medium] }
-
-    /// Defines the vertical padding of the Cell.
-    open var verticalCellPadding: CGFloat { globalTokens.spacing[.xSmall] }
-
-    /// Defines the font of the footnote of the Cell.
-    open var footnoteFont: FontInfo { aliasTokens.typography[.caption1] }
-
-    /// Defiens the font of the sublabel of the Cell.
-    open var sublabelFont: FontInfo { aliasTokens.typography[.body2] }
-
-    /// Defines the font of the label of the Cell.
-    open var labelFont: FontInfo { aliasTokens.typography[.body1] }
+    /// Defines the size of the leading view of the Cell.
+    var cellLeadingViewSize: () -> MSFListCellLeadingViewSize
 }
