84ae601a24f170d3cdf355b473fe42a03748ad13
Mike Schreiber <mischreiber@microsoft.com>
2022-07-31 17:58:13 -0700

    HUD

--- ios/FluentUI/Vnext/HUD/HUDTokens.swift
+++ ios/FluentUI/Vnext/HUD/HUDTokens.swift
@@ -6,40 +6,70 @@
 import SwiftUI
 import UIKit
 
-/// Design token set for the `ActivityIndicator` control.
-open class HeadsUpDisplayTokens: ControlTokens {
+/// Design token set for the `HeadsUpDisplay` control.
+public class HeadsUpDisplayTokenSet: ControlTokenSet<HeadsUpDisplayTokenSet.Tokens> {
+    public enum Tokens: TokenSetKey {
+        /// The color of the squared background of the Heads-up display.
+        case backgroundColor
 
-    /// The color of the squared background of the Heads-up display.
-    open var backgroundColor: DynamicColor { DynamicColor(light: ColorValue(r: 0.129, g: 0.129, b: 0.129, a: 0.9),
-                                                          dark: ColorValue(r: 0.188, g: 0.188, b: 0.188, a: 1)) }
+        /// The color of the contents presented by the Heads-up display.
+        case foregroundColor
 
-    /// The color of the contents presented by the Heads-up display.
-    open var foregroundColor: DynamicColor { DynamicColor(light: globalTokens.neutralColors[.white],
-                                                          dark: ColorValue(r: 0.882, g: 0.882, b: 0.882, a: 1),
-                                                          darkHighContrast: globalTokens.neutralColors[.white]) }
+        /// The corner radius of the squared background of the Heads-up display.
+        case cornerRadius
 
-    /// The corner radius of the squared background of the Heads-up display.
-    open var cornerRadius: CGFloat {
-        return globalTokens.borderRadius[.medium]
+        /// The distance between the label and image contents from the left and right edges of the squared background of the Heads-up display.
+        case horizontalPadding
+
+        /// The distance between the label and image contents from the top and bottom edges of the squared background of the Heads-up display.
+        case verticalPadding
+
+        /// The minimum value for the side of the squared background of the Heads-up display.
+        case minSize
+
+        /// The maximum value for the side of the squared background of the Heads-up display.
+        case maxSize
     }
 
-    /// The distance between the label and image contents from the left and right edges of the squared background of the Heads-up display.
-    open var horizontalPadding: CGFloat {
-        return globalTokens.spacing[.small]
-    }
+    override func defaultValue(_ token: Tokens) -> ControlTokenValue {
+        switch token {
+        case .backgroundColor:
+            return .dynamicColor {
+                DynamicColor(light: ColorValue(r: 0.129, g: 0.129, b: 0.129, a: 0.9),
+                             dark: ColorValue(r: 0.188, g: 0.188, b: 0.188, a: 1))
+            }
 
-    /// The distance between the label and image contents from the top and bottom edges of the squared background of the Heads-up display.
-    open var verticalPadding: CGFloat {
-        return globalTokens.spacing[.large]
-    }
+        case .foregroundColor:
+            return .dynamicColor {
+                DynamicColor(light: self.globalTokens.neutralColors[.white],
+                             dark: ColorValue(r: 0.882, g: 0.882, b: 0.882, a: 1),
+                             darkHighContrast: self.globalTokens.neutralColors[.white])
+            }
 
-    /// The minimum value for the side of the squared background of the Heads-up display.
-    open var minSize: CGFloat {
-        return 100
-    }
+        case .cornerRadius:
+            return .float {
+                return self.globalTokens.borderRadius[.medium]
+            }
 
-    /// The maximum value for the side of the squared background of the Heads-up display.
-    open var maxSize: CGFloat {
-        return 192
+        case .horizontalPadding:
+            return .float {
+                return self.globalTokens.spacing[.small]
+            }
+
+        case .verticalPadding:
+            return .float {
+                return self.globalTokens.spacing[.large]
+            }
+
+        case .minSize:
+            return .float {
+                return 100
+            }
+
+        case .maxSize:
+            return .float {
+                return 192
+            }
+        }
     }
 }
