fd8cd3970af17568bd8e92f55371fcfd04511b57
Mike Schreiber <mischreiber@microsoft.com>
2022-08-01 16:09:12 -0700

    [Fluent2 TokenSet] PillButton[Bar] (#1117)
    * PillButton[Bar] part 1
    
    * Fix UIKit theme updating

---
+++ ios/FluentUI/Pill Button Bar/PillButtonBarTokenSet.swift
@@ -0,0 +1,63 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import UIKit
+
+/// Design token set for the `PillButtonBar` control
+public class PillButtonBarTokenSet: ControlTokenSet<PillButtonBarTokenSet.Tokens> {
+    public enum Tokens: TokenSetKey {
+        /// Maximum spacing between `PillButton` controls
+        case maxButtonsSpacing
+
+        /// Minimum spacing between `PillButton` controls
+        case  minButtonsSpacing
+
+        /// Minimum width of the last button that must be showing on screen when the `PillButtonBar` loads or redraws
+        case minButtonVisibleWidth
+
+        /// Minimum width of a `PillButton`
+        case minButtonWidth
+
+        /// Minimum height of the `PillButtonBar`
+        case minHeight
+
+        /// Offset from the leading edge when the `PillButtonBar` loads or redraws
+        case sideInset
+    }
+
+    override func defaultValue(_ token: Tokens) -> ControlTokenValue {
+        switch token {
+        case .maxButtonsSpacing:
+            return .float {
+                10
+            }
+
+        case .minButtonsSpacing:
+            return .float {
+                self.globalTokens.spacing[.xSmall]
+            }
+
+        case .minButtonVisibleWidth:
+            return .float {
+                self.globalTokens.spacing[.large]
+            }
+
+        case .minButtonWidth:
+            return .float {
+                56
+            }
+
+        case .minHeight:
+            return .float {
+                28
+            }
+
+        case .sideInset:
+            return .float {
+                self.globalTokens.spacing[.medium]
+            }
+        }
+    }
+}
