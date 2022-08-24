microsoft/fluentui-apple

commit 6b205e9b386a476af4f2699c18d9d195d5132b07

Integrating Fluent2 HUD into main. (#1034)
* Integrating Fluent2 HUD into main.

* Build break fix: Providing defaults for state variables.

ios/FluentUI.Demo/FluentUI.Demo/Demos/HUDDemoController.swift
@@ -6,55 +6,127 @@
 import FluentUI
 import UIKit
 
-class HUDDemoController: DemoController {
+class HUDDemoController: DemoTableViewController {
+    override init(nibName nibNameOrNil: String?,
+                  bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder: NSCoder) {
+        preconditionFailure("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        container.addArrangedSubview(createButton(title: "Show activity HUD", action: #selector(showActivityHUD)))
-        container.addArrangedSubview(createButton(title: "Show success HUD", action: #selector(showSuccessHUD)))
-        container.addArrangedSubview(createButton(title: "Show failure HUD", action: #selector(showFailureHUD)))
-        container.addArrangedSubview(createButton(title: "Show custom HUD", action: #selector(showCustomHUD)))
-        container.addArrangedSubview(createButton(title: "Show custom non-blocking HUD", action: #selector(showCustomNonBlockingHUD)))
-        container.addArrangedSubview(createButton(title: "Show HUD with no label", action: #selector(showNoLabelHUD)))
-        container.addArrangedSubview(createButton(title: "Show HUD with tap gesture callback", action: #selector(showGestureHUD)))
-        container.addArrangedSubview(createButton(title: "Show HUD with updating caption", action: #selector(showUpdateHUD)))
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.identifier)
+        tableView.register(ActionsCell.self, forCellReuseIdentifier: ActionsCell.identifier)
     }
 
-    @objc private func showActivityHUD() {
-        HUD.shared.show(in: view, with: HUDParams(caption: "Loading for 3 seconds"))
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return HUDDemoSection.allCases.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return HUDDemoSection.allCases[section].rows.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = HUDDemoSection.allCases[indexPath.section]
+        let row = section.rows[indexPath.row]
+
+        switch row {
+        case .swiftUIDemo:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier) as? TableViewCell else {
+                return UITableViewCell()
+            }
+            cell.setup(title: row.title)
+            cell.accessoryType = .disclosureIndicator
+
+            return cell
+
+        case .showHUD:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ActionsCell.identifier) as? ActionsCell else {
+                return UITableViewCell()
+            }
+
+            cell.setup(action1Title: row.title, action1Type: .regular)
+            cell.action1Button.addTarget(self,
+                                         action: section.action,
+                                         for: .touchUpInside)
+            cell.bottomSeparatorType = .full
+
+            return cell
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return HUDDemoSection.allCases[section].title
+    }
+
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return HUDDemoSection.allCases[indexPath.section].rows[indexPath.row] == .swiftUIDemo
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else {
+            return
+        }
+
+        cell.setSelected(false, animated: true)
+
+        switch HUDDemoSection.allCases[indexPath.section].rows[indexPath.row] {
+        case .swiftUIDemo:
+            navigationController?.pushViewController(HUDDemoControllerSwiftUI(),
+                                                     animated: true)
+        default:
+            break
+        }
+    }
+
+    @objc private func showActivityHUD(sender: UIButton) {
+        HUD.shared.show(from: self,
+                        with: HUDParams(caption: "Loading for 3 seconds"))
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             HUD.shared.hide()
         }
     }
 
     @objc private func showSuccessHUD(sender: UIButton) {
-        HUD.shared.showSuccess(from: self, with: "Success")
+        HUD.shared.showSuccess(from: self,
+                               with: "Success")
     }
 
     @objc private func showFailureHUD(sender: UIButton) {
-        HUD.shared.showFailure(from: self, with: "Failure")
+        HUD.shared.showFailure(from: self,
+                               with: "Failure")
     }
 
     @objc private func showCustomHUD(sender: UIButton) {
-        HUD.shared.show(in: self.view, with: HUDParams(caption: "Custom", image: UIImage(named: "flag-48x48"), isPersistent: false))
+        HUD.shared.show(from: self, with: HUDParams(caption: "Custom",
+                                                    image: UIImage(named: "flag-40x40"),
+                                                    isPersistent: false))
     }
 
     @objc private func showCustomNonBlockingHUD(sender: UIButton) {
-        HUD.shared.show(in: view, with: HUDParams(caption: "Custom image non-blocking", image: UIImage(named: "flag-48x48"), isBlocking: false))
+        HUD.shared.show(from: self,
+                        with: HUDParams(caption: "Custom image non-blocking",
+                                        image: UIImage(named: "flag-40x40"),
+                                        isBlocking: false))
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             HUD.shared.hide()
         }
     }
 
     @objc private func showNoLabelHUD(sender: UIButton) {
-        HUD.shared.show(in: view)
+        HUD.shared.show(from: self)
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             HUD.shared.hide()
         }
     }
 
     @objc private func showGestureHUD(sender: UIButton) {
-        HUD.shared.show(in: view, with: HUDParams(caption: "Downloading..."), onTap: {
+        HUD.shared.show(from: self, with: HUDParams(caption: "Downloading..."), onTap: {
             self.showMessage("Stop Download?", autoDismiss: false) {
                 HUD.shared.hide()
             }
@@ -62,7 +134,8 @@ class HUDDemoController: DemoController {
     }
 
     @objc private func showUpdateHUD(sender: UIButton) {
-        HUD.shared.show(in: view, with: HUDParams(caption: "Downloading..."))
+        HUD.shared.show(from: self,
+                        with: HUDParams(caption: "Downloading..."))
         var time: TimeInterval = 0
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
             time += timer.timeInterval
@@ -75,6 +148,161 @@ class HUDDemoController: DemoController {
                     HUD.shared.hide()
                 }
             }
+        }
+    }
+
+    override func showMessage(_ message: String, autoDismiss: Bool = true, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        present(alert, animated: true)
+
+        if autoDismiss {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.dismiss(animated: true)
+            }
+        } else {
+            let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+                self.dismiss(animated: true, completion: completion)
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            alert.addAction(okAction)
+            alert.addAction(cancelAction)
+        }
+
+    }
+
+    private enum HUDDemoSection: CaseIterable {
+        case swiftUI
+        case activity
+        case success
+        case failure
+        case custom
+        case nonblocking
+        case nolabel
+        case tapgesture
+        case updatingcaption
+
+        var isDemoSection: Bool {
+            return self != .swiftUI
+        }
+
+        var rows: [HUDDemoRow] {
+            switch self {
+            case .swiftUI:
+                return [.swiftUIDemo]
+            case .activity,
+                 .success,
+                 .failure,
+                 .custom,
+                 .nonblocking,
+                 .nolabel,
+                 .tapgesture,
+                 .updatingcaption:
+                return [.showHUD]
+            }
+        }
+
+        var action: Selector {
+            switch self {
+            case .swiftUI:
+                preconditionFailure("This section should not need a selector.")
+            case .activity:
+                return #selector(showActivityHUD)
+            case .success:
+                return #selector(showSuccessHUD)
+            case .failure:
+                return #selector(showFailureHUD)
+            case .custom:
+                return #selector(showCustomHUD)
+            case .nonblocking:
+                return #selector(showCustomNonBlockingHUD)
+            case .nolabel:
+                return #selector(showNoLabelHUD)
+            case .tapgesture:
+                return #selector(showGestureHUD)
+            case .updatingcaption:
+                return #selector(showUpdateHUD)
+            }
+        }
+
+        var title: String {
+            switch self {
+            case .swiftUI:
+                return "SwiftUI"
+            case .activity:
+                return "Activity Type"
+            case .success:
+                return "Success Type"
+            case .failure:
+                return "Failure Type"
+            case .custom:
+                return "Custom Type"
+            case .nonblocking:
+                return "Non-blocking"
+            case .nolabel:
+                return "No label"
+            case .tapgesture:
+                return "Tap gesture callback"
+            case .updatingcaption:
+                return "Updating caption"
+            }
+        }
+    }
+
+    private enum HUDDemoRow: CaseIterable {
+        case swiftUIDemo
+        case showHUD
+
+        var isDemoRow: Bool { return self != .swiftUIDemo }
+
+        var title: String {
+            switch self {
+            case .swiftUIDemo:
+                return "SwiftUI Demo"
+            case .showHUD:
+                return "Show"
+            }
+        }
+    }
+}
+
+extension HUDDemoController: DemoAppearanceDelegate {
+    func themeWideOverrideDidChange(isOverrideEnabled: Bool) {
+        guard let fluentTheme = self.view.window?.fluentTheme else {
+            return
+        }
+        if isOverrideEnabled {
+            fluentTheme.register(controlType: HeadsUpDisplay.self, tokens: {
+                ThemeWideOverrideActivityHeadsUpDisplayTokens()
+            })
+        } else {
+            fluentTheme.register(controlType: HeadsUpDisplay.self, tokens: nil)
+        }
+
+    }
+
+    func perControlOverrideDidChange(isOverrideEnabled: Bool) {
+        HUD.shared.overrideTokens = isOverrideEnabled ? PerControlOverrideHeadsUpDisplayTokens() : nil
+    }
+
+    func isThemeWideOverrideApplied() -> Bool {
+        return view.window?.fluentTheme.tokenOverride(for: HeadsUpDisplay.self) != nil
+    }
+
+    // MARK: - Custom tokens
+
+    private class ThemeWideOverrideActivityHeadsUpDisplayTokens: HeadsUpDisplayTokens {
+        override var backgroundColor: DynamicColor {
+            return aliasTokens.backgroundColors[.brandHover]
+        }
+    }
+
+    private class PerControlOverrideHeadsUpDisplayTokens: HeadsUpDisplayTokens {
+        override var cornerRadius: CGFloat {
+            return globalTokens.borderRadius[.xLarge]
+        }
+
+        override var foregroundColor: DynamicColor {
+            return globalTokens.brandColors[.primary]
         }
     }
 }

ios/FluentUI.Demo/FluentUI.Demo/Demos/HUDDemoController_SwiftUI.swift
@@ -1,0 +1,100 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import FluentUI
+import UIKit
+import SwiftUI
+
+class HUDDemoControllerSwiftUI: UIHostingController<HUDDemoView> {
+    override init?(coder aDecoder: NSCoder, rootView: HUDDemoView) {
+        preconditionFailure("init(coder:) has not been implemented")
+    }
+
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        preconditionFailure("init(coder:) has not been implemented")
+    }
+
+    init() {
+        super.init(rootView: HUDDemoView())
+        self.title = "HUD (SwiftUI)"
+    }
+}
+
+struct HUDDemoView: View {
+
+    public var body: some View {
+        VStack {
+            HeadsUpDisplay(type: type,
+                           label: label)
+
+            ScrollView {
+                Group {
+                    Group {
+                        VStack(spacing: 0) {
+                            Text("Presentation")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .font(.title)
+                            Divider()
+                        }
+
+                        FluentUIDemoToggle(titleKey: "Blocks interaction",
+                                           isOn: $isBlocking)
+
+                        Button(action: {
+                            isPresented = true
+
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                isPresented = false
+                            }
+                        },
+                               label: {
+                            Text("Present HUD for 3 seconds")
+                        })
+                    }
+
+                    Group {
+                        VStack(spacing: 0) {
+                            Text("Label")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .font(.title)
+                            Divider()
+                        }
+
+                        TextField("Label", text: $label)
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                        VStack(spacing: 0) {
+                            Text("Type")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .font(.title)
+                            Divider()
+                        }
+
+                        Picker(selection: $type, label: EmptyView()) {
+                            Text(".activity").tag(HUDType.activity)
+                            Text(".success").tag(HUDType.success)
+                            Text(".failure").tag(HUDType.failure)
+                            Text(".custom").tag(HUDType.custom(image: UIImage(named: "flag-40x40")!))
+                        }
+                        .labelsHidden()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+                .padding()
+            }
+        }
+        .presentHUD(type: type,
+                    isBlocking: isBlocking,
+                    isPresented: $isPresented,
+                    label: label)
+    }
+
+    @State var isBlocking: Bool = true
+    @State var isPresented: Bool = false
+    @State var label: String = ""
+    @State var type: HUDType = .activity
+}

ios/FluentUI.Demo/FluentUI.Demo.xcodeproj/project.pbxproj
@@ -28,6 +28,7 @@
 		5328D97B26FBA3EA00F3723B /* IndeterminateProgressBarDemoController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5328D97926FBA3E900F3723B /* IndeterminateProgressBarDemoController.swift */; };
 		532FE3DB26EA6D8D007539C0 /* ActivityIndicatorDemoController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 532FE3D926EA6D8D007539C0 /* ActivityIndicatorDemoController.swift */; };
 		532FE3DC26EA6D8D007539C0 /* ActivityIndicatorDemoController_SwiftUI.swift in Sources */ = {isa = PBXBuildFile; fileRef = 532FE3DA26EA6D8D007539C0 /* ActivityIndicatorDemoController_SwiftUI.swift */; };
+		5336B17D27F7817300B01E0D /* HUDDemoController_SwiftUI.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5336B17B27F7817300B01E0D /* HUDDemoController_SwiftUI.swift */; };
 		5373D55F2694C3070032A3B4 /* AvatarDemoController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 5373D55E2694C3070032A3B4 /* AvatarDemoController.swift */; };
 		7D0931C124AAA3D30072458A /* SideTabBarDemoController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 7D0931C024AAA3D30072458A /* SideTabBarDemoController.swift */; };
 		80AECC0C2630F1BB005AF2F3 /* BottomCommandingDemoController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 80AECC0B2630F1BB005AF2F3 /* BottomCommandingDemoController.swift */; };
@@ -86,6 +87,7 @@
 		5328D97926FBA3E900F3723B /* IndeterminateProgressBarDemoController.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = IndeterminateProgressBarDemoController.swift; sourceTree = "<group>"; };
 		532FE3D926EA6D8D007539C0 /* ActivityIndicatorDemoController.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = ActivityIndicatorDemoController.swift; sourceTree = "<group>"; };
 		532FE3DA26EA6D8D007539C0 /* ActivityIndicatorDemoController_SwiftUI.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = ActivityIndicatorDemoController_SwiftUI.swift; sourceTree = "<group>"; };
+		5336B17B27F7817300B01E0D /* HUDDemoController_SwiftUI.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = HUDDemoController_SwiftUI.swift; sourceTree = "<group>"; };
 		5373D55E2694C3070032A3B4 /* AvatarDemoController.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = AvatarDemoController.swift; sourceTree = "<group>"; };
 		5373F95426F28D3A007F1410 /* IndeterminateProgressBarDemoController_SwiftUI.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = IndeterminateProgressBarDemoController_SwiftUI.swift; sourceTree = "<group>"; };
 		5373F95626F28D9B007F1410 /* IndeterminateProgressBarDemoController.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = IndeterminateProgressBarDemoController.swift; sourceTree = "<group>"; };
@@ -316,6 +318,7 @@
 				FD7254EC21471A3F002F4069 /* DateTimePickerDemoController.swift */,
 				A5DCA75D211E3A92005F4CB7 /* DrawerDemoController.swift */,
 				B4E8F65121CD9579008A1598 /* HUDDemoController.swift */,
+				5336B17B27F7817300B01E0D /* HUDDemoController_SwiftUI.swift */,
 				5328D97926FBA3E900F3723B /* IndeterminateProgressBarDemoController.swift */,
 				5328D97826FBA3E900F3723B /* IndeterminateProgressBarDemoController_SwiftUI.swift */,
 				A589F855211BA71000471C23 /* LabelDemoController.swift */,
@@ -494,6 +497,7 @@
 				B4D852DB225C010A004B1B29 /* ButtonDemoController.swift in Sources */,
 				53097D4127028AEA00A6E4DC /* PopupMenuDemoController.swift in Sources */,
 				5306075926A1E73F002D49CF /* AvatarGroupDemoController.swift in Sources */,
+				5336B17D27F7817300B01E0D /* HUDDemoController_SwiftUI.swift in Sources */,
 				92561E732718AD090072ED00 /* DemoTableViewController.swift in Sources */,
 				43488C4A270FB7E800124C71 /* NotificationViewDemoController_SwiftUI.swift in Sources */,
 				497DC2DE24185896008D86F8 /* PillButtonBarDemoController.swift in Sources */,

ios/FluentUI.Demo/FluentUI.Demo/Demos.swift
@@ -24,6 +24,7 @@ struct Demos {
         DemoDescriptor("AvatarGroup", AvatarGroupDemoController.self),
         DemoDescriptor("CardNudge", CardNudgeDemoController.self),
         DemoDescriptor("ColorTokens", ColorTokensDemoController.self),
+        DemoDescriptor("HUD", HUDDemoController.self),
         DemoDescriptor("IndeterminateProgressBar", IndeterminateProgressBarDemoController.self),
         DemoDescriptor("NotificationView", NotificationViewDemoController.self),
         DemoDescriptor("Other cells", OtherCellsDemoController.self),
@@ -42,7 +43,6 @@ struct Demos {
         DemoDescriptor("CommandBar", CommandBarDemoController.self),
         DemoDescriptor("DateTimePicker", DateTimePickerDemoController.self),
         DemoDescriptor("DrawerController", DrawerDemoController.self),
-        DemoDescriptor("HUD", HUDDemoController.self),
         DemoDescriptor("Label", LabelDemoController.self),
         DemoDescriptor("NavigationController", NavigationControllerDemoController.self),
         DemoDescriptor("PeoplePicker", PeoplePickerDemoController.self),

commit a792f21856a0dd715e135edd7b6bf64cb090b509

fix crash in HUD Demo app
Regressed by commit 6b205e9 when updating HUD to swiftui control it wasn't referring the the correct fluent demo app asset icon.

ios/FluentUI.Demo/FluentUI.Demo/Demos/HUDDemoController.swift
@@ -104,14 +104,14 @@ class HUDDemoController: DemoTableViewController {
 
     @objc private func showCustomHUD(sender: UIButton) {
         HUD.shared.show(from: self, with: HUDParams(caption: "Custom",
-                                                    image: UIImage(named: "flag-40x40"),
+                                                    image: UIImage(named: "flag-48x48"),
                                                     isPersistent: false))
     }
 
     @objc private func showCustomNonBlockingHUD(sender: UIButton) {
         HUD.shared.show(from: self,
                         with: HUDParams(caption: "Custom image non-blocking",
-                                        image: UIImage(named: "flag-40x40"),
+                                        image: UIImage(named: "flag-48x48"),
                                         isBlocking: false))
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             HUD.shared.hide()

ios/FluentUI.Demo/FluentUI.Demo/Demos/HUDDemoController_SwiftUI.swift
@@ -78,7 +78,7 @@ struct HUDDemoView: View {
                             Text(".activity").tag(HUDType.activity)
                             Text(".success").tag(HUDType.success)
                             Text(".failure").tag(HUDType.failure)
-                            Text(".custom").tag(HUDType.custom(image: UIImage(named: "flag-40x40")!))
+                            Text(".custom").tag(HUDType.custom(image: UIImage(named: "flag-48x48")!))
                         }
                         .labelsHidden()
                         .frame(maxWidth: .infinity, alignment: .leading)