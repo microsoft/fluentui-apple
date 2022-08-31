dfe873792f29dc352b70238e9c55ae5abaa63ffc
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-08-30 17:38:52 -0700

    Fix lint errors

--- ios/FluentUI/Popup Menu/PopupMenuItemTokenSet.swift
+++ ios/FluentUI/Popup Menu/PopupMenuItemTokenSet.swift
@@ -12,18 +12,21 @@ class PopupMenuItemTokenSet: TableViewCellTokenSet {
                 DynamicColor(light: ColorValue(0x212121 /* gray900 */),
                              lightHighContrast: GlobalTokens.neutralColors(.black),
                              dark: ColorValue(0xE1E1E1 /* gray100 */),
-                             darkHighContrast: GlobalTokens.neutralColors(.white)) },
+                             darkHighContrast: GlobalTokens.neutralColors(.white))
+            },
             .subtitleColor: .dynamicColor { /* textSecondary */
                 DynamicColor(light: ColorValue(0x6E6E6E /* gray500 */),
                              lightHighContrast: ColorValue(0x303030 /* gray700 */),
                              dark: ColorValue(0x919191 /* gray400 */),
-                             darkHighContrast: ColorValue(0xC8C8C8 /* gray200 */)) },
+                             darkHighContrast: ColorValue(0xC8C8C8 /* gray200 */))
+            },
             .imageColor: .dynamicColor { /* iconSecondary */
                 DynamicColor(light: ColorValue(0x919191 /* gray400 */),
                              lightHighContrast: ColorValue(0x404040 /* gray600 */),
                              dark: ColorValue(0x6E6E6E /* gray500 */),
                              darkHighContrast: ColorValue(0xACACAC /* gray300 */),
-                             darkElevated: ColorValue(0x919191 /* gray400 */)) },
+                             darkElevated: ColorValue(0x919191 /* gray400 */))
+            },
             .cellBackgroundColor: .dynamicColor { DynamicColor(light: .clear) }
         ])
     }
