microsoft/fluentui-apple

commit 6b205e9b386a476af4f2699c18d9d195d5132b07

Integrating Fluent2 HUD into main. (#1034)
* Integrating Fluent2 HUD into main.

* Build break fix: Providing defaults for state variables.

ios/FluentUI.Demo/FluentUI.Demo/Demos/HUDDemoController.swift
@@ -6,55 +6,127 @@
 import FluentUI
 import UIKit
 
-class HUDDemoController: DemoController {
+class HUDDemoController: DemoTableViewController {
+    override init(nibName nibNameOrNil: String?,
+                  bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder: NSCoder) {
+        preconditionFailure("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        container.addArrangedSubview(createButton(title: "Show activity HUD", action: #selector(showActivityHUD)))
-        container.addArrangedSubview(createButton(title: "Show success HUD", action: #selector(showSuccessHUD)))
-        container.addArrangedSubview(createButton(title: "Show failure HUD", action: #selector(showFailureHUD)))
-        container.addArrangedSubview(createButton(title: "Show custom HUD", action: #selector(showCustomHUD)))
-        container.addArrangedSubview(createButton(title: "Show custom non-blocking HUD", action: #selector(showCustomNonBlockingHUD)))
-        container.addArrangedSubview(createButton(title: "Show HUD with no label", action: #selector(showNoLabelHUD)))
-        container.addArrangedSubview(createButton(title: "Show HUD with tap gesture callback", action: #selector(showGestureHUD)))
-        container.addArrangedSubview(createButton(title: "Show HUD with updating caption", action: #selector(showUpdateHUD)))
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.identifier)
+        tableView.register(ActionsCell.self, forCellReuseIdentifier: ActionsCell.identifier)
     }
 
-    @objc private func showActivityHUD() {
-        HUD.shared.show(in: view, with: HUDParams(caption: "Loading for 3 seconds"))
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return HUDDemoSection.allCases.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return HUDDemoSection.allCases[section].rows.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = HUDDemoSection.allCases[indexPath.section]
+        let row = section.rows[indexPath.row]
+
+        switch row {
+        case .swiftUIDemo:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier) as? TableViewCell else {
+                return UITableViewCell()
+            }
+            cell.setup(title: row.title)
+            cell.accessoryType = .disclosureIndicator
+
+            return cell
+
+        case .showHUD:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: ActionsCell.identifier) as? ActionsCell else {
+                return UITableViewCell()
+            }
+
+            cell.setup(action1Title: row.title, action1Type: .regular)
+            cell.action1Button.addTarget(self,
+                                         action: section.action,
+                                         for: .touchUpInside)
+            cell.bottomSeparatorType = .full
+
+            return cell
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return HUDDemoSection.allCases[section].title
+    }
+
+    override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return HUDDemoSection.allCases[indexPath.section].rows[indexPath.row] == .swiftUIDemo
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else {
+            return
+        }
+
+        cell.setSelected(false, animated: true)
+
+        switch HUDDemoSection.allCases[indexPath.section].rows[indexPath.row] {
+        case .swiftUIDemo:
+            navigationController?.pushViewController(HUDDemoControllerSwiftUI(),
+                                                     animated: true)
+        default:
+            break
+        }
+    }
+
+    @objc private func showActivityHUD(sender: UIButton) {
+        HUD.shared.show(from: self,
+                        with: HUDParams(caption: "Loading for 3 seconds"))
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             HUD.shared.hide()
         }
     }
 
     @objc private func showSuccessHUD(sender: UIButton) {
-        HUD.shared.showSuccess(from: self, with: "Success")
+        HUD.shared.showSuccess(from: self,
+                               with: "Success")
     }
 
     @objc private func showFailureHUD(sender: UIButton) {
-        HUD.shared.showFailure(from: self, with: "Failure")
+        HUD.shared.showFailure(from: self,
+                               with: "Failure")
     }
 
     @objc private func showCustomHUD(sender: UIButton) {
-        HUD.shared.show(in: self.view, with: HUDParams(caption: "Custom", image: UIImage(named: "flag-48x48"), isPersistent: false))
+        HUD.shared.show(from: self, with: HUDParams(caption: "Custom",
+                                                    image: UIImage(named: "flag-40x40"),
+                                                    isPersistent: false))
     }
 
     @objc private func showCustomNonBlockingHUD(sender: UIButton) {
-        HUD.shared.show(in: view, with: HUDParams(caption: "Custom image non-blocking", image: UIImage(named: "flag-48x48"), isBlocking: false))
+        HUD.shared.show(from: self,
+                        with: HUDParams(caption: "Custom image non-blocking",
+                                        image: UIImage(named: "flag-40x40"),
+                                        isBlocking: false))
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             HUD.shared.hide()
         }
     }
 
     @objc private func showNoLabelHUD(sender: UIButton) {
-        HUD.shared.show(in: view)
+        HUD.shared.show(from: self)
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             HUD.shared.hide()
         }
     }
 
     @objc private func showGestureHUD(sender: UIButton) {
-        HUD.shared.show(in: view, with: HUDParams(caption: "Downloading..."), onTap: {
+        HUD.shared.show(from: self, with: HUDParams(caption: "Downloading..."), onTap: {
             self.showMessage("Stop Download?", autoDismiss: false) {
                 HUD.shared.hide()
             }
@@ -62,7 +134,8 @@ class HUDDemoController: DemoController {
     }
 
     @objc private func showUpdateHUD(sender: UIButton) {
-        HUD.shared.show(in: view, with: HUDParams(caption: "Downloading..."))
+        HUD.shared.show(from: self,
+                        with: HUDParams(caption: "Downloading..."))
         var time: TimeInterval = 0
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
             time += timer.timeInterval
@@ -75,6 +148,161 @@ class HUDDemoController: DemoController {
                     HUD.shared.hide()
                 }
             }
+        }
+    }
+
+    override func showMessage(_ message: String, autoDismiss: Bool = true, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        present(alert, animated: true)
+
+        if autoDismiss {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.dismiss(animated: true)
+            }
+        } else {
+            let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+                self.dismiss(animated: true, completion: completion)
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            alert.addAction(okAction)
+            alert.addAction(cancelAction)
+        }
+
+    }
+
+    private enum HUDDemoSection: CaseIterable {
+        case swiftUI
+        case activity
+        case success
+        case failure
+        case custom
+        case nonblocking
+        case nolabel
+        case tapgesture
+        case updatingcaption
+
+        var isDemoSection: Bool {
+            return self != .swiftUI
+        }
+
+        var rows: [HUDDemoRow] {
+            switch self {
+            case .swiftUI:
+                return [.swiftUIDemo]
+            case .activity,
+                 .success,
+                 .failure,
+                 .custom,
+                 .nonblocking,
+                 .nolabel,
+                 .tapgesture,
+                 .updatingcaption:
+                return [.showHUD]
+            }
+        }
+
+        var action: Selector {
+            switch self {
+            case .swiftUI:
+                preconditionFailure("This section should not need a selector.")
+            case .activity:
+                return #selector(showActivityHUD)
+            case .success:
+                return #selector(showSuccessHUD)
+            case .failure:
+                return #selector(showFailureHUD)
+            case .custom:
+                return #selector(showCustomHUD)
+            case .nonblocking:
+                return #selector(showCustomNonBlockingHUD)
+            case .nolabel:
+                return #selector(showNoLabelHUD)
+            case .tapgesture:
+                return #selector(showGestureHUD)
+            case .updatingcaption:
+                return #selector(showUpdateHUD)
+            }
+        }
+
+        var title: String {
+            switch self {
+            case .swiftUI:
+                return "SwiftUI"
+            case .activity:
+                return "Activity Type"
+            case .success:
+                return "Success Type"
+            case .failure:
+                return "Failure Type"
+            case .custom:
+                return "Custom Type"
+            case .nonblocking:
+                return "Non-blocking"
+            case .nolabel:
+                return "No label"
+            case .tapgesture:
+                return "Tap gesture callback"
+            case .updatingcaption:
+                return "Updating caption"
+            }
+        }
+    }
+
+    private enum HUDDemoRow: CaseIterable {
+        case swiftUIDemo
+        case showHUD
+
+        var isDemoRow: Bool { return self != .swiftUIDemo }
+
+        var title: String {
+            switch self {
+            case .swiftUIDemo:
+                return "SwiftUI Demo"
+            case .showHUD:
+                return "Show"
+            }
+        }
+    }
+}
+
+extension HUDDemoController: DemoAppearanceDelegate {
+    func themeWideOverrideDidChange(isOverrideEnabled: Bool) {
+        guard let fluentTheme = self.view.window?.fluentTheme else {
+            return
+        }
+        if isOverrideEnabled {
+            fluentTheme.register(controlType: HeadsUpDisplay.self, tokens: {
+                ThemeWideOverrideActivityHeadsUpDisplayTokens()
+            })
+        } else {
+            fluentTheme.register(controlType: HeadsUpDisplay.self, tokens: nil)
+        }
+
+    }
+
+    func perControlOverrideDidChange(isOverrideEnabled: Bool) {
+        HUD.shared.overrideTokens = isOverrideEnabled ? PerControlOverrideHeadsUpDisplayTokens() : nil
+    }
+
+    func isThemeWideOverrideApplied() -> Bool {
+        return view.window?.fluentTheme.tokenOverride(for: HeadsUpDisplay.self) != nil
+    }
+
+    // MARK: - Custom tokens
+
+    private class ThemeWideOverrideActivityHeadsUpDisplayTokens: HeadsUpDisplayTokens {
+        override var backgroundColor: DynamicColor {
+            return aliasTokens.backgroundColors[.brandHover]
+        }
+    }
+
+    private class PerControlOverrideHeadsUpDisplayTokens: HeadsUpDisplayTokens {
+        override var cornerRadius: CGFloat {
+            return globalTokens.borderRadius[.xLarge]
+        }
+
+        override var foregroundColor: DynamicColor {
+            return globalTokens.brandColors[.primary]
         }
     }
 }

ios/FluentUI/HUD/HUDTokens.swift
@@ -1,0 +1,45 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+import UIKit
+
+/// Design token set for the `ActivityIndicator` control.
+open class HeadsUpDisplayTokens: ControlTokens {
+
+    /// The color of the squared background of the Heads-up display.
+    open var backgroundColor: DynamicColor { DynamicColor(light: ColorValue(r: 0.129, g: 0.129, b: 0.129, a: 0.9),
+                                                          dark: ColorValue(r: 0.188, g: 0.188, b: 0.188, a: 1)) }
+
+    /// The color of the contents presented by the Heads-up display.
+    open var foregroundColor: DynamicColor { DynamicColor(light: globalTokens.neutralColors[.white],
+                                                          dark: ColorValue(r: 0.882, g: 0.882, b: 0.882, a: 1),
+                                                          darkHighContrast: globalTokens.neutralColors[.white]) }
+
+    /// The corner radius of the squared background of the Heads-up display.
+    open var cornerRadius: CGFloat {
+        return globalTokens.borderRadius[.medium]
+    }
+
+    /// The distance between the label and image contents from the left and right edges of the squared background of the Heads-up display.
+    open var horizontalPadding: CGFloat {
+        return globalTokens.spacing[.small]
+    }
+
+    /// The distance between the label and image contents from the top and bottom edges of the squared background of the Heads-up display.
+    open var verticalPadding: CGFloat {
+        return globalTokens.spacing[.large]
+    }
+
+    /// The minimum value for the side of the squared background of the Heads-up display.
+    open var minSize: CGFloat {
+        return 100
+    }
+
+    /// The maximum value for the side of the squared background of the Heads-up display.
+    open var maxSize: CGFloat {
+        return 192
+    }
+}

ios/FluentUI/HUD/HeadsUpDisplay.swift
@@ -1,0 +1,201 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+/// Defines the content type of the Heads-up display.
+/// The `.activity` value will make the HUD display an Activity Indicator.
+/// The `.success` and `.failure` values will make the HUD display its default images for the cases.
+/// The `.custom` value allows a UIImage to be passed as a parameter for the HUD to display.
+public enum HUDType: Equatable, Hashable {
+    case activity
+    case custom(image: UIImage)
+    case failure
+    case success
+
+    public func hash(into hasher: inout Hasher) {
+        switch self {
+        case .custom(let image):
+            hasher.combine(image)
+        case .activity, .success, .failure:
+                break
+        }
+    }
+}
+
+/// Properties available to customize the state of the Heads-up display state.
+public protocol MSFHUDState {
+
+    /// Label string presented by the Heads-up display.
+    var label: String? { get set }
+
+    /// The action executed when the Heads-up display is tapped.
+    var tapAction: (() -> Void)? { get set }
+
+    /// The `HUDType` enum value of the Heads-up display.
+    var type: HUDType { get set }
+
+    /// Design token set for this control, to use in place of the control's default Fluent tokens.
+    var overrideTokens: HeadsUpDisplayTokens? { get set }
+}
+
+/// View that represents the Heads-up display.
+public struct HeadsUpDisplay: View, ConfigurableTokenizedControl {
+    public var body: some View {
+        let label = state.label ?? ""
+        let type = state.type
+        let foregroundColor = Color(dynamicColor: tokens.foregroundColor)
+        let verticalPadding = tokens.verticalPadding
+        let horizontalPadding = tokens.horizontalPadding
+
+        HStack(alignment: .center) {
+            VStack {
+                switch type {
+                case .activity:
+                    ActivityIndicator(size: .xLarge)
+                        .isAnimating(true)
+                        .color(UIColor(dynamicColor: tokens.foregroundColor))
+                case .custom, .failure, .success:
+                    let image: UIImage = {
+                        switch type {
+                        case.activity:
+                            preconditionFailure("HUDType.activity does not have an associated image.")
+                        case .custom(let image):
+                            return image.withRenderingMode(.alwaysTemplate)
+                        case .failure:
+                            return UIImage.staticImageNamed("dismiss-36x36")!
+                        case.success:
+                            return UIImage.staticImageNamed("checkmark-36x36")!
+                        }
+                    }()
+
+                    Image(uiImage: image)
+                        .foregroundColor(foregroundColor)
+                }
+
+                if !label.isEmpty {
+                    Spacer()
+                        .frame(height: verticalPadding)
+                    Text(label)
+                        .foregroundColor(foregroundColor)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.center)
+                        .fixedSize(horizontal: false, vertical: true)
+                }
+            }
+        }
+        .padding(EdgeInsets(top: verticalPadding,
+                            leading: horizontalPadding,
+                            bottom: verticalPadding,
+                            trailing: horizontalPadding))
+        .squareShaped(minSize: tokens.minSize,
+                      maxSize: tokens.maxSize)
+        .background(Rectangle()
+                        .fill(Color(dynamicColor: tokens.backgroundColor))
+                        .frame(maxWidth: .infinity,
+                               maxHeight: .infinity,
+                               alignment: .center)
+                        .cornerRadius(tokens.cornerRadius)
+        )
+        .contentShape(Rectangle())
+        .onChange(of: isPresented, perform: { present in
+            if present {
+                presentAnimated()
+            } else {
+                dismissAnimated()
+            }
+        })
+        .onAnimationComplete(for: presentationScaleFactor) {
+            resetScaleFactor()
+        }
+        .opacity(opacity)
+        .scaleEffect(presentationScaleFactor)
+        .onTapGesture {
+            state.tapAction?()
+        }
+    }
+
+    /// Initializes the SwiftUI View for the Heads-up display.
+    /// - Parameters:
+    ///   - type: The `HUDType` enum value of the Heads-up display.
+    ///   - isPresented: Binding boolean that controls whether the Heads-up display is presented.
+    ///   - label: Label string presented by the Heads-up display.
+    ///   - tapAction: Closure executed when the user taps on the Heads-up display.
+    public init(type: HUDType,
+                isPresented: Binding<Bool>? = nil,
+                label: String? = nil,
+                tapAction: (() -> Void)? = nil) {
+        let stateImpl = MSFHUDStateImpl(type: type)
+        stateImpl.label = label
+        stateImpl.tapAction = tapAction
+        state = stateImpl
+
+        if let isPresented = isPresented {
+            _isPresented = isPresented
+            opacity = Constants.opacityDismissed
+            presentationScaleFactor = HUD.Constants.showAnimationScale
+        } else {
+            _isPresented = .constant(true)
+            opacity = Constants.opacityPresented
+            presentationScaleFactor = Constants.presentationScaleFactorDefault
+        }
+    }
+
+    let defaultTokens: HeadsUpDisplayTokens = .init()
+    var tokens: HeadsUpDisplayTokens {
+        return resolvedTokens
+    }
+    @Environment(\.fluentTheme) var fluentTheme: FluentTheme
+    @Binding var isPresented: Bool
+    @ObservedObject var state: MSFHUDStateImpl
+
+    private func resetScaleFactor() {
+        guard presentationScaleFactor != Constants.presentationScaleFactorDefault else {
+            return
+        }
+
+        presentationScaleFactor = HUD.Constants.showAnimationScale
+    }
+
+    private func presentAnimated() {
+        withAnimation(.linear(duration: HUD.Constants.showAnimationDuration)) {
+            opacity = Constants.opacityPresented
+            presentationScaleFactor = Constants.presentationScaleFactorDefault
+        }
+    }
+
+    private func dismissAnimated() {
+        withAnimation(.linear(duration: HUD.Constants.hideAnimationDuration)) {
+            opacity = Constants.opacityDismissed
+            presentationScaleFactor = HUD.Constants.hideAnimationScale
+        }
+    }
+
+    @State private var opacity: Double = Constants.opacityPresented
+    @State private var presentationScaleFactor: CGFloat = Constants.presentationScaleFactorDefault
+
+    private struct Constants {
+        static let presentationScaleFactorDefault: CGFloat = 1
+        static let opacityPresented: Double = 1.0
+        static let opacityDismissed: Double = 0.0
+    }
+}
+
+/// Properties available to customize the state of the HUD
+class MSFHUDStateImpl: NSObject,
+                       ObservableObject,
+                       Identifiable,
+                       ControlConfiguration,
+                       MSFHUDState {
+    @Published var label: String?
+    @Published var overrideTokens: HeadsUpDisplayTokens?
+    var tapAction: (() -> Void)?
+    @Published var type: HUDType
+
+    init(type: HUDType) {
+        self.type = type
+        super.init()
+    }
+}

commit 23c308fd4b109f6a67ad198890d14c4cbbfc67cc

[Fluent 2 iOS] HUD colors & padding update (#1189)
* Updated hud colors & padding

* fix crash in HUD Demo app

Regressed by commit 6b205e9 when updating HUD to swiftui control it wasn't referring the the correct fluent demo app asset icon.

* Updated comments

* Changed activity indicator color

* Updated hud colors & padding

* Updated comments

* Changed activity indicator color

* Fixed remaining conflict

* Fixed swiftlint errors

Co-authored-by: Harrie Shin <hyshin@microsoft.com>

ios/FluentUI.Demo/FluentUI.Demo/Demos/HUDDemoController.swift
@@ -301,7 +301,7 @@ extension HUDDemoController: DemoAppearanceDelegate {
             return GlobalTokens.borderRadius(.xLarge)
         }
 
-        override var foregroundColor: DynamicColor {
+        override var labelColor: DynamicColor {
             return aliasTokens.brandColors[.primary]
         }
     }

ios/FluentUI/HUD/HUDTokens.swift
@@ -10,13 +10,13 @@ import UIKit
 open class HeadsUpDisplayTokens: ControlTokens {
 
     /// The color of the squared background of the Heads-up display.
-    open var backgroundColor: DynamicColor { DynamicColor(light: ColorValue(r: 0.129, g: 0.129, b: 0.129, a: 0.9),
-                                                          dark: ColorValue(r: 0.188, g: 0.188, b: 0.188, a: 1)) }
+    open var backgroundColor: DynamicColor { aliasTokens.colors[.backgroundInverted] }
 
-    /// The color of the contents presented by the Heads-up display.
-    open var foregroundColor: DynamicColor { DynamicColor(light: GlobalTokens.neutralColors(.white),
-                                                          dark: ColorValue(r: 0.882, g: 0.882, b: 0.882, a: 1),
-                                                          darkHighContrast: GlobalTokens.neutralColors(.white)) }
+    /// The color of the activity indicator presented by the Heads-up display.
+    open var activityIndicatorColor: DynamicColor { aliasTokens.colors[.foreground2] }
+
+    /// The color of the label presented by the Heads-up display.
+    open var labelColor: DynamicColor { aliasTokens.colors[.foregroundInverted1] }
 
     /// The corner radius of the squared background of the Heads-up display.
     open var cornerRadius: CGFloat {
@@ -31,6 +31,16 @@ open class HeadsUpDisplayTokens: ControlTokens {
     /// The distance between the label and image contents from the top and bottom edges of the squared background of the Heads-up display.
     open var verticalPadding: CGFloat {
         return GlobalTokens.spacing(.large)
+    }
+
+    /// The distance between the top of the hud panel and the activity indicator
+    open var topPadding: CGFloat {
+        return GlobalTokens.spacing(.large)
+    }
+
+    /// The distance between the bottom of the hud panel and the label
+    open var bottomPadding: CGFloat {
+        return GlobalTokens.spacing(.medium)
     }
 
     /// The minimum value for the side of the squared background of the Heads-up display.

ios/FluentUI/HUD/HeadsUpDisplay.swift
@@ -46,7 +46,6 @@ public struct HeadsUpDisplay: View, ConfigurableTokenizedControl {
     public var body: some View {
         let label = state.label ?? ""
         let type = state.type
-        let foregroundColor = Color(dynamicColor: tokens.foregroundColor)
         let verticalPadding = tokens.verticalPadding
         let horizontalPadding = tokens.horizontalPadding
 
@@ -56,7 +55,7 @@ public struct HeadsUpDisplay: View, ConfigurableTokenizedControl {
                 case .activity:
                     ActivityIndicator(size: .xLarge)
                         .isAnimating(true)
-                        .color(UIColor(dynamicColor: tokens.foregroundColor))
+                        .color(UIColor(dynamicColor: tokens.activityIndicatorColor))
                 case .custom, .failure, .success:
                     let image: UIImage = {
                         switch type {
@@ -72,23 +71,23 @@ public struct HeadsUpDisplay: View, ConfigurableTokenizedControl {
                     }()
 
                     Image(uiImage: image)
-                        .foregroundColor(foregroundColor)
+                        .foregroundColor(Color(dynamicColor: tokens.activityIndicatorColor))
                 }
 
                 if !label.isEmpty {
                     Spacer()
                         .frame(height: verticalPadding)
                     Text(label)
-                        .foregroundColor(foregroundColor)
+                        .foregroundColor(Color(dynamicColor: tokens.labelColor))
                         .lineLimit(2)
                         .multilineTextAlignment(.center)
                         .fixedSize(horizontal: false, vertical: true)
                 }
             }
         }
-        .padding(EdgeInsets(top: verticalPadding,
+        .padding(EdgeInsets(top: label.isEmpty ? tokens.verticalPadding : tokens.topPadding,
                             leading: horizontalPadding,
-                            bottom: verticalPadding,
+                            bottom: label.isEmpty ? tokens.verticalPadding : tokens.bottomPadding,
                             trailing: horizontalPadding))
         .squareShaped(minSize: tokens.minSize,
                       maxSize: tokens.maxSize)