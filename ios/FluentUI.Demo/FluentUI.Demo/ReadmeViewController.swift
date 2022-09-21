1bd3b0b5ca7cb3404f38f688488f8926e52710c6
Lamine Male <106181067+laminesm@users.noreply.github.com>
2022-09-21 15:17:29 -0700

    Created scroll view and labels

--- ios/FluentUI.Demo/FluentUI.Demo/ReadmeViewController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/ReadmeViewController.swift
@@ -4,14 +4,77 @@
 //
 
 import UIKit
+import FluentUI
 
 class ReadmeViewController: UIViewController {
+
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+
+    let titleLabel: Label = {
+        let label = Label()
+        label.text = "Readme"
+        label.style = .headline
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.textColor = .blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let subtitleLabel: Label = {
+        let label = Label()
+        label.text = "Body"
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupScrollViewConstraints()
+        setupViews()
+    }
+
+    private func setupViews() {
+        contentView.addSubview(titleLabel)
+        titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35).isActive = true
+        titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3 / 4).isActive = true
+
+        contentView.addSubview(subtitleLabel)
+        subtitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 25).isActive = true
+        subtitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3 / 4).isActive = true
+        subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+
+    private func setupScrollViewConstraints() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+
+        scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+    }
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 
         modalPresentationStyle = .popover
-        preferredContentSize.height = 600
+        preferredContentSize = CGSize(width: 300, height: 400)
         popoverPresentationController?.permittedArrowDirections = .up
     }
 
