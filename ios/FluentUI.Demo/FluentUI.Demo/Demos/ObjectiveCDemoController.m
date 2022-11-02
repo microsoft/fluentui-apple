f65c49db414e8e4f1d7f1752467915c8e15e1a3d
Mike Schreiber <mischreiber@microsoft.com>
2022-11-02 15:10:23 -0700

    WIP: `@objc` for `GlobalTokens`

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/ObjectiveCDemoController.m
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/ObjectiveCDemoController.m
@@ -60,6 +60,9 @@
 
 - (MSFButton *)createButtonWithTitle:(NSString *)title action:(SEL)action {
     MSFButton* button = [[MSFButton alloc] init];
+    MSFColorValue *colorValue = [MSFGlobalTokens colorForSharedColorSet:MSFSharedColorSetsPink
+                                                                  token:MSFSharedColorsTokensPrimary];
+    button.titleLabel.textColor = [[UIColor alloc] initWithColorValue:colorValue];
     button.titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
     [button setTitle:title forState:UIControlStateNormal];
     [button addTarget:self action:action forControlEvents:UIControlEventTouchUpInside];
