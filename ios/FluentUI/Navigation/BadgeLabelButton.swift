a2c48be8ea798f32cc65db4c45b99a6927df442d
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-11-10 16:02:14 -0800

    Clean up BadgeLabelButton

--- ios/FluentUI/Navigation/BadgeLabelButton.swift
+++ ios/FluentUI/Navigation/BadgeLabelButton.swift
@@ -25,9 +25,7 @@ class BadgeLabelButton: UIButton {
 
         super.init(frame: frame)
 
-        if #available(iOS 15.0, *) {
-            configuration = UIButton.Configuration.plain()
-        }
+        configuration = UIButton.Configuration.plain()
 
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(badgeValueDidChange),
@@ -69,15 +67,9 @@ class BadgeLabelButton: UIButton {
 
     private var badgeFrameOriginX: CGFloat {
         let xOrigin: CGFloat = {
-            if #available(iOS 15.0, *) {
-                return isLeftToRightUserInterfaceLayoutDirection ?
-                frame.size.width - (configuration?.contentInsets.leading ?? 0) :
-                configuration?.contentInsets.trailing ?? 0
-            } else {
-                return isLeftToRightUserInterfaceLayoutDirection ?
-                frame.size.width - contentEdgeInsets.left :
-                contentEdgeInsets.left
-            }
+            return isLeftToRightUserInterfaceLayoutDirection ?
+            frame.size.width - (configuration?.contentInsets.leading ?? 0) :
+            configuration?.contentInsets.trailing ?? 0
         }()
 
         return (xOrigin - badgeWidth / 2)
@@ -128,13 +120,8 @@ class BadgeLabelButton: UIButton {
             landscapeImage = landscapeImage?.withRenderingMode(.alwaysTemplate)
         }
 
-        if #available(iOS 15.0, *) {
-            configuration?.image = traitCollection.verticalSizeClass == .regular ? portraitImage : landscapeImage
-            configuration?.title = item.title
-        } else {
-            setImage(traitCollection.verticalSizeClass == .regular ? portraitImage : landscapeImage, for: .normal)
-            setTitle(item.title, for: .normal)
-        }
+        configuration?.image = traitCollection.verticalSizeClass == .regular ? portraitImage : landscapeImage
+        configuration?.title = item.title
 
         if let action = item.action {
             addTarget(item.target, action: action, for: .touchUpInside)
