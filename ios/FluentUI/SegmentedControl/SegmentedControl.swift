c2e02cb0823865b02608c01faef4f0cf1bd0d310
Mike Schreiber <mischreiber@microsoft.com>
2022-11-16 15:04:27 -0800

    Remove retain cycles

--- ios/FluentUI/SegmentedControl/SegmentedControl.swift
+++ ios/FluentUI/SegmentedControl/SegmentedControl.swift
@@ -415,7 +415,9 @@ open class SegmentedControl: UIView, TokenizedControlInternal {
     }
 
     public typealias TokenSetKeyType = SegmentedControlTokenSet.Tokens
-    lazy public var tokenSet: SegmentedControlTokenSet = .init(style: { self.style })
+    lazy public var tokenSet: SegmentedControlTokenSet = .init(style: { [weak self] in
+        return self?.style ?? .primaryPill
+    })
     var tokenSetSink: AnyCancellable?
 
     var selectionChangeAnimationDuration: TimeInterval { return 0.2 }
