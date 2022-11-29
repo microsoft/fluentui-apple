6ce330bd73fb3a28295b568d539e072574e3e592
huwilkes <67026548+huwilkes@users.noreply.github.com>
2022-11-29 09:22:58 -0800

    Update demo

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/ButtonDemoController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/ButtonDemoController.swift
@@ -17,10 +17,12 @@ class ButtonDemoController: DemoController {
 
             let button = Button(style: style)
             button.setTitle("Button", for: .normal)
+            buttons.append(button)
 
             let disabledButton = Button(style: style)
             disabledButton.isEnabled = false
             disabledButton.setTitle("Button", for: .normal)
+            buttons.append(disabledButton)
 
             addRow(items: [button, disabledButton], itemSpacing: 20)
 
@@ -28,20 +30,24 @@ class ButtonDemoController: DemoController {
                 let iconButton = Button(style: style)
                 iconButton.setTitle("Button", for: .normal)
                 iconButton.image = image
+                buttons.append(iconButton)
 
                 let disabledIconButton = Button(style: style)
                 disabledIconButton.isEnabled = false
                 disabledIconButton.setTitle("Button", for: .normal)
                 disabledIconButton.image = image
+                buttons.append(disabledIconButton)
 
                 addRow(items: [iconButton, disabledIconButton], itemSpacing: 20)
 
                 let iconOnlyButton = Button(style: style)
                 iconOnlyButton.image = image
+                buttons.append(iconOnlyButton)
 
                 let disabledIconOnlyButton = Button(style: style)
                 disabledIconOnlyButton.isEnabled = false
                 disabledIconOnlyButton.image = image
+                buttons.append(disabledIconOnlyButton)
 
                 addRow(items: [iconOnlyButton, disabledIconOnlyButton], itemSpacing: 20)
             }
@@ -51,17 +57,21 @@ class ButtonDemoController: DemoController {
         let button = Button(style: .primaryFilled)
         button.setTitle("Longer Text Button", for: .normal)
         button.titleLabel?.numberOfLines = 0
+        buttons.append(button)
 
         let iconButton = Button(style: .primaryFilled)
         iconButton.setTitle("Longer Text Button", for: .normal)
         iconButton.titleLabel?.numberOfLines = 0
         iconButton.image = ButtonStyle.primaryFilled.image
+        buttons.append(iconButton)
 
         addRow(items: [button])
         addRow(items: [iconButton])
 
         container.addArrangedSubview(UIView())
     }
+
+    private var buttons: [Button] = []
 }
 
 extension ButtonStyle {
@@ -93,5 +103,39 @@ extension ButtonStyle {
         case .tertiaryOutline:
             return nil
         }
+    }
+}
+
+extension ButtonDemoController: DemoAppearanceDelegate {
+    func themeWideOverrideDidChange(isOverrideEnabled: Bool) {
+        guard let fluentTheme = self.view.window?.fluentTheme else {
+            return
+        }
+
+        fluentTheme.register(tokenSetType: ButtonTokenSet.self, tokenSet: isOverrideEnabled ? themeWideOverrideButtonTokens : nil)
+    }
+
+    func perControlOverrideDidChange(isOverrideEnabled: Bool) {
+        for button in buttons {
+            button.tokenSet.replaceAllOverrides(with: isOverrideEnabled ? perControlOverrideButtonTokens : nil)
+        }
+    }
+
+    func isThemeWideOverrideApplied() -> Bool {
+        return self.view.window?.fluentTheme.tokens(for: ButtonTokenSet.self)?.isEmpty == false
+    }
+
+    // MARK: - Custom tokens
+
+    private var themeWideOverrideButtonTokens: [ButtonTokenSet.Tokens: ControlTokenValue] {
+        return [
+            .titleFont: .fontInfo { FontInfo(name: "Times", size: 20.0, weight: .regular) },
+        ]
+    }
+
+    private var perControlOverrideButtonTokens: [ButtonTokenSet.Tokens: ControlTokenValue] {
+        return [
+            .titleFont: .fontInfo { FontInfo(name: "Papyrus", size: 20.0, weight: .regular) }
+        ]
     }
 }
