adbdec5724be3b0b6cb3f9fc6275b4eca7176e89
James Edwards <edjamesmsft@outlook.com>
2022-12-06 13:09:17 -0800

    [Fluent2 iOS] Fluent2 BadgeLabel (#1426)
    * Updating BadgeLabel to use global and alias tokens instead of Colors.* based colors.
    
    * Making textColor and backgroundColor symmetrical
    
    * Refactoring
    
    * Adding themeDidChange functionality

--- ios/FluentUI/Label/BadgeLabel.swift
+++ ios/FluentUI/Label/BadgeLabel.swift
@@ -23,21 +23,38 @@ class BadgeLabel: UILabel {
     /// Base function for initialization
     private func initBase() {
         layer.masksToBounds = true
-        backgroundColor = Colors.Palette.dangerPrimary.color
-        textColor = .white
         textAlignment = .center
         font = UIFont.systemFont(ofSize: Constants.badgeFontSize, weight: .regular)
         isHidden = true
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(themeDidChange),
+                                               name: .didChangeTheme,
+                                               object: nil)
+
+        updateColors()
     }
 
     override func didMoveToWindow() {
         super.didMoveToWindow()
+        updateColors()
+    }
 
-        guard shouldUseWindowColor, let window = window else {
+    @objc private func themeDidChange(_ notification: Notification) {
+        guard let themeView = notification.object as? UIView, self.isDescendant(of: themeView) else {
             return
         }
-        textColor = UIColor(light: Colors.primary(for: window), dark: .white)
-        backgroundColor = UIColor(light: .white, dark: Colors.primary(for: window))
+        updateColors()
+    }
+
+    private func updateColors() {
+        if shouldUseWindowColor {
+            textColor = UIColor(dynamicColor: DynamicColor(light: fluentTheme.aliasTokens.colors[.brandForeground1].light, dark: GlobalTokens.neutralColors(.white)))
+            backgroundColor = UIColor(dynamicColor: DynamicColor(light: GlobalTokens.neutralColors(.white), dark: fluentTheme.aliasTokens.colors[.brandBackground1].dark))
+        } else {
+            textColor = UIColor(colorValue: GlobalTokens.neutralColors(.white))
+            backgroundColor = UIColor(dynamicColor: fluentTheme.aliasTokens.sharedColors[.dangerBackground2])
+        }
     }
 
     private struct Constants {
