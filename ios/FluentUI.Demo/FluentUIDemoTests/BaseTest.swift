d0a2349e63a168429557578d8b4c4c66e4f7925f
Joanna Qu <joannaquu@gmail.com>
2023-01-09 14:13:13 -0800

    remove setup logic

--- ios/FluentUI.Demo/FluentUIDemoTests/BaseTest.swift
+++ ios/FluentUI.Demo/FluentUIDemoTests/BaseTest.swift
@@ -7,7 +7,6 @@ import XCTest
 
 class BaseTest: XCTestCase {
     let app = XCUIApplication()
-    let fluentUIDev: String = "FluentUI DEV"
     // must be overridden
     var controlName: String { "Base" }
 
@@ -15,27 +14,14 @@ class BaseTest: XCTestCase {
         try super.setUpWithError()
         continueAfterFailure = false
         app.launch()
-
-        let onHomePage: Bool = app.staticTexts[fluentUIDev].exists
-        let controlPage: XCUIElement = app.staticTexts[controlName]
-        let onDifferentControlPage: Bool = !onHomePage && !controlPage.exists
-        let backButton: XCUIElement = app.buttons[fluentUIDev].exists ? app.buttons[fluentUIDev] : app.buttons["Dismiss"]
-
-        if onHomePage {
-            XCTAssertTrue(!onDifferentControlPage)
-            controlPage.tap()
-        } else if onDifferentControlPage {
-            XCTAssertTrue(!onHomePage && !controlPage.exists)
-            backButton.tap()
-            controlPage.tap()
-        }
+        app.staticTexts[controlName].tap()
     }
 
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         app.terminate()
         let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
-        springboard.icons[fluentUIDev].press(forDuration: 1)
+        springboard.icons["FluentUI DEV"].press(forDuration: 1)
         springboard.buttons["Remove App"].tap()
         springboard.alerts.buttons["Delete App"].tap()
         springboard.alerts.buttons["Delete"].tap()
