d47e058daabeea49d2c5e7dcbf3500829884b27d
Joanna Qu <joannaquu@gmail.com>
2023-01-27 13:28:29 -0800

    change to convenience init

--- ios/FluentUI/People Picker/PersonaBadgeViewDataSource.swift
+++ ios/FluentUI/People Picker/PersonaBadgeViewDataSource.swift
@@ -11,9 +11,13 @@ import UIKit
 open class PersonaBadgeViewDataSource: BadgeViewDataSource {
     @objc open var persona: Persona
 
-    @objc public init(persona: Persona, style: BadgeView.Style = .default, size: BadgeView.Size = .medium, customView: UIView? = nil) {
+    @objc public init(persona: Persona, style: BadgeView.Style = .default, size: BadgeView.Size = .medium) {
         self.persona = persona
         super.init(text: persona.name, style: style, size: size)
+    }
+
+    @objc public convenience init(persona: Persona, style: BadgeView.Style = .default, size: BadgeView.Size = .medium, customView: UIView? = nil) {
+        self.init(persona: persona, style: style, size: size)
         super.customView = customView
     }
 }
