1b5b7a08d5d69d77c5e4f29fe74adc51627e646a
Lamine Male <106181067+laminesm@users.noreply.github.com>
2023-02-07 12:48:08 -0800

    Remove duplicated color assignments

--- ios/FluentUI/Pill Button Bar/PillButton.swift
+++ ios/FluentUI/Pill Button Bar/PillButton.swift
@@ -255,9 +255,6 @@ open class PillButton: UIButton {
                     setTitleColor(customSelectedTextColor ?? PillButton.selectedHighlightedTitleColor(for: fluentTheme, for: style),
                                   for: .highlighted)
                 }
-
-                setTitleColor(customSelectedTextColor ?? PillButton.selectedTitleColor(for: fluentTheme, for: style), for: .normal)
-                setTitleColor(customSelectedTextColor ?? PillButton.selectedHighlightedTitleColor(for: fluentTheme, for: style), for: .highlighted)
             } else {
                 resolvedBackgroundColor = PillButton.selectedDisabledBackgroundColor(for: fluentTheme, for: style)
                 if #available(iOS 15.0, *) {
