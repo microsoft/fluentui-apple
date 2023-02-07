6c2dfbf4c68999e57db3af451d1c22398e0b61e7
jeaniehuynh <31874971+jeaniehuynh@users.noreply.github.com>
2023-02-06 17:15:21 -0800

    Bring BottomSheet from fluent2-tokens into main

---
+++ ios/FluentUI/Bottom Sheet/BottomSheetTokenSet.swift
@@ -0,0 +1,29 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import UIKit
+
+public class BottomSheetTokenSet: ControlTokenSet<BottomSheetTokenSet.Tokens> {
+	public enum Tokens: TokenSetKey {
+		case backgroundColor
+		case cornerRadius
+		case shadow
+	}
+
+	init() {
+		super.init { token, theme in
+			switch token {
+			case .backgroundColor:
+				return .dynamicColor { DynamicColor(light: GlobalTokens.neutralColors(.white),
+													dark: ColorValue(0x212121))
+				}
+			case .cornerRadius:
+				return .float { GlobalTokens.corner(.radius120) }
+			case .shadow:
+				return .shadowInfo { theme.aliasTokens.shadow[.shadow28] }
+			}
+		}
+	}
+}
