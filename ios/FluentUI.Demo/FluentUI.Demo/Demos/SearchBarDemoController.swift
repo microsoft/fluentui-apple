063421418f0bb6a101f7d58948effc1c86de6d2d
Lamine Male <106181067+laminesm@users.noreply.github.com>
2023-02-21 19:53:18 -0500

    Add brand style to SearchBar demo (#1591)
    * Add brand style to searchbar demo
    
    * Add custom stackview to change the background

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/SearchBarDemoController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/SearchBarDemoController.swift
@@ -11,6 +11,7 @@ class SearchBarDemoController: DemoController, SearchBarDelegate {
         static let badgeViewCornerRadius: CGFloat = 10
         static let badgeViewSideLength: CGFloat = 20
         static let badgeViewMaxFontSize: CGFloat = 40
+        static let searchBarStackviewMargin: CGFloat = 16
     }
 
     private lazy var searchBarWithBadgeView: SearchBar =
@@ -28,16 +29,70 @@ class SearchBarDemoController: DemoController, SearchBarDelegate {
         return buildBadgeView(text: "Kat Larsson", customView: imageView)
     }()
 
+    private var searchBars: [SearchBar] = []
+
+    let segmentedControl: SegmentedControl = {
+        let segmentedControl = SegmentedControl(items: [SegmentItem(title: "System"), SegmentItem(title: "Brand")],
+                                                style: .primaryPill)
+
+        return segmentedControl
+    }()
+
+    @objc private func updateSearchbars() {
+        if segmentedControl.selectedSegmentIndex == 1 {
+            searchBarsStackView.backgroundColor = NavigationBar.Style.primary.backgroundColor(fluentTheme: view.fluentTheme)
+            updateSearchBarsStyles(to: .lightContent)
+        } else {
+            searchBarsStackView.backgroundColor = NavigationBar.Style.system.backgroundColor(fluentTheme: view.fluentTheme)
+            updateSearchBarsStyles(to: .darkContent)
+        }
+    }
+
+    private func updateSearchBarsStyles(to style: SearchBar.Style) {
+        for searchBar in searchBars {
+            searchBar.style = style
+        }
+    }
+
+    // Used to change the SearchBars' background color between brand and system styles
+    private let searchBarsStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.layoutMargins = UIEdgeInsets(top: Constants.searchBarStackviewMargin,
+                                               left: Constants.searchBarStackviewMargin,
+                                               bottom: Constants.searchBarStackviewMargin,
+                                               right: Constants.searchBarStackviewMargin)
+        stackView.axis = .vertical
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.spacing = Constants.searchBarStackviewMargin
+        return stackView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let searchBarNoAutocorrect = buildSearchBar(autocorrectionType: .no, placeholderText: "no autocorrect")
         let searchBarAutocorrect = buildSearchBar(autocorrectionType: .yes, placeholderText: "autocorrect")
 
-        container.addArrangedSubview(searchBarNoAutocorrect)
-        container.addArrangedSubview(searchBarAutocorrect)
-        container.addArrangedSubview(searchBarWithBadgeView)
-        container.addArrangedSubview(searchBarWithAvatarBadgeView)
+        searchBars = [searchBarNoAutocorrect, searchBarAutocorrect, searchBarWithBadgeView, searchBarWithAvatarBadgeView]
+
+        container.addArrangedSubview(segmentedControl)
+        container.addArrangedSubview(UIView())
+
+        for searchBar in searchBars {
+            searchBarsStackView.addArrangedSubview(searchBar)
+        }
+
+        container.addArrangedSubview(searchBarsStackView)
+
+        segmentedControl.onSelectAction = { [weak self] (_, _) in
+            guard let strongSelf = self else {
+                return
+            }
+
+            strongSelf.updateSearchbars()
+        }
+
+        updateSearchbars()
     }
 
     func buildBadgeView(text: String, customView: UIView? = nil) -> UIView {
