b4cbcf301e3b0e1212ecb221a1a49bcaa3c88868
Des Marks <johnmarks@microsoft.com>
2023-02-23 12:18:17 -0800

    Add title transformer

--- ios/FluentUI/Pill Button Bar/PillButton.swift
+++ ios/FluentUI/Pill Button Bar/PillButton.swift
@@ -221,6 +221,13 @@ open class PillButton: UIButton, TokenizedControlInternal {
         var attributedTitle = AttributedString(itemTitle)
         attributedTitle.font = UIFont.fluent(titleFont)
         configuration?.attributedTitle = attributedTitle
+        
+        let attributedTitleTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.fluent(self.titleFont)
+            return outgoing
+        }
+        configuration?.titleTextAttributesTransformer = attributedTitleTransformer
 
         // Workaround for Apple bug: when UIButton.Configuration is used with UIControl's isSelected = true, accessibilityLabel doesn't get set automatically
         accessibilityLabel = itemTitle
