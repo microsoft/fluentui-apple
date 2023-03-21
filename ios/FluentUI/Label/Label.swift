b8918e7f1a3eb0dbbe7b88f1b6db6c2279dd331a
Adam Gleitman <adgleitm@microsoft.com>
2023-03-21 13:31:53 -0700

    Invalidate Label font when content size category changes

--- ios/FluentUI/Label/Label.swift
+++ ios/FluentUI/Label/Label.swift
@@ -127,6 +127,7 @@ open class Label: UILabel, TokenizedControlInternal {
 
     @objc private func handleContentSizeCategoryDidChange() {
         if adjustsFontForContentSizeCategory {
+            labelFont = nil
             updateFont()
         }
     }
