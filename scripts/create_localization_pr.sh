bcfb803144431c8fda067b652f72642654cd4960
huwilkes <huwilkes@microsoft.com>
2023-03-29 12:52:37 -0700

    Update localization action

--- scripts/create_localization_pr.sh
+++ scripts/create_localization_pr.sh
@@ -4,6 +4,15 @@
 
 newBranch="tdbuild`date +%s`"
 
-git checkout -b $newBranch
-git push --set-upstream origin $newBranch
-gh pr create -B main -H $newBranch --title '[Localization] Localized Resource Files' --body  'Latest localized resource files from Touchdown Build' -l 'Automated PR'
+if [[ `git status --porcelain` ]]; then
+  echo "Pushing new localization changes"
+  git config --global user.email $EMAIL
+  git config --global user.name $NAME
+  git add .
+  git commit -m 'Latest localized resource files from Touchdown Build'
+  git checkout -b $newBranch
+  git push --set-upstream origin $newBranch
+  gh pr create -B main -H $newBranch --title '[Localization] Localized Resource Files' --body  'Latest localized resource files from Touchdown Build' -l 'Automated PR'
+else
+  echo "No changes found"
+fi
