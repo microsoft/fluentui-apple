fd3ecddd3e34d12e414e86a54a54c5c81c02dff6
James Edwards <mosuve@microsoft.com>
2023-04-11 15:15:03 -0700

    Fixing deprecation message

--- ios/FluentUI/Navigation/SearchBar/SearchBarTokenSet.swift
+++ ios/FluentUI/Navigation/SearchBar/SearchBarTokenSet.swift
@@ -180,9 +180,9 @@ extension SearchBarTokenSet {
 public extension SearchBar {
     @objc(MSFSearchBarStyle)
     enum Style: Int {
-        @available(*, deprecated, message: "lightContent is now deprecated. Please use onCanvas.")
+        @available(*, deprecated, message: "lightContent is now deprecated. Please use onBrandNavigationBar.")
         case lightContent
-        @available(*, deprecated, message: "darkContent is now deprecated. Please use onBrand.")
+        @available(*, deprecated, message: "darkContent is now deprecated. Please use onCanvas.")
         case darkContent
         case onCanvas
         case onSystemNavigationBar
