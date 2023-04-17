c0143c1d18143411d2f1f34c77eac7bcccea5981
Sophia <sophia.lee0416@gmail.com>
2023-04-17 17:16:38 -0400

    Tokenizing Separator

---
+++ ios/FluentUI/Separator/SeparatorTokenSet.swift
@@ -0,0 +1,26 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import UIKit
+
+/// Design token set for the `Separator` control.
+public class SeparatorTokenSet: ControlTokenSet<SeparatorTokenSet.Tokens> {
+    public enum Tokens: TokenSetKey {
+        /// The color of the Separator
+        case color
+    }
+
+    init() {
+        super.init { token, theme in
+            switch token {
+            case .color:
+                return .uiColor { theme.color(.stroke2) }
+            }
+        }
+    }
+
+    /// The default thickness for the divider: half pt.
+    static var thickness: CGFloat { return GlobalTokens.stroke(.width05) }
+}
