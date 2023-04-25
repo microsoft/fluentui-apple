d1255de957b72ed16e1726e83f07613b0bf89850
Mike Schreiber <mischreiber@microsoft.com>
2023-04-25 08:56:24 -0700

    Bumping iOS SDK to 15.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.7
 
 import PackageDescription
 
@@ -6,7 +6,7 @@ let package = Package(
     name: "FluentUI",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
         .macOS(.v10_15),
     ],
     products: [
