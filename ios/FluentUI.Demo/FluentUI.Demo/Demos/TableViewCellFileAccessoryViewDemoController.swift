6e2022dfa23158836dc1fdbecd0b43a1ff2dee87
huwilkes <huwilkes@microsoft.com>
2023-06-09 13:59:04 -0700

    Update usage of  in demos

--- ios/FluentUI.Demo/FluentUI.Demo/Demos/TableViewCellFileAccessoryViewDemoController.swift
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/TableViewCellFileAccessoryViewDemoController.swift
@@ -294,7 +294,7 @@ class TableViewCellFileAccessoryViewDemoController: DemoTableViewController {
     private lazy var minusBottomOverlapButton: UIButton = createPlusMinusButton(plus: false, #selector(decrementBottomActionsOverlap))
 
     private func createPlusMinusButton(plus: Bool, _ selector: Selector) -> UIButton {
-        let button = Button(style: .outline)
+        let button = Button(style: .outlineAccent)
         button.image = UIImage(named: plus ? "ic_fluent_add_20_regular" : "ic_fluent_subtract_20_regular")
         button.addTarget(self,
                          action: selector,
