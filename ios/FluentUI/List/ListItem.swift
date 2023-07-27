d2eed04ab84285e7bb613c7e377cc2f49209f507
Alexander Boswell <alboswel@microsoft.com>
2023-07-27 12:07:57 -0700

    ListItem initial MVP (#1845)
    Setup new control for ListItem that is a SwiftUI version of MSFTableViewCell

---
+++ ios/FluentUI/List/ListItem.swift
@@ -0,0 +1,213 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+public typealias ListItemAccessoryType = TableViewCellAccessoryType
+public typealias ListItemBackgroundStyleType = TableViewCellBackgroundStyleType
+public typealias ListItemCustomViewSize = MSFTableViewCellCustomViewSize
+public typealias ListItemTokenSet = TableViewCellTokenSet
+
+/// View that represents an item in a List.
+public struct ListItem<LeadingContent: View,
+                       TrailingContent: View,
+                       Title: StringProtocol,
+                       Subtitle: StringProtocol,
+                       Footer: StringProtocol>: View {
+
+	// MARK: Initializer
+
+	/// Creates a ListItem view
+	/// - Parameters:
+	///   - title: Text that appears as the first line of text
+	///   - subtitle: Text that appears as the second line of text
+	///   - footer: Text that appears as the third line of text
+	///   - leadingContent: The content that appears on the leading edge of the view
+	///   - trailingContent: The content that appears on the trailing edge of the view, next to the accessory type if provided
+    public init(title: Title,
+                subtitle: Subtitle = String(),
+                footer: Footer = String(),
+                @ViewBuilder leadingContent: @escaping () -> LeadingContent,
+                @ViewBuilder trailingContent: @escaping () -> TrailingContent) {
+        self.title = title
+        self.subtitle = subtitle
+        self.footer = footer
+        self.leadingContent = leadingContent
+        self.trailingContent = trailingContent
+    }
+
+    public var body: some View {
+        tokenSet.update(fluentTheme)
+
+        @ViewBuilder
+        var labelStack: some View {
+            let titleView = Text(title)
+                                .foregroundColor(Color(uiColor: tokenSet[.titleColor].uiColor))
+                                .font(Font(tokenSet[.titleFont].uiFont))
+                                .frame(minHeight: ListItemTokenSet.titleHeight)
+                                .lineLimit(state.titleLineLimit)
+                                .truncationMode(state.titleTruncationMode)
+
+            switch layoutType {
+            case .oneLine:
+                titleView
+            case .twoLines, .threeLines:
+                let subtitleView = Text(subtitle)
+                    .foregroundColor(Color(uiColor: tokenSet[.subtitleColor].uiColor))
+                    .lineLimit(state.subtitleLineLimit)
+                    .truncationMode(state.subtitleTruncationMode)
+                VStack(alignment: .leading, spacing: ListItemTokenSet.labelVerticalSpacing) {
+                    titleView
+                    if layoutType == .twoLines {
+                        subtitleView
+                            .font(Font(tokenSet[.subtitleTwoLinesFont].uiFont))
+                            .frame(minHeight: ListItemTokenSet.subtitleTwoLineHeight)
+                    } else if layoutType == .threeLines {
+                        subtitleView
+                            .font(Font(tokenSet[.subtitleThreeLinesFont].uiFont))
+                            .frame(minHeight: ListItemTokenSet.subtitleThreeLineHeight)
+                        Text(footer)
+                            .foregroundColor(Color(uiColor: tokenSet[.footerColor].uiColor))
+                            .font(Font(tokenSet[.footerFont].uiFont))
+                            .frame(minHeight: ListItemTokenSet.footerHeight)
+                            .lineLimit(state.footerLineLimit)
+                            .truncationMode(state.footerTruncationMode)
+                    }
+                }
+            }
+        }
+
+        @ViewBuilder
+        var accessoryView: some View {
+            if let accessoryType = state.accessoryType,
+               let icon = accessoryType.icon,
+               let iconColor = accessoryType.iconColor(tokenSet: tokenSet, fluentTheme: fluentTheme) {
+                let image = Image(uiImage: icon)
+                    .foregroundColor(Color(uiColor: iconColor))
+                if accessoryType == .detailButton {
+                    SwiftUI.Button {
+                        if let onAccessoryTapped = state.onAccessoryTapped {
+                            onAccessoryTapped()
+                        }
+                    } label: {
+                        image
+                    }
+                } else {
+                    image
+                }
+            }
+        }
+
+        @ViewBuilder
+        var backgroundView: some View {
+            if let backgroundColor = state.backgroundStyleType.defaultColor(tokenSet: tokenSet) {
+                Color(backgroundColor)
+            }
+        }
+
+        @ViewBuilder
+        var contentView: some View {
+            HStack(alignment: .center) {
+                if let leadingContent {
+                    leadingContent()
+                        .padding(.trailing, ListItemTokenSet.horizontalSpacing)
+                }
+                labelStack
+                    .padding(.trailing, ListItemTokenSet.horizontalSpacing)
+                Spacer()
+                if let trailingContent {
+                    trailingContent()
+                        .tint(Color(fluentTheme.color(.brandForeground1)))
+                }
+                accessoryView
+                    .padding(.leading, ListItemTokenSet.horizontalSpacing)
+            }
+            .padding(EdgeInsets(top: ListItemTokenSet.paddingVertical,
+                                leading: ListItemTokenSet.paddingLeading,
+                                bottom: ListItemTokenSet.paddingVertical,
+                                trailing: ListItemTokenSet.paddingTrailing))
+            .frame(minHeight: layoutType.minHeight)
+            .background(backgroundView)
+            .listRowInsets(EdgeInsets())
+        }
+
+        return contentView
+    }
+
+    @StateObject var state: ListItemState = ListItemState()
+
+    private var layoutType: LayoutType {
+        if !subtitle.isEmpty {
+            if !footer.isEmpty {
+                return .threeLines
+            }
+            return .twoLines
+        }
+        return .oneLine
+    }
+
+    private enum LayoutType {
+        case oneLine
+        case twoLines
+        case threeLines
+
+        var minHeight: CGFloat {
+            switch self {
+            case .oneLine:
+                return ListItemTokenSet.oneLineMinHeight
+            case .twoLines:
+                return ListItemTokenSet.twoLineMinHeight
+            case .threeLines:
+                return ListItemTokenSet.threeLineMinHeight
+            }
+        }
+    }
+
+    @Environment(\.fluentTheme) private var fluentTheme: FluentTheme
+
+    private var leadingContent: (() -> LeadingContent)?
+    private var trailingContent: (() -> TrailingContent)?
+
+    private let footer: Footer
+    private let subtitle: Subtitle
+    private let title: Title
+    private let tokenSet: ListItemTokenSet = ListItemTokenSet(customViewSize: { ListItemCustomViewSize.zero })
+}
+
+// MARK: Additional Initializers
+
+public extension ListItem where LeadingContent == EmptyView, TrailingContent == EmptyView {
+    init(title: Title,
+         subtitle: Subtitle = String(),
+         footer: Footer = String()) {
+        self.title = title
+        self.subtitle = subtitle
+        self.footer = footer
+    }
+}
+
+public extension ListItem where TrailingContent == EmptyView {
+    init(title: Title,
+         subtitle: Subtitle = String(),
+         footer: Footer = String(),
+         @ViewBuilder leadingContent: @escaping () -> LeadingContent) {
+        self.title = title
+        self.subtitle = subtitle
+        self.footer = footer
+        self.leadingContent = leadingContent
+    }
+}
+
+public extension ListItem where LeadingContent == EmptyView {
+    init(title: Title,
+         subtitle: Subtitle = String(),
+         footer: Footer = String(),
+         @ViewBuilder trailingContent: @escaping () -> TrailingContent) {
+        self.title = title
+        self.subtitle = subtitle
+        self.footer = footer
+        self.trailingContent = trailingContent
+    }
+}
---
+++ ios/FluentUI/List/ListItemState.swift
@@ -0,0 +1,36 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+/// Properties available to customize the appearance of a `ListItem`.
+class ListItemState: ControlState {
+    /// The `ListItemAccessoryType` that the view should display.
+    var accessoryType: ListItemAccessoryType?
+
+    /// The background styling of the `ListItem` to match the type of `List` it is displayed in.
+    var backgroundStyleType: ListItemBackgroundStyleType = .plain
+
+    /// The handler for when the `detailButton` accessory view is tapped.
+    var onAccessoryTapped: (() -> Void)?
+
+    /// The maximum amount of lines shown for the `title`.
+    var titleLineLimit: Int = 1
+
+    /// The maximum amount of lines shown for the `subtitle`.
+    var subtitleLineLimit: Int = 1
+
+    /// The maximum amount of lines shown for the `footer`.
+    var footerLineLimit: Int = 1
+
+    /// The truncation mode of the `title`.
+    var titleTruncationMode: Text.TruncationMode = .tail
+
+    /// The truncation mode of the `subtitle`.
+    var subtitleTruncationMode: Text.TruncationMode = .tail
+
+    /// The truncation mode of the `footer`.
+    var footerTruncationMode: Text.TruncationMode = .tail
+}
