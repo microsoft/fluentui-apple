e24c61017cc6468a387e978a22a227644878a842
Alexander Boswell <alboswel@microsoft.com>
2023-07-31 09:00:45 -0700

    Setup new files

---
+++ ios/FluentUI.Demo/FluentUI.Demo/Demos/ListItemDemoController.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import FluentUI
+import UIKit
+
+class ListItemDemoController: DemoController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
