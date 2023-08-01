079af858132c6174dd23e776e7a8beec09da5545
Alexander Boswell <alboswel@microsoft.com>
2023-08-01 16:56:43 -0700

    [ListItem] Add support for leading content size in tokens (#1855)
    * support custom view size
    * fix list item modifiers to not use an @ObservedObject
    * rename file

--- ios/FluentUI/List/ListItem+Extensions.swift
+++ ios/FluentUI/List/ListItem+Extensions.swift
@@ -9,47 +9,62 @@ public extension ListItem {
     /// - Parameter accessoryType: Type of accessory to display.
     /// - Returns: The modified `ListItem` with the property set.
     func accessoryType(_ accessoryType: ListItemAccessoryType) -> ListItem {
-        state.accessoryType = accessoryType
-        return self
+        var listItem = self
+        listItem.accessoryType = accessoryType
+        return listItem
     }
 
     /// The handler for when the `detailButton` accessory is tapped.
     /// - Parameter handler: The logic to execute when the accessory is tapped.
     /// - Returns: The modified `ListItem` with the property set.
     func onAccessoryTapped(_ handler: @escaping (() -> Void)) -> ListItem {
-        state.onAccessoryTapped = handler
-        return self
+        var listItem = self
+        listItem.onAccessoryTapped = handler
+        return listItem
     }
 
     /// The line limit for `title`.
     /// - Parameter titleLineLimit: The  number of lines to display for the `title`.
     /// - Returns: The modified `ListItem` with the property set.
     func titleLineLimit(_ titleLineLimit: Int) -> ListItem {
-        state.titleLineLimit = titleLineLimit
-        return self
+        var listItem = self
+        listItem.titleLineLimit = titleLineLimit
+        return listItem
     }
 
     /// The line limit for `subtitle`.
     /// - Parameter subtitleLineLimit: The  number of lines to display for the `subtitle`.
     /// - Returns: The modified `ListItem` with the property set.
     func subtitleLineLimit(_ subtitleLineLimit: Int) -> ListItem {
-        state.subtitleLineLimit = subtitleLineLimit
-        return self
+        var listItem = self
+        listItem.subtitleLineLimit = subtitleLineLimit
+        return listItem
     }
 
     /// The line limit for `footer`.
     /// - Parameter footerLineLimit: The  number of lines to display for the `footer`.
     /// - Returns: The modified `ListItem` with the property set.
     func footerLineLimit(_ footerLineLimit: Int) -> ListItem {
-        state.footerLineLimit = footerLineLimit
-        return self
+        var listItem = self
+        listItem.footerLineLimit = footerLineLimit
+        return listItem
     }
 
     /// The background styling of the `ListItem` to match the type of `List` it is displayed in.
     /// - Parameter backgroundStyleType: The style of the background.
     /// - Returns: The modified `ListItem` with the property set.
     func backgroundStyleType(_ backgroundStyleType: ListItemBackgroundStyleType) -> ListItem {
-        state.backgroundStyleType = backgroundStyleType
-        return self
+        var listItem = self
+        listItem.backgroundStyleType = backgroundStyleType
+        return listItem
+    }
+
+    /// The size of the `LeadingContent`.
+    /// - Parameter size: The size the leading content should be.
+    /// - Returns: The modified `ListItem` with the property set.
+    func leadingContentSize(_ size: ListItemLeadingContentSize) -> ListItem {
+        var listItem = self
+        listItem.tokenSet = ListItemTokenSet(customViewSize: { size })
+        return listItem
     }
 }
