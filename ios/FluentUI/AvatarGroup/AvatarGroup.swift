d79fd94863b10a8c73abe8c3bb101427e7231977
huwilkes <67026548+huwilkes@users.noreply.github.com>
2023-08-18 14:27:19 -0700

    Add unread dot

--- ios/FluentUI/AvatarGroup/AvatarGroup.swift
+++ ios/FluentUI/AvatarGroup/AvatarGroup.swift
@@ -197,9 +197,7 @@ public struct AvatarGroup: View, TokenizedControlView {
         }
 
         @ViewBuilder
-        var avatarGroupContent: some View {
-            let animation = Animation.linear(duration: animationDuration)
-
+        var avatarGroup: some View {
             HStack(spacing: 0) {
                 ForEach(enumeratedAvatars.prefix(avatarsToDisplay), id: \.1) { index, avatar in
                     avatarView(at: index, for: avatar)
@@ -213,19 +211,52 @@ public struct AvatarGroup: View, TokenizedControlView {
                     .transition(AnyTransition.opacity)
                 }
             }
-            .animation(animation, value: state.avatars)
-            .animation(animation, value: [state.maxDisplayedAvatars, state.overflowCount])
-            .animation(animation, value: state.style)
-            .animation(animation, value: state.size)
-            .frame(maxWidth: .infinity,
-                   minHeight: groupHeight,
-                   maxHeight: .infinity,
-                   alignment: .leading)
-            .accessibilityElement(children: .combine)
-            .accessibilityLabel(groupLabel)
         }
 
-        return avatarGroupContent
+        @ViewBuilder
+        var unreadGroup: some View {
+            if state.isUnread {
+                let strokeWidth = AvatarGroupTokenSet.unreadDotStrokeWidth
+                let dotSize = AvatarGroupTokenSet.unreadDotSize
+                avatarGroup
+                    .overlay(alignment: .topTrailing) {
+                        Circle()
+                            .foregroundColor(Color(tokenSet[.unreadDotColor].uiColor))
+                            .frame(width: dotSize, height: dotSize)
+                            // Add half the strokeWidth as padding to get the stroke drawn around the outside of the
+                            // dot instead of having the stroke centered on the edge of the dot, but it needs to be
+                            // inset slightly to not have a gap.
+                            .padding(strokeWidth / 2 - strokeInset)
+                            .overlay() {
+                                Circle()
+                                    .stroke(Color(tokenSet[.backgroundColor].uiColor),
+                                            lineWidth: strokeWidth)
+                            }
+                            .offset(x: AvatarGroupTokenSet.unreadDotHorizontalOffset,
+                                    y: AvatarGroupTokenSet.unreadDotVerticalOffset)
+                    }
+            } else {
+                avatarGroup
+            }
+        }
+
+        @ViewBuilder
+        var animatedGroup: some View {
+            let animation = Animation.linear(duration: animationDuration)
+            unreadGroup
+                .animation(animation, value: state.avatars)
+                .animation(animation, value: [state.maxDisplayedAvatars, state.overflowCount])
+                .animation(animation, value: state.style)
+                .animation(animation, value: state.size)
+                .frame(maxWidth: .infinity,
+                       minHeight: groupHeight,
+                       maxHeight: .infinity,
+                       alignment: .leading)
+                .accessibilityElement(children: .combine)
+                .accessibilityLabel(groupLabel)
+        }
+
+        return animatedGroup
     }
 
     var avatarsToDisplay: Int {
@@ -269,6 +300,7 @@ public struct AvatarGroup: View, TokenizedControlView {
     @ObservedObject var state: MSFAvatarGroupStateImpl
 
     private let animationDuration: CGFloat = 0.1
+    private let strokeInset: CGFloat = 0.1
 
     private func createOverflow(count: Int) -> Avatar {
         let state = MSFAvatarStateImpl(style: .overflow, size: state.size)
