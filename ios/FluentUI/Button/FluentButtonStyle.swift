c0c0c3d31890bef1b367ece200e249b0103e5c57
Mike Schreiber <mischreiber@microsoft.com>
2024-03-26 11:25:09 -0700

    Fix floating buttons and add hover effect

--- ios/FluentUI/Button/FluentButtonStyle.swift
+++ ios/FluentUI/Button/FluentButtonStyle.swift
@@ -43,7 +43,7 @@ public struct FluentButtonStyle: SwiftUI.ButtonStyle {
 
         @ViewBuilder var backgroundView: some View {
             if isFloatingStyle {
-                foregroundColor.clipShape(Capsule())
+                backgroundColor.clipShape(Capsule())
             } else {
                 backgroundColor.clipShape(RoundedRectangle(cornerRadius: cornerRadius))
             }
@@ -63,7 +63,6 @@ public struct FluentButtonStyle: SwiftUI.ButtonStyle {
             }
         }
 
-        // TODO: why are floating buttons filled with the wrong color?
         return configuration.label
             .font(Font(tokenSet[.titleFont].uiFont))
             .foregroundStyle(foregroundColor)
@@ -72,6 +71,7 @@ public struct FluentButtonStyle: SwiftUI.ButtonStyle {
             .background(backgroundView)
             .overlay { overlayView }
             .applyShadow(shadowInfo: shadowInfo)
+            .pointerInteraction(isEnabled)
     }
 
     @ObservedObject private var tokenSet: ButtonTokenSet
@@ -95,100 +95,4 @@ public struct FluentButtonStyle: SwiftUI.ButtonStyle {
     }
 
     private var isFloatingStyle: Bool { style.isFloating }
-
-//    @Environment(\.isEnabled) var isEnabled: Bool
-//    @Environment(\.isFocused) var isFocused: Bool
-//    @ObservedObject var tokenSet: ButtonTokenSet
-//
-//    var text: String?
-//    var image: Image?
-//    var style: FluentUI.ButtonStyle
-//    var sizeCategory: FluentUI.ButtonSizeCategory
-//
-//    func makeBody(configuration: Self.Configuration) -> some View {
-//        let isPressed = configuration.isPressed
-//        let isDisabled = !isEnabled
-//        let isFocused = isFocused
-//        let isFloatingStyle = style.isFloating
-//        let shouldUsePressedShadow = isDisabled || isPressed
-//        let verticalPadding = tokenSet[.minVerticalPadding].float
-//        let horizontalPadding = ButtonTokenSet.horizontalPadding(style: style, size: sizeCategory)
-//        let foregroundColor: Color
-//        let borderColor: Color
-//        let backgroundColor: Color
-//        if isDisabled {
-//            foregroundColor = Color(tokenSet[.foregroundDisabledColor].uiColor)
-//            borderColor = Color(tokenSet[.borderDisabledColor].uiColor)
-//            backgroundColor = Color(tokenSet[.backgroundDisabledColor].uiColor)
-//        } else if isPressed || isFocused {
-//            foregroundColor = Color(tokenSet[.foregroundPressedColor].uiColor)
-//            borderColor = Color(tokenSet[.borderPressedColor].uiColor)
-//            backgroundColor = Color(tokenSet[.backgroundPressedColor].uiColor)
-//        } else {
-//            foregroundColor = Color(tokenSet[.foregroundColor].uiColor)
-//            borderColor = Color(tokenSet[.borderColor].uiColor)
-//            backgroundColor = Color(tokenSet[.backgroundColor].uiColor)
-//        }
-//
-//        @ViewBuilder
-//        var buttonContent: some View {
-//            HStack(spacing: tokenSet[.interspace].float) {
-//                if let image {
-//                    image
-//                        .resizable()
-//                        .foregroundColor(foregroundColor)
-//                        .frame(width: tokenSet[.iconSize].float, height: tokenSet[.iconSize].float, alignment: .center)
-//                }
-//                if let text {
-//                    Text(text)
-//                        .multilineTextAlignment(.center)
-//                        .font(.init(tokenSet[.textFont].uiFont))
-//                        .frame(minHeight: isFloatingStyle ? tokenSet[.textMinimumHeight].float : nil)
-//                }
-//            }
-//            .padding(EdgeInsets(top: verticalPadding,
-//                                leading: horizontalPadding,
-//                                bottom: verticalPadding,
-//                                trailing: horizontalPadding))
-//            .modifyIf(isFloatingStyle && !(text?.isEmpty ?? true), { view in
-//                view.padding(.horizontal, tokenSet[.textAdditionalHorizontalPadding].float )
-//            })
-//            .frame(maxWidth: .infinity, minHeight: tokenSet[.minHeight].float, maxHeight: .infinity)
-//            .foregroundColor(foregroundColor)
-//        }
-//
-//        @ViewBuilder
-//        var buttonBackground: some View {
-//            if tokenSet[.borderWidth].float > 0 {
-//                buttonContent.background(
-//                    RoundedRectangle(cornerRadius: tokenSet[.cornerRadius].float)
-//                        .strokeBorder(lineWidth: tokenSet[.borderWidth].float, antialiased: false)
-//                        .foregroundColor(borderColor)
-//                        .contentShape(Rectangle()))
-//            } else {
-//                buttonContent.background(
-//                    RoundedRectangle(cornerRadius: tokenSet[.cornerRadius].float)
-//                        .fill(backgroundColor))
-//            }
-//        }
-//
-//        let shadowInfo = (shouldUsePressedShadow ? tokenSet[.shadowPressed] : tokenSet[.shadowRest]).shadowInfo
-//
-//        @ViewBuilder
-//        var button: some View {
-//            if isFloatingStyle {
-//                buttonBackground
-//                    .clipShape(Capsule())
-//                    .applyShadow(shadowInfo: shadowInfo)
-//                    .contentShape(Capsule())
-//            } else {
-//                buttonBackground
-//                    .contentShape(RoundedRectangle(cornerRadius: tokenSet[.cornerRadius].float))
-//            }
-//        }
-//
-//        return button
-//            .pointerInteraction(isEnabled)
-//    }
-//
 }
