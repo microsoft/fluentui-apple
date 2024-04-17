9579fc73e2f09daa0a66a2603e36586e3c3b19c2
Mike Schreiber <mischreiber@microsoft.com>
2024-04-17 14:17:17 -0700

    Adding `minimumScaleFactor` to HUD rendering (#1997)
    * Adding `minimumScaleFactor` to HUD rendering
    
    * Move minimumScaleFactor constant to HeadsUpDisplayTokenSet

--- ios/FluentUI/HUD/HeadsUpDisplay.swift
+++ ios/FluentUI/HUD/HeadsUpDisplay.swift
@@ -79,6 +79,7 @@ public struct HeadsUpDisplay: View, TokenizedControlView {
                     Spacer()
                         .frame(height: verticalPadding)
                     Text(label)
+                        .minimumScaleFactor(HeadsUpDisplayTokenSet.labelMinimumScaleFactor)
                         .foregroundColor(Color(tokenSet[.labelColor].uiColor))
                         .lineLimit(2)
                         .multilineTextAlignment(.center)
