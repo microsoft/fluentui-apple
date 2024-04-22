373c3089e3d7bc05567a26b6e94c56bbf2979a38
Joanna Qu <55368679+joannaquu@users.noreply.github.com>
2024-04-22 17:28:22 -0400

    add support for ListItem on visionOS

--- ios/FluentUI/List/ListItem.swift
+++ ios/FluentUI/List/ListItem.swift
@@ -17,25 +17,28 @@ public struct ListItem<LeadingContent: View,
                        Subtitle: StringProtocol,
                        Footer: StringProtocol>: View {
 
-	// MARK: Initializer
+    // MARK: Initializer
 
-	/// Creates a ListItem view
-	/// - Parameters:
-	///   - title: Text that appears as the first line of text
-	///   - subtitle: Text that appears as the second line of text
-	///   - footer: Text that appears as the third line of text
-	///   - leadingContent: The content that appears on the leading edge of the view
-	///   - trailingContent: The content that appears on the trailing edge of the view, next to the accessory type if provided
+    /// Creates a `ListItem`
+    /// - Parameters:
+    ///   - title: Text that appears as the first line of text
+    ///   - subtitle: Text that appears as the second line of text
+    ///   - footer: Text that appears as the third line of text
+    ///   - leadingContent: The content that appears on the leading edge of the view
+    ///   - trailingContent: The content that appears on the trailing edge of the view, next to the accessory type if provided
+    ///   - action: The action to be dispatched by tapping on the `ListItem`
     public init(title: Title,
                 subtitle: Subtitle = String(),
                 footer: Footer = String(),
                 @ViewBuilder leadingContent: @escaping () -> LeadingContent,
-                @ViewBuilder trailingContent: @escaping () -> TrailingContent) {
+                @ViewBuilder trailingContent: @escaping () -> TrailingContent,
+                action: (() -> Void)? = nil) {
         self.title = title
         self.subtitle = subtitle
         self.footer = footer
         self.leadingContent = leadingContent
         self.trailingContent = trailingContent
+        self.action = action
         let layoutType = ListItem.layoutType(subtitle: subtitle, footer: footer)
         self.tokenSet = ListItemTokenSet(customViewSize: { layoutType.leadingContentSize })
     }
@@ -92,12 +95,6 @@ public struct ListItem<LeadingContent: View,
                 let image = Image(uiImage: icon)
                     .foregroundColor(Color(uiColor: iconColor))
                     .accessibilityIdentifier(AccessibilityIdentifiers.accessoryImage)
-                    .padding(EdgeInsets(top: ListItemTokenSet.paddingVertical,
-                                        leading: ListItemTokenSet.horizontalSpacing,
-                                        bottom: ListItemTokenSet.paddingVertical,
-                                        trailing: ListItemTokenSet.paddingTrailing))
-                    // A non clear background must be applied for VoiceOver focus ring to be around the padded view
-                    .background(backgroundView)
                 if accessoryType == .detailButton {
                     SwiftUI.Button {
                         if let onAccessoryTapped = onAccessoryTapped {
@@ -106,6 +103,12 @@ public struct ListItem<LeadingContent: View,
                     } label: {
                         image
                     }
+#if os(visionOS)
+                    .buttonStyle(.bordered)
+                    .clipShape(Circle())
+#else
+                    .buttonStyle(.plain)
+#endif
                     .accessibilityIdentifier(AccessibilityIdentifiers.accessoryDetailButton)
                     .accessibility(label: Text("Accessibility.TableViewCell.MoreActions.Label".localized))
                     .accessibility(hint: Text("Accessibility.TableViewCell.MoreActions.Hint".localized))
@@ -146,7 +149,7 @@ public struct ListItem<LeadingContent: View,
         }
 
         @ViewBuilder
-        var contentView: some View {
+        var innerContent: some View {
             HStack(alignment: .center) {
                 HStack(spacing: 0) {
                     leadingContentView
@@ -172,14 +175,30 @@ public struct ListItem<LeadingContent: View,
                         .accessibilitySortPriority(1)
                 }
                 accessoryView
+                    .padding(EdgeInsets(top: ListItemTokenSet.paddingVertical,
+                                        leading: ListItemTokenSet.horizontalSpacing,
+                                        bottom: ListItemTokenSet.paddingVertical,
+                                        trailing: ListItemTokenSet.paddingTrailing))
             }
             .frame(minHeight: layoutType.minHeight)
             .opacity(isEnabled ? ListItemTokenSet.enabledAlpha : ListItemTokenSet.disabledAlpha)
             .background(backgroundView)
-            .listRowInsets(EdgeInsets())
+        }
+
+        @ViewBuilder
+        var contentView: some View {
+            if let action = action {
+                SwiftUI.Button(action: action, label: {
+                    innerContent
+                })
+                .buttonStyle(ListItemButtonStyle(backgroundStyle: backgroundStyleType))
+            } else {
+                innerContent
+            }
         }
 
         return contentView
+                .listRowInsets(EdgeInsets())
     }
 
     private static func layoutType(subtitle: Subtitle, footer: Footer) -> LayoutType {
@@ -224,7 +243,7 @@ public struct ListItem<LeadingContent: View,
 
     // MARK: Internal variables
 
-    /// The `ListItemAccessoryType` that the view should display.
+    /// The `ListItemAccessoryType` that the `ListItem` should display.
     var accessoryType: ListItemAccessoryType = .none
 
     /// The background styling of the `ListItem` to match the type of `List` it is displayed in.
@@ -264,10 +283,34 @@ public struct ListItem<LeadingContent: View,
 
     private var leadingContent: (() -> LeadingContent)?
     private var trailingContent: (() -> TrailingContent)?
+    private var action: (() -> Void)?
 
     private let footer: Footer
     private let subtitle: Subtitle
     private let title: Title
+}
+
+// MARK: Internal structs
+
+private struct ListItemButtonStyle: SwiftUI.ButtonStyle {
+    init(backgroundStyle: ListItemBackgroundStyleType) {
+        self.backgroundStyle = backgroundStyle
+    }
+
+    func makeBody(configuration: Configuration) -> some View {
+        let backgroundColor = configuration.isPressed ? ListItemTokenSet(customViewSize: { ListItemLeadingContentSize.zero })[.cellBackgroundSelectedColor].uiColor : .clear
+        let cornerRadius = backgroundStyle == .plain && Compatibility.isDeviceIdiomVision() ? 16.0 : 0
+
+        return configuration.label
+            .background(Color(backgroundColor))
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+            .contentShape(RoundedRectangle(cornerRadius: cornerRadius))
+            .pointerInteraction(isEnabled)
+    }
+
+    let backgroundStyle: ListItemBackgroundStyleType
+
+    @Environment(\.isEnabled) private var isEnabled: Bool
 }
 
 // MARK: Constants
@@ -287,10 +330,12 @@ private struct AccessibilityIdentifiers {
 public extension ListItem where LeadingContent == EmptyView, TrailingContent == EmptyView {
     init(title: Title,
          subtitle: Subtitle = String(),
-         footer: Footer = String()) {
+         footer: Footer = String(),
+         action: (() -> Void)? = nil) {
         self.title = title
         self.subtitle = subtitle
         self.footer = footer
+        self.action = action
         let layoutType = ListItem.layoutType(subtitle: subtitle, footer: footer)
         self.tokenSet = ListItemTokenSet(customViewSize: { layoutType.leadingContentSize })
     }
@@ -300,11 +345,13 @@ public extension ListItem where TrailingContent == EmptyView {
     init(title: Title,
          subtitle: Subtitle = String(),
          footer: Footer = String(),
-         @ViewBuilder leadingContent: @escaping () -> LeadingContent) {
+         @ViewBuilder leadingContent: @escaping () -> LeadingContent,
+         action: (() -> Void)? = nil) {
         self.title = title
         self.subtitle = subtitle
         self.footer = footer
         self.leadingContent = leadingContent
+        self.action = action
         let layoutType = ListItem.layoutType(subtitle: subtitle, footer: footer)
         self.tokenSet = ListItemTokenSet(customViewSize: { layoutType.leadingContentSize })
     }
@@ -314,11 +361,13 @@ public extension ListItem where LeadingContent == EmptyView {
     init(title: Title,
          subtitle: Subtitle = String(),
          footer: Footer = String(),
-         @ViewBuilder trailingContent: @escaping () -> TrailingContent) {
+         @ViewBuilder trailingContent: @escaping () -> TrailingContent,
+         action: (() -> Void)? = nil) {
         self.title = title
         self.subtitle = subtitle
         self.footer = footer
         self.trailingContent = trailingContent
+        self.action = action
         let layoutType = ListItem.layoutType(subtitle: subtitle, footer: footer)
         self.tokenSet = ListItemTokenSet(customViewSize: { layoutType.leadingContentSize })
     }
