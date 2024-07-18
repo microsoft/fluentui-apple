cda11a888ae0b5a29f47d9ab1610f596b8d102df
Sophia Lee <sophia.lee0416@gmail.com>
2024-07-17 21:17:40 -0700

    New method for separator hiding (#2068) (#2073)
    (cherry picked from commit 30b8885f5d563b3a0b32f0c42a203900c2065340)
    (cherry picked from commit 60aea2bed91fb219ffec8a93a09daefc2c41ab61)
    
    Co-authored-by: Lukas Capkovic <3610850+lcapkovic@users.noreply.github.com>

--- ios/FluentUI/Table View/TableViewCell.swift
+++ ios/FluentUI/Table View/TableViewCell.swift
@@ -1517,9 +1517,9 @@ open class TableViewCell: UITableViewCell, TokenizedControlInternal {
         // A hacky way to hide the system separator by squeezing it just enough to have zero width.
         // This is the only known way to hide the separator without making the UITableView do it for us.
         let boundsWidth = bounds.width
-        if separatorInset.left != boundsWidth || separatorInset.right != 0 {
-            separatorInset.left = boundsWidth
-            separatorInset.right = 0
+        let targetSystemSeparatorInset = UIEdgeInsets(top: 0, left: boundsWidth, bottom: 0, right: 0)
+        if separatorInset.left < boundsWidth {
+            separatorInset = targetSystemSeparatorInset
         }
 
         layoutSeparator(topSeparator, with: topSeparatorType, at: 0)
