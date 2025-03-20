275785ec8c6496379ec7a43dc5d6d673b3226e76
Alexander Boswell <alboswel@microsoft.com>
2025-03-20 07:56:56 -0700

    fix whitespace

--- Sources/FluentUI_iOS/Components/List/FluentListSectionHeader.swift
+++ Sources/FluentUI_iOS/Components/List/FluentListSectionHeader.swift
@@ -7,8 +7,8 @@ import SwiftUI
 
 /// Defines the visual style of the FluentListSectionHeader
 public enum ListSectionHeaderStyle {
-	case regular
-	case primary
+    case regular
+    case primary
 }
 
 /// This component is a work in progress. Expect changes to be made to it on a somewhat regular basis.
@@ -21,25 +21,25 @@ public struct FluentListSectionHeader<Title: StringProtocol, TrailingContent: Vi
     /// Creates a `FluentListSectionHeader`
     /// - Parameters:
     ///   - title: title of the section to be shown in the header.
-	///   - style:  The visual style of how the header should be displayed.
+    ///   - style:  The visual style of how the header should be displayed.
     ///   - trailingContent: content that appears on the trailing edge of the view.
     public init(title: Title,
-				style: ListSectionHeaderStyle,
+                style: ListSectionHeaderStyle,
                 @ViewBuilder trailingContent: @escaping () -> TrailingContent) {
         self.title = title
-		self.tokenSet = .init(style: { FluentListSectionHeader.tokenSetStyle(style) }, accessoryButtonStyle: { .regular })
+        self.tokenSet = .init(style: { FluentListSectionHeader.tokenSetStyle(style) }, accessoryButtonStyle: { .regular })
         self.trailingContent = trailingContent
     }
 
     public var body: some View {
-		tokenSet.update(fluentTheme)
+        tokenSet.update(fluentTheme)
 
         @ViewBuilder var titleView: some View {
 			Text(title)
-			// By default uppercasing is applied, setting nil to not enforce casing
-				.textCase(nil)
-				.font(Font(tokenSet[.textFont].uiFont))
-				.foregroundStyle(tokenSet[.textColor].color)
+                // By default uppercasing is applied, setting nil to not enforce casing
+                .textCase(nil)
+                .font(Font(tokenSet[.textFont].uiFont))
+                .foregroundStyle(tokenSet[.textColor].color)
         }
 
         @ViewBuilder var contentView: some View {
@@ -60,20 +60,20 @@ public struct FluentListSectionHeader<Title: StringProtocol, TrailingContent: Vi
 	// MARK: private methods
 
 	static func tokenSetStyle(_ style: ListSectionHeaderStyle) -> TableViewHeaderFooterView.Style {
-		switch style {
-		case .regular:
-			return .header
-		case .primary:
-			return .headerPrimary
-		}
-	}
+        switch style {
+        case .regular:
+            return .header
+        case .primary:
+            return .headerPrimary
+        }
+    }
 
     // MARK: Private variables
 
     private var trailingContent: (() -> TrailingContent)?
     private let title: Title
-	private let tokenSet: TableViewHeaderFooterViewTokenSet
-	@Environment(\.fluentTheme) private var fluentTheme: FluentTheme
+    private let tokenSet: TableViewHeaderFooterViewTokenSet
+    @Environment(\.fluentTheme) private var fluentTheme: FluentTheme
 }
 
 // MARK: Additional Initializers
@@ -81,13 +81,13 @@ public struct FluentListSectionHeader<Title: StringProtocol, TrailingContent: Vi
 public extension FluentListSectionHeader where TrailingContent == EmptyView {
     init(title: Title) {
         self.title = title
-		self.tokenSet = .init(style: { .header }, accessoryButtonStyle: { .regular })
+        self.tokenSet = .init(style: { .header }, accessoryButtonStyle: { .regular })
     }
 }
 
 public extension FluentListSectionHeader where TrailingContent == EmptyView {
-	init(title: Title, style: ListSectionHeaderStyle) {
-		self.title = title
-		self.tokenSet = .init(style: { FluentListSectionHeader.tokenSetStyle(style) }, accessoryButtonStyle: { .regular })
-	}
+    init(title: Title, style: ListSectionHeaderStyle) {
+        self.title = title
+        self.tokenSet = .init(style: { FluentListSectionHeader.tokenSetStyle(style) }, accessoryButtonStyle: { .regular })
+    }
 }
