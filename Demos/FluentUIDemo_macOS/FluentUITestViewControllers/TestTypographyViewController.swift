80921e745153274bc4c0b6cb0b32a93726a14bc4
Veronica Salas <veronicasalas@Veronicas-MacBook-Pro.local>
2025-04-30 13:35:03 -0700

    adding nsfont+extension and test view controller

---
+++ Demos/FluentUIDemo_macOS/FluentUITestViewControllers/TestTypographyViewController.swift
@@ -0,0 +1,60 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+#if canImport(FluentUI_common)
+import FluentUI_common
+#endif
+import AppKit
+import FluentUI
+
+class TestTypographyViewController: NSViewController {
+	override func viewDidLoad() {
+		let containerView = NSStackView(frame: .zero)
+		containerView.orientation = .vertical
+
+		for typographyToken in FluentTheme.TypographyToken.allCases {
+			let text = NSTextField(labelWithString: typographyToken.text)
+			text.font = NSFont.fluent(fluentTheme.typographyInfo(typographyToken))
+			containerView.addArrangedSubview(text)
+		}
+
+		view = containerView
+	}
+}
+
+// MARK: - Private extensions
+private let fluentTheme = FluentTheme()
+
+private extension FluentTheme.TypographyToken {
+	var text: String {
+		switch self {
+		case .display:
+			return "Display"
+		case .largeTitle:
+			return "Large Title"
+		case .title1:
+			return "Title 1"
+		case .title2:
+			return "Title 2"
+		case .title3:
+			return "Title 3"
+		case .body1Strong:
+			return "Body 1 Strong"
+		case .body1:
+			return "Body 1"
+		case .body2Strong:
+			return "Body 2 Strong"
+		case .body2:
+			return "Body 2"
+		case .caption1Strong:
+			return "Caption 1 Strong"
+		case .caption1:
+			return "Caption 1"
+		case .caption2:
+			return "Caption 2"
+		}
+	}
+}
+
