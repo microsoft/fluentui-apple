6f04357e852cc9eb441da398b012143cc74160d5
Mike Schreiber <mischreiber@microsoft.com>
2025-05-21 10:17:14 -0700

    Create NavigationSplitView and basic demos

---
+++ Demos/FluentUIDemo/Sources/Demo.swift
@@ -0,0 +1,96 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+
+/// An iterable enumeration of all available demos.
+enum Demo: CaseIterable, Hashable {
+    case button
+    case shimmer
+
+    var title: String {
+        switch self {
+        case .button:
+            return "Button"
+        case .shimmer:
+            return "Shimmer"
+        }
+    }
+
+    /// Returns the `View` instance for the given demo.
+    var view: any View {
+        switch self {
+        case .button:
+            return ButtonDemoView()
+        case .shimmer:
+            return ShimmerDemoView()
+        }
+    }
+
+    /// Only some demos are supported on visionOS.
+    var supportsVisionOS: Bool {
+        switch self {
+        case .button:
+            return true
+        case .shimmer:
+            return false
+        }
+    }
+}
+
+enum DemoListSection: CaseIterable, Hashable {
+    case fluent2Controls
+    case fluent2DesignTokens
+    case controls
+#if DEBUG
+    case debug
+#endif
+
+    var title: String {
+        switch self {
+        case .fluent2Controls:
+            return "Fluent 2 Controls"
+        case .fluent2DesignTokens:
+            return "Fluent 2 Design Tokens"
+        case .controls:
+            return "Controls"
+#if DEBUG
+        case .debug:
+            return "DEBUG"
+#endif
+        }
+    }
+
+    var demos: [Demo] {
+        switch self {
+        case .fluent2Controls:
+            return Demos.fluent2
+        case .fluent2DesignTokens:
+            return Demos.fluent2DesignTokens
+        case .controls:
+            return Demos.controls
+#if DEBUG
+        case .debug:
+            return Demos.debug
+#endif
+        }
+    }
+}
+
+private struct Demos {
+    static let fluent2: [Demo] = [
+        .button,
+        .shimmer
+    ]
+
+    static let fluent2DesignTokens: [Demo] = [
+    ]
+
+    static let controls: [Demo] = [
+    ]
+
+    static let debug: [Demo] = [
+    ]
+}
---
+++ Demos/FluentUIDemo/Sources/DemoViews/ButtonDemoView.swift
@@ -0,0 +1,29 @@
+//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License.
+//
+
+import SwiftUI
+import FluentUI
+
+struct ButtonDemoView: View {
+    @Environment(\.fluentTheme) var fluentTheme
+
+    var body: some View {
+        VStack {
+            Button(action: {}, label: {
+                HStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                    Text("Hello, world!")
+                }
+            })
+#if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+            .buttonStyle(FluentButtonStyle(style: .accent))
+#endif
+            .controlSize(.extraLarge)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(fluentTheme.swiftUIColor(.background1))
+    }
+}
