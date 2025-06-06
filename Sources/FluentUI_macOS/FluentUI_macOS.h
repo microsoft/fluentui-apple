99052de0baff5777b498078a6a16aeb8065e6237
Mike Schreiber <mischreiber@microsoft.com>
2025-06-06 10:05:31 -0700

    Update modulemaps (#2173)

--- Sources/FluentUI_macOS/FluentUI_macOS.h
+++ Sources/FluentUI_macOS/FluentUI_macOS.h
@@ -3,7 +3,5 @@
 //  Licensed under the MIT License.
 //
 
-#import <UIKit/UIKit.h>
+#import <AppKit/AppKit.h>
 // In this header, you should import all the public headers of your framework using statements like #import <FluentUI/PublicHeader.h>
-
-
