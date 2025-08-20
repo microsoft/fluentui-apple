07febe4545036c57655a177d7fa90749e02bdf50
Mike Schreiber <mischreiber@microsoft.com>
2025-08-20 14:29:00 -0700

    [iOS][macOS] Move PlatformProviding code to `FluentUI_common` (#2198)
    * Move implementation of PlatformThemeProviding to common
    
    * Same for FontInfo and GlobalTokens

--- Sources/FluentUI_common/Core/Theme/Extensions/iOS/GlobalTokens+iOS.swift
+++ Sources/FluentUI_common/Core/Theme/Extensions/iOS/GlobalTokens+iOS.swift
@@ -3,32 +3,31 @@
 //  Licensed under the MIT License.
 //
 
-#if canImport(FluentUI_common)
-import FluentUI_common
-#endif
+#if canImport(UIKit)
 import SwiftUI
 
 extension GlobalTokens : PlatformGlobalTokenProviding {
     public static func fontSize(for token: GlobalTokens.FontSizeToken) -> CGFloat {
         switch token {
         case .size100:
-            return 10.0
-        case .size200:
-            return 11.0
-        case .size300:
             return 12.0
-        case .size400:
+        case .size200:
             return 13.0
-        case .size500:
+        case .size300:
             return 15.0
-        case .size600:
+        case .size400:
             return 17.0
-        case .size700:
+        case .size500:
+            return 20.0
+        case .size600:
             return 22.0
+        case .size700:
+            return 28.0
         case .size800:
-            return 26.0
+            return 34.0
         case .size900:
-            return 44.0
+            return 60.0
         }
     }
 }
+#endif // canImport(UIKit)
