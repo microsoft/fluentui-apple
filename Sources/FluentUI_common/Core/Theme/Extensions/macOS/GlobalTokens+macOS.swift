f1c7acce2489b7d0047451460a0edd507ea85557
Mike Schreiber <mischreiber@microsoft.com>
2025-08-20 10:00:06 -0700

    Same for FontInfo and GlobalTokens

--- Sources/FluentUI_common/Core/Theme/Extensions/macOS/GlobalTokens+macOS.swift
+++ Sources/FluentUI_common/Core/Theme/Extensions/macOS/GlobalTokens+macOS.swift
@@ -3,32 +3,31 @@
 //  Licensed under the MIT License.
 //
 
-#if canImport(FluentUI_common)
-import FluentUI_common
-#endif
+#if canImport(AppKit)
 import SwiftUI
 
 extension GlobalTokens : PlatformGlobalTokenProviding {
     public static func fontSize(for token: GlobalTokens.FontSizeToken) -> CGFloat {
         switch token {
         case .size100:
-            return 12.0
+            return 10.0
         case .size200:
-            return 13.0
+            return 11.0
         case .size300:
-            return 15.0
+            return 12.0
         case .size400:
-            return 17.0
+            return 13.0
         case .size500:
-            return 20.0
+            return 15.0
         case .size600:
-            return 22.0
+            return 17.0
         case .size700:
-            return 28.0
+            return 22.0
         case .size800:
-            return 34.0
+            return 26.0
         case .size900:
-            return 60.0
+            return 44.0
         }
     }
 }
+#endif // canImport(AppKit)
