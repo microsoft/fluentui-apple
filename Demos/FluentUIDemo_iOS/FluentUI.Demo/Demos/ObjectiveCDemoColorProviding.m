7d316cd5c578fd2bcce8d578c9dc92420972a03b
Daniel Morales <dans_alex@hotmail.com>
2026-02-05 15:12:33 -0800

    Removing unnecessary stuff from iOS tests

--- Demos/FluentUIDemo_iOS/FluentUI.Demo/Demos/ObjectiveCDemoColorProviding.m
+++ Demos/FluentUIDemo_iOS/FluentUI.Demo/Demos/ObjectiveCDemoColorProviding.m
@@ -36,8 +36,6 @@
     return dynamicColor;
 }
 
-/// MARK: ColorProviding
-
 - (UIColor *)brandStrokeColor {
     return nil;
 }
@@ -124,72 +122,6 @@
 
 - (UIColor *)brandGradient3 {
     return [self brandGradient3];
-}
-
-/// MARK: BrandColorProviding
-
-- (UIColor *)brand10 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand10Color"];
-}
-
-- (UIColor *)brand20 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand20Color"];
-}
-
-- (UIColor *)brand30 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand30Color"];
-}
-
-- (UIColor *)brand40 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand40Color"];
-}
-
-- (UIColor *)brand50 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand50Color"];
-}
-
-- (UIColor *)brand60 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand60Color"];
-}
-
-- (UIColor *)brand70 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand70Color"];
-}
-
-- (UIColor *)brand80 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand80Color"];
-}
-
-- (UIColor *)brand90 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand90Color"];
-}
-
-- (UIColor *)brand100 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand100Color"];
-}
-
-- (UIColor *)brand110 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand110Color"];
-}
-
-- (UIColor *)brand120 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand120Color"];
-}
-
-- (UIColor *)brand130 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand130Color"];
-}
-
-- (UIColor *)brand140 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand140Color"];
-}
-
-- (UIColor *)brand150 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand150Color"];
-}
-
-- (UIColor *)brand160 {
-    return [UIColor colorNamed:@"Colors/DemoGreenBrand160Color"];
 }
 
 @end
